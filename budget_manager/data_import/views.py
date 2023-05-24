4ed4e26605a7ccb50b607aa1483267bae665bbaf
Dawid Mateusiak <dawidm@media-press.tv>
2023-05-24 12:21:23 +0200

    Updating and deleting ImportFile objects

--- budget_manager/data_import/views.py
+++ budget_manager/data_import/views.py
@@ -1,12 +1,20 @@
 from rest_framework import viewsets
-from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    ListModelMixin,
+    RetrieveModelMixin,
+    UpdateModelMixin,
+)
 from rest_framework.permissions import IsAuthenticated
 
 from .models import ImportFile
 from .serializers import ImportFileSerializer
 
 
-class ImportFileViewSet(ListModelMixin, CreateModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
+class ImportFileViewSet(
+    ListModelMixin, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin, viewsets.GenericViewSet
+):
     permission_classes = (IsAuthenticated,)
     queryset = ImportFile.objects.all()
     serializer_class = ImportFileSerializer
