d8c02950a21c255e951c30f188b1e56fbe11b4fa
MateDawid <mateusiakdawid@gmail.com>
2023-06-24 23:00:10 +0200

    Add headers and content to ImportFile model

--- budget_manager/data_import/models.py
+++ budget_manager/data_import/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class ImportFile(models.Model):
     file = models.FileField(upload_to='import_files', validators=[FileExtensionValidator(allowed_extensions=['csv'])])
     filename = models.CharField(max_length=128)
+    headers = models.JSONField(null=True)
+    content = models.JSONField(null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
