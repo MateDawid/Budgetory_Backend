9332fec1b8282507ffb81b30ac313f8ab10775d5
MateDawid <mateusiakdawid@gmail.com>
2023-06-25 13:13:43 +0200

    Saving csv data file and reading its content

--- budget_manager/data_import/views.py
+++ budget_manager/data_import/views.py
@@ -1,4 +1,5 @@
 import csv
+from copy import deepcopy
 from io import StringIO
 from typing import Union
 
@@ -26,49 +27,61 @@ class ImportFileViewSet(
     serializer_class = ImportFileSerializer
 
     @staticmethod
-    def read_csv_file(csv_file: InMemoryUploadedFile) -> tuple[list, list]:
+    def get_csv_data(csv_file: InMemoryUploadedFile, fixed_header_prefix: str = '__temp__') -> list:
+        def get_fixed_headers() -> list:
+            """Cleans headers from empty strings"""
+            header_number = 1
+            fixed_headers = []
+            for header in reader.fieldnames:
+                if not header:
+                    fixed_headers.append(f'{fixed_header_prefix}{header_number}')
+                    header_number += 1
+                    continue
+                fixed_headers.append(header)
+            return fixed_headers
+
+        def get_fixed_content() -> list:
+            """Fill last valid key with data from empty columns"""
+            reader.fieldnames = get_fixed_headers()
+            last_valid_header = None
+            for temp_header in reader.fieldnames[::-1]:
+                if temp_header.startswith(fixed_header_prefix):
+                    continue
+                last_valid_header = temp_header
+                break
+            fixed_lines = []
+            for line in reader:
+                fixed_line = {}
+                for key, value in line.items():
+                    if key == last_valid_header:
+                        value_parts = value.split(':')
+                        fixed_line[last_valid_header] = {value_parts[0]: ':'.join(value_parts[1:]).strip()}
+                        continue
+                    if key.startswith(fixed_header_prefix):
+                        value_parts = value.split(':')
+                        fixed_line[last_valid_header][value_parts[0]] = ':'.join(value_parts[1:]).strip()
+                        continue
+                    fixed_line[key] = value
+                fixed_line['id'] = str(fixed_line[last_valid_header])
+                fixed_lines.append(fixed_line)
+            return fixed_lines
+
         """Returns tuple, containing file headers and file lines"""
-        def get_headers(delimiter: str = ':') -> Union[list, Response]:
-            """Returns headers. Retrieves headers from file lines if needed"""
-            raw_headers = lines[0]
-            # All headers provided, returning them
-            if all(raw_headers):
-                return raw_headers
-            # Checking if data rows lengths the same as header length
-            raw_headers_length = len(raw_headers)
-            if not all([len(line) == raw_headers_length for line in lines]):
-                return Response(
-                    data={"error": "Headers length not the same as rows length"},
-                    status=status.HTTP_409_CONFLICT
-                )
-            # Collecting missing headers
-            processed_headers = []
-            for line in lines[1:]:
-                for cell_number, cell in enumerate(line):
-                    original_header = raw_headers[cell_number]
-                    if original_header:
-                        if original_header not in processed_headers:
-                            processed_headers.append(original_header)
-                        continue
-                    cell_parts = cell.split(delimiter)
-                    if len(cell_parts) < 2:
-                        continue
-                    header = cell_parts[0].strip()
-                    if header not in processed_headers:
-                        processed_headers.append(header)
-            return processed_headers
-
-        def get_content() -> list:
-            """Returns list of data dicts from csv lines"""
-            return []
-
         read_file = csv_file.read()
         decoded_file = read_file.decode(encoding='utf-8', errors="replace")
         csv_source = StringIO(decoded_file)
-        lines = [line for line in csv.reader(csv_source)]
-        if not lines:
-            return [], {}
-        return get_headers(), get_content()
+        reader = csv.DictReader(csv_source)
+        # All fieldnames provided
+        if all(reader.fieldnames):
+            return [line for line in reader]
+        # Not all fieldnames provided
+        lines = get_fixed_content()
+        return lines
+
+    @staticmethod
+    def get_headers(csv_data: list, delimiter: str = ':') -> Union[list, Response]:
+        """Returns csv file headers. Retrieves headers from file lines if needed"""
+        return list(csv_data[0].keys())
 
     def create(self, request, *args, **kwargs) -> Response:
         serializer = self.get_serializer(data=request.data)
@@ -76,10 +89,11 @@ class ImportFileViewSet(
         csv_file = request.data['file']
         if not request.data.get('filename', ''):
             request.data['filename'] = csv_file.name
-        headers, content = self.read_csv_file(csv_file)
+        csv_content = self.get_csv_data(csv_file)
+        csv_headers = self.get_headers(csv_content)
         obj = serializer.save()
-        obj.headers = headers
-        obj.content = content
+        obj.headers = csv_headers
+        obj.content = csv_content
         obj.save()
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        response_headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=response_headers)
