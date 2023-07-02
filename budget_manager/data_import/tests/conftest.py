3ecfb106385d4f7cc44be1496ccd0bb193161a0a
MateDawid <mateusiakdawid@gmail.com>
2023-07-02 21:56:53 +0200

    Remove useless test

--- budget_manager/data_import/tests/conftest.py
+++ budget_manager/data_import/tests/conftest.py
@@ -4,9 +4,3 @@ from pytest_factoryboy import register
 from data_import.tests.factories import ImportFileFactory
 
 register(ImportFileFactory)
-
-
-@pytest.fixture
-def function_fixture():
-    print('Fixture for each test')
-    return 1
