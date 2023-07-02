a472023be92737c9c1d64ed5738988ba81c85dcf
MateDawid <mateusiakdawid@gmail.com>
2023-07-02 21:55:32 +0200

    Add pytest-django library and configuring it

--- budget_manager/data_import/tests/conftest.py
+++ budget_manager/data_import/tests/conftest.py
@@ -1,5 +1,12 @@
+import pytest
 from pytest_factoryboy import register
 
 from data_import.tests.factories import ImportFileFactory
 
 register(ImportFileFactory, 'import_file_factory')
+
+
+@pytest.fixture
+def function_fixture():
+    print('Fixture for each test')
+    return 1
