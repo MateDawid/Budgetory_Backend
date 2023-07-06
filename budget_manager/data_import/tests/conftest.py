7ee11950e00094994e2907c924fcce7496a79eeb
MateDawid <mateusiakdawid@gmail.com>
2023-07-06 08:33:00 +0200

    Correct ImportFileFactory, add fixtures for data files testing

--- budget_manager/data_import/tests/conftest.py
+++ budget_manager/data_import/tests/conftest.py
@@ -1,6 +1,27 @@
+import csv
+import sys
+from io import StringIO
+
 import pytest
+from data_import.tests.factories import ImportFileFactory
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from pytest_factoryboy import register
 
-from data_import.tests.factories import ImportFileFactory
-
 register(ImportFileFactory)
+
+
+@pytest.fixture
+def valid_csv_file_content():
+    data = [{'column_1': 1, 'column_2': 2}, {'column_1': 1, 'column_2': 2}]
+    return data
+
+
+@pytest.fixture
+def valid_data_file(valid_csv_file_content):
+    io_file = StringIO()
+    writer = csv.DictWriter(io_file, fieldnames=valid_csv_file_content[0].keys())
+    writer.writeheader()
+    writer.writerows(valid_csv_file_content)
+    io_file.seek(0)
+    file = InMemoryUploadedFile(io_file, 'FileField', 'valid_file.csv', 'txt/csv', sys.getsizeof(io_file), None)
+    return file
--- budget_manager/data_import/tests/factories.py
+++ budget_manager/data_import/tests/factories.py
@@ -1,15 +1,10 @@
 import factory
-import pytz
-
 from data_import.models import ImportFile
 
 
 class ImportFileFactory(factory.django.DjangoModelFactory):
-    file = factory.Faker('file')
     filename = factory.Faker('word')
-    headers = factory.Faker('pylist')
-    content = factory.Faker('pydict')
-    date_added = factory.Faker('date_time_between', start_date="-30y", end_date="now", tzinfo=pytz.UTC)
+    date_added = factory.Faker('date_time_between', start_date='-1y', end_date='now')
 
     class Meta:
         model = ImportFile
