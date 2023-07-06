8517e822778b152bac40cd9284b28a3a24abd27b
MateDawid <mateusiakdawid@gmail.com>
2023-07-06 22:58:16 +0200

    Not store ImportFile file and filename in db

--- budget_manager/data_import/models.py
+++ budget_manager/data_import/models.py
@@ -6,10 +6,6 @@ class ImportFile(models.Model):
     file = models.FileField(
         upload_to='import_files', null=True, validators=[FileExtensionValidator(allowed_extensions=['csv'])]
     )
-    filename = models.CharField(max_length=128)
     headers = models.JSONField(null=True)
     content = models.JSONField(null=True)
     date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.filename} ({self.date_added})'
