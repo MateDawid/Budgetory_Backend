251312593784bc64eec53d0cff41286837f46c69
MateDawid <mateusiakdawid@gmail.com>
2023-07-13 19:49:05 +0200

    Test for invalid file extension

---
+++ tests/data_import/test_serializers.py
@@ -0,0 +1,14 @@
+import pytest
+from data_import.serializers import ImportFileSerializer
+from rest_framework.exceptions import ValidationError
+
+
+@pytest.mark.django_db
+def test_import_file_invalid_extension(txt_file):
+    serializer = ImportFileSerializer(data={'file': txt_file})
+    with pytest.raises(ValidationError) as exc:
+        serializer.is_valid(raise_exception=True)
+    assert (
+        'Only .csv files are allowed - .txt is not valid extension.'
+        == exc.value.get_full_details()['file'][0]['message']
+    )
