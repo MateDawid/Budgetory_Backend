a5a4f6d3b9c9b29403dc74ba2193f0aca812b2a3
MateDawid <mateusiakdawid@gmail.com>
2023-07-13 06:55:59 +0200

    Test for adding new data file

--- budget_manager/data_import/tests/test_viewsets.py
+++ budget_manager/data_import/tests/test_viewsets.py
@@ -6,7 +6,7 @@ from rest_framework.test import force_authenticate
 
 
 @pytest.mark.django_db
-def test_get_import_files(valid_csv_file_content, import_file_factory, api_rf, user):
+def test_get_import_files_list(valid_csv_file_content, import_file_factory, api_rf, user):
     headers = list(valid_csv_file_content[0].keys())
     import_file = import_file_factory(content=valid_csv_file_content, headers=headers)
 
@@ -19,3 +19,25 @@ def test_get_import_files(valid_csv_file_content, import_file_factory, api_rf, u
     assert response.data['results'][-1]['content'] == import_file.content
     assert response.data['results'][-1]['headers'] == import_file.headers
     assert len(response.data['results']) == len(ImportFile.objects.all())
+
+
+@pytest.mark.django_db
+def test_import_file_creation(valid_data_file, valid_csv_file_content, api_rf, user):
+    url = f'api/import_file/'
+    data = {
+        'file': valid_data_file,
+        'filename': 'test_file',
+    }
+
+    view = ImportFileViewSet.as_view({'post': 'create'})
+    request = api_rf.post(url, data=data)
+    force_authenticate(request, user)
+    response = view(request)
+
+    assert status.HTTP_201_CREATED == response.status_code
+    assert ImportFile.objects.filter(filename='test_file').exists()
+
+    import_file = ImportFile.objects.get(filename='test_file')
+    assert import_file.headers == list(valid_csv_file_content[0].keys())
+    assert import_file.content == valid_csv_file_content
+    assert str(import_file) == f'test_file ({import_file.date_added})'
