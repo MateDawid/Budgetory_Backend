3ef144f462ed29519b878ac8a4e6a46599c41fde
MateDawid <mateusiakdawid@gmail.com>
2023-07-13 20:09:01 +0200

    Test for empty csv file column validation error

--- tests/data_import/conftest.py
+++ tests/data_import/conftest.py
@@ -12,17 +12,34 @@ register(ImportFileFactory)
 
 
 @pytest.fixture
-def valid_csv_file_content() -> list:
+def valid_file_content() -> list:
     data = [{'column_1': '1', 'column_2': '1'}, {'column_1': '1', 'column_2': '1'}]
     return data
 
 
 @pytest.fixture
-def valid_data_file(valid_csv_file_content: list) -> InMemoryUploadedFile:
+def valid_data_file(valid_file_content: list) -> InMemoryUploadedFile:
     io_file = StringIO()
-    writer = csv.DictWriter(io_file, fieldnames=valid_csv_file_content[0].keys())
+    writer = csv.DictWriter(io_file, fieldnames=valid_file_content[0].keys())
     writer.writeheader()
-    writer.writerows(valid_csv_file_content)
+    writer.writerows(valid_file_content)
+    io_file.seek(0)
+    file = InMemoryUploadedFile(io_file, 'FileField', 'valid_file.csv', 'txt/csv', sys.getsizeof(io_file), None)
+    return file
+
+
+@pytest.fixture
+def invalid_headers_file_content() -> list:
+    data = [{'column_1': '1', 'column_2': '1', '': 'INVALID'}, {'column_1': '1', 'column_2': '1', '': 'INVALID'}]
+    return data
+
+
+@pytest.fixture
+def invalid_headers_data_file(invalid_headers_file_content: list) -> InMemoryUploadedFile:
+    io_file = StringIO()
+    writer = csv.DictWriter(io_file, fieldnames=invalid_headers_file_content[0].keys())
+    writer.writeheader()
+    writer.writerows(invalid_headers_file_content)
     io_file.seek(0)
     file = InMemoryUploadedFile(io_file, 'FileField', 'valid_file.csv', 'txt/csv', sys.getsizeof(io_file), None)
     return file
