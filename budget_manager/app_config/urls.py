d543191625a4d7a70a8a9244176ee7cec502e8be
MateDawid <mateusiakdawid@gmail.com>
2023-10-02 08:20:30 +0200

    Remove frontend part

--- budget_manager/app_config/urls.py
+++ budget_manager/app_config/urls.py
@@ -2,18 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 from rest_framework import routers
 
 router = routers.SimpleRouter()
 
-front_urls = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('data_import/', TemplateView.as_view(template_name='data_import.html'), name='data_import'),
-]
-
 urlpatterns = [
-    path('', include(front_urls)),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/auth/', include('rest_framework.urls')),
