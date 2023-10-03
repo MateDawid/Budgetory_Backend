f3f321e2e55e9f20cdf9f6ed081b94d34c826a02
MateDawid <mateusiakdawid@gmail.com>
2023-10-03 08:11:22 +0200

    Rename budget_manager to budget_api

--- budget_api/data_import/views.py
+++ budget_api/data_import/views.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from rest_framework.mixins import (
     CreateModelMixin,
     DestroyModelMixin,
     ListModelMixin,
     RetrieveModelMixin,
 )
-from rest_framework.parsers import MultiPartParser, JSONParser, FormParser
+from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 
 from .models import ImportFile
