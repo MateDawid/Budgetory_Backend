f697758dd552659c89cce5b73ec65aca16ec9306
MateDawid <mateusiakdawid@gmail.com>
2023-10-11 09:37:58 +0200

    Serializer and view for AuthToken

--- budget_api/app_users/serializers.py
+++ budget_api/app_users/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 
@@ -29,3 +30,25 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user auth token."""
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+        user = authenticate(request=self.context.get('request'), username=email, password=password)
+        if not user:
+            msg = _('Unable to authenticate user with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
--- budget_api/app_users/urls.py
+++ budget_api/app_users/urls.py
@@ -6,5 +6,6 @@ app_name = 'app_users'
 urlpatterns = [
     path('', views.ListUserView.as_view(), name='list'),
     path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
     path('me/', views.AuthenticatedUserView.as_view(), name='me'),
 ]
