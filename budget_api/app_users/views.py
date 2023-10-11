388f8af74a431ee5f862a4da02abaa2b95643d12
MateDawid <mateusiakdawid@gmail.com>
2023-10-11 09:37:58 +0200

    Serializer and view for AuthToken

--- budget_api/app_users/views.py
+++ budget_api/app_users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import authentication, generics, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import AuthTokenSerializer, UserSerializer
 
 
 class ListUserView(generics.ListAPIView):
@@ -9,7 +11,7 @@ class ListUserView(generics.ListAPIView):
 
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
-    authentication_classes = [authentication.BasicAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAdminUser]
 
 
@@ -19,11 +21,18 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
+class CreateTokenView(ObtainAuthToken):
+    """View to create new auth token for user."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
 class AuthenticatedUserView(generics.RetrieveUpdateAPIView):
     """View for managing authenticated user."""
 
     serializer_class = UserSerializer
-    authentication_classes = [authentication.BasicAuthentication]
+    authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
