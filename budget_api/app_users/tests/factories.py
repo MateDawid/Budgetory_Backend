900aab8791a53cedac15c6d21a2d8e38ee897b49
MateDawid <mateusiakdawid@gmail.com>
2023-10-12 09:40:28 +0200

    Fix UserFactory to match custom User model

--- budget_api/app_users/tests/factories.py
+++ budget_api/app_users/tests/factories.py
@@ -6,5 +6,5 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = get_user_model()
 
-    username = factory.Faker('user_name')
+    name = factory.Faker('user_name')
     email = factory.Faker('email')
