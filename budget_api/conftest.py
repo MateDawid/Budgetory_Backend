1820f08ef373f7b09359e545384024043afb9276
MateDawid <mateusiakdawid@gmail.com>
2023-10-14 13:45:19 +0200

    api_client in conftest

--- budget_api/conftest.py
+++ budget_api/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from app_users.tests.factories import UserFactory
 from pytest_django.lazy_django import skip_if_no_django
 from pytest_factoryboy import register
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient, APIRequestFactory
 
 register(UserFactory)
 
@@ -11,3 +11,9 @@ register(UserFactory)
 def api_rf() -> APIRequestFactory:
     skip_if_no_django()
     return APIRequestFactory()
+
+
+@pytest.fixture(scope='session')
+def api_client() -> APIClient:
+    skip_if_no_django()
+    return APIClient()
