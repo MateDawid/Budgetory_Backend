fa79f86c4bb2807248c17c0ca8c596166a6c3dfe
MateDawid <mateusiakdawid@gmail.com>
2023-10-16 12:16:09 +0200

    Tests for ListUserView

--- budget_api/app_users/tests/test_views.py
+++ budget_api/app_users/tests/test_views.py
@@ -1,5 +1,7 @@
 import pytest
+from app_users.serializers import UserSerializer
 from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
@@ -78,7 +80,34 @@ class TestCreateUserView:
 class TestListUserView:
     """Tests for ListUserView"""
 
-    pass
+    @pytest.mark.django_db
+    def test_getting_user_list_as_admin(
+        self, api_client: APIClient, superuser: AbstractBaseUser, base_user: AbstractBaseUser
+    ):
+        """Test successful collecting user list as admin user."""
+        api_client.force_authenticate(superuser)
+
+        response = api_client.get(LIST_USER_URL)
+
+        users = get_user_model().objects.all().order_by('id')
+        serializer = UserSerializer(users, many=True)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['results'] == serializer.data
+
+    @pytest.mark.django_db
+    def test_getting_user_list_as_base_user(self, api_client: APIClient, base_user: AbstractBaseUser):
+        """Test error on collecting user list as base user."""
+        api_client.force_authenticate(base_user)
+        response = api_client.get(LIST_USER_URL)
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert response.data['detail'] == 'You do not have permission to perform this action.'
+
+    def test_getting_user_list_not_authenticated(self, api_client: APIClient):
+        """Test error on collecting user list as unauthenticated user."""
+        response = api_client.get(LIST_USER_URL)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert str(response.data['detail']) == 'Authentication credentials were not provided.'
 
 
 class TestAuthenticatedUserView:
