da35ae4d26dc4064467ba7f45376cf2474107ca5
MateDawid <mateusiakdawid@gmail.com>
2023-10-17 10:56:22 +0200

    Add user ForeignKey to BudgetingPeriod model

--- budget_api/periods/models.py
+++ budget_api/periods/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -5,9 +6,10 @@ class BudgetingPeriod(models.Model):
     """Model for period in which budget will be calculated and reported."""
 
     name = models.CharField(max_length=128)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='budgeting_periods')
     date_start = models.DateField(null=False, blank=False)
     date_end = models.DateField(null=False, blank=False)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.name} ({self.user.email})'
