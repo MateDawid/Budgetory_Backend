201c660f127a958cfdede4fd993b74b313192d45
MateDawid <mateusiakdawid@gmail.com>
2023-10-19 10:17:00 +0200

    Custom admin view for BudgetingPeriod

--- budget_api/periods/admin.py
+++ budget_api/periods/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from periods.models import BudgetingPeriod
 
-admin.site.register(BudgetingPeriod)
+
+@admin.register(BudgetingPeriod)
+class BudgetingPeriodAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'date_start', 'date_end', 'is_active')
+    list_filter = ('is_active', 'user__email')
