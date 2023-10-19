45df8d63fc89afadaf3a2abcf3835d36a6840d93
MateDawid <mateusiakdawid@gmail.com>
2023-10-19 10:19:24 +0200

    Validating BudgetingPeriod data before saving in database

--- budget_api/periods/models.py
+++ budget_api/periods/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Q
 
 
 class BudgetingPeriod(models.Model):
@@ -12,4 +14,46 @@ class BudgetingPeriod(models.Model):
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
+        """String representation of BudgetingPeriod model instance."""
         return f'{self.name} ({self.user.email})'
+
+    def clean_is_active(self):
+        """Check if is_active field is valid. If is_active not given, pass it to default model validation."""
+        if self.is_active is None:
+            return
+        if self.is_active and self.user.budgeting_periods.filter(is_active=True).exclude(pk=self.pk).exists():
+            raise ValidationError('is_active: User already has active budgeting period.', code='active-invalid')
+
+    def clean_name(self):
+        """Check if name field is valid. If name not given, pass it to default model validation."""
+        if self.name is None:
+            return
+        if self.name and self.user.budgeting_periods.filter(name=self.name).exclude(pk=self.pk).exists():
+            raise ValidationError("name: User's budgeting period with given name already exists.", code='name-invalid')
+
+    def clean_dates(self):
+        """Check if date_start and date_end fields are valid. If date_start or date_end not given,
+        pass them to default model validation."""
+        if self.date_start is None or self.date_end is None:
+            return
+        if (
+            self.user.budgeting_periods.filter(
+                Q(date_start__lte=self.date_start, date_end__gte=self.date_start)
+                | Q(date_start__lte=self.date_end, date_end__gte=self.date_end)
+                | Q(date_start__gte=self.date_start, date_end__lte=self.date_end)
+            )
+            .exclude(pk=self.pk)
+            .exists()
+        ):
+            raise ValidationError(
+                "date_start: Budgeting period date range collides with other user's budgeting periods.",
+                code='period-range-invalid',
+            )
+        if self.date_start >= self.date_end:
+            raise ValidationError('start_date: Start date should be earlier than end date.', code='date-invalid')
+
+    def clean(self):
+        """Clean BudgetingPeriod input data before saving in database."""
+        self.clean_is_active()
+        self.clean_name()
+        self.clean_dates()
