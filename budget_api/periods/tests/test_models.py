473d5454a0064d52be5c1a1ebbd9529713a510c4
MateDawid <mateusiakdawid@gmail.com>
2023-10-21 19:59:26 +0200

    Test for is_active in BudgetingPeriod

--- budget_api/periods/tests/test_models.py
+++ budget_api/periods/tests/test_models.py
@@ -1,6 +1,7 @@
 import datetime
 
 import pytest
+from django.core.exceptions import ValidationError
 from django.db import DataError, IntegrityError
 from periods.models import BudgetingPeriod
 
@@ -9,7 +10,7 @@ from periods.models import BudgetingPeriod
 class TestBudgetingPeriodModel:
     """Tests for BudgetingPeriod model"""
 
-    payload = {
+    payload: dict = {
         'name': '2023_01',
         'date_start': datetime.date(day=1, month=1, year=2023),
         'date_end': datetime.date(day=31, month=1, year=2023),
@@ -35,9 +36,9 @@ class TestBudgetingPeriodModel:
         payload_1['user'] = user
         payload_2 = {
             'name': '2023_02',
+            'user': user,
             'date_start': datetime.date(day=1, month=2, year=2023),
             'date_end': datetime.date(day=28, month=2, year=2023),
-            'user': user,
         }
         budgeting_period_1 = BudgetingPeriod.objects.create(**payload_1)
         budgeting_period_2 = BudgetingPeriod.objects.create(**payload_2)
@@ -53,9 +54,11 @@ class TestBudgetingPeriodModel:
         """Test creating period with the same params by two different users."""
         payload_1 = self.payload.copy()
         payload_1['user'] = user_factory()
+        payload_1['is_active'] = True
 
         payload_2 = self.payload.copy()
         payload_2['user'] = user_factory()
+        payload_1['is_active'] = True
 
         BudgetingPeriod.objects.create(**payload_1)
         BudgetingPeriod.objects.create(**payload_2)
@@ -89,3 +92,34 @@ class TestBudgetingPeriodModel:
             BudgetingPeriod.objects.create(**payload)
         assert f'DETAIL:  Key (name, user_id)=({payload["name"]}, {user.id}) already exists.' in str(exc.value)
         assert BudgetingPeriod.objects.filter(user=user).count() == 1
+
+    def test_error_is_active_set_already(self, user):
+        """Test error on making period active, when another user period active already."""
+        payload_1 = self.payload.copy()
+        payload_1['user'] = user
+        payload_1['is_active'] = True
+        active_period = BudgetingPeriod.objects.create(**payload_1)
+
+        payload_2 = {
+            'name': '2023_02',
+            'user': user,
+            'date_start': datetime.date(day=1, month=2, year=2023),
+            'date_end': datetime.date(day=28, month=2, year=2023),
+            'is_active': True,
+        }
+        with pytest.raises(ValidationError) as exc:
+            BudgetingPeriod.objects.create(**payload_2)
+        assert str(exc.value) == "['is_active: User already has active budgeting period.']"
+        assert BudgetingPeriod.objects.filter(user=user).count() == 1
+        assert BudgetingPeriod.objects.filter(user=user).first() == active_period
+
+    @pytest.mark.django_db(transaction=True)
+    def test_error_is_active_none(self, user):
+        """Test error on creating period with is_active set to None."""
+        payload = self.payload.copy()
+        payload['user'] = user
+        payload['is_active'] = None
+        with pytest.raises(IntegrityError) as exc:
+            BudgetingPeriod.objects.create(**payload)
+        assert 'null value in column "is_active"' in str(exc.value)
+        assert not BudgetingPeriod.objects.filter(user=user).exists()
