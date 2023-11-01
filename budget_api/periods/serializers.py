7b373b77fc4623053b27f15d604d782696326422
MateDawid <mateusiakdawid@gmail.com>
2023-11-01 21:14:06 +0100

    Serializer for BudgetingPeriod

---
+++ budget_api/periods/serializers.py
@@ -0,0 +1,11 @@
+from periods.models import BudgetingPeriod
+from rest_framework import serializers
+
+
+class BudgetingPeriodSerializer(serializers.ModelSerializer):
+    """Serializer for BudgetingPeriod."""
+
+    class Meta:
+        model = BudgetingPeriod
+        fields = ['id', 'name', 'user', 'date_start', 'date_end', 'is_active']
+        read_only_fields = ['id', 'user']
