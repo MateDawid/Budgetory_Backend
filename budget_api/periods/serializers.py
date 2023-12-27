5781f9a5ef10f30ed0ebc04a050df577b6c3a106
MateDawid <mateusiakdawid@gmail.com>
2023-12-27 17:36:57 +0100

    Tests for dates in BudgetingPeriodSerializer

--- budget_api/periods/serializers.py
+++ budget_api/periods/serializers.py
@@ -1,4 +1,8 @@
+import datetime
+from typing import Union
+
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from periods.models import BudgetingPeriod
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -13,11 +17,32 @@ class BudgetingPeriodSerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
     def validate_is_active(self, value: bool) -> bool:
+        """Validates is_active param by checking if any other period is not marked as active already."""
         if value is True:
             user = get_user_model().objects.get(id=self.initial_data.get('user'))
             active_periods = user.budgeting_periods.filter(is_active=True).exclude(
                 pk=getattr(self.instance, 'pk', None)
             )
             if active_periods.exists():
-                raise ValidationError('User already has active budgeting period.', code='active-invalid')
+                raise ValidationError('User already has active budgeting period.')
         return value
+
+    def validate_date_start(self, date_start: datetime.date) -> Union[datetime.date, None]:
+        """Validates date_start and date_end by checking they are in proper order or if any colliding periods exist."""
+        date_end = self.initial_data['date_end']
+        if date_start is None or date_end is None:
+            return date_start
+        user = get_user_model().objects.get(id=self.initial_data['user'])
+        if date_start >= date_end:
+            raise ValidationError('Start date should be earlier than end date.')
+        if (
+            user.budgeting_periods.filter(
+                Q(date_start__lte=date_start, date_end__gte=date_start)
+                | Q(date_start__lte=date_end, date_end__gte=date_end)
+                | Q(date_start__gte=date_start, date_end__lte=date_end)
+            )
+            .exclude(pk=getattr(self.instance, 'pk', None))
+            .exists()
+        ):
+            raise ValidationError("Budgeting period date range collides with other user's budgeting periods.")
+        return date_start
