d48a890e8dbb3f7cf8fe375657effe1ae9a4124a
MateDawid <mateusiakdawid@gmail.com>
2023-12-27 15:49:38 +0100

    Test for BudgetingPeriod name already used

--- budget_api/periods/serializers.py
+++ budget_api/periods/serializers.py
@@ -8,3 +8,4 @@ class BudgetingPeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = BudgetingPeriod
         fields = ['id', 'user', 'name', 'date_start', 'date_end', 'is_active']
+        read_only_fields = ['id']
