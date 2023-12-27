2d9d313da578d4b3dc5353c2c1664803becadbd1
MateDawid <mateusiakdawid@gmail.com>
2023-12-27 17:36:57 +0100

    Tests for dates in BudgetingPeriodSerializer

--- budget_api/periods/tests/test_serializers.py
+++ budget_api/periods/tests/test_serializers.py
@@ -1,4 +1,5 @@
 from datetime import date
+from typing import Union
 
 import pytest
 from periods.models import BudgetingPeriod
@@ -236,3 +237,114 @@ class TestBudgetingPeriodSerializer:
         assert 'is_active' in exc.value.detail
         assert exc.value.detail['is_active'][0] == 'This field may not be null.'
         assert not BudgetingPeriod.objects.filter(user=user).exists()
+
+    @pytest.mark.parametrize('date_start, date_end', ((None, date.today()), (date.today(), None), (None, None)))
+    def test_error_date_not_set(self, user, date_start: Union[date, None], date_end: Union[date, None]):
+        """Test error on saving BudgetingPeriodSerializer with date_start or date_end set to None."""
+        payload = {
+            'name': '2023_01',
+            'user': user.id,
+            'date_start': date_start,
+            'date_end': date_end,
+        }
+
+        serializer = BudgetingPeriodSerializer(data=payload)
+
+        with pytest.raises(ValidationError) as exc:
+            serializer.is_valid(raise_exception=True)
+        assert 'date_start' in exc.value.detail or 'date_end' in exc.value.detail
+        assert (
+            exc.value.detail.get('date_start', [''])[0] == 'This field may not be null.'
+            or exc.value.detail.get('date_end', [''])[0] == 'This field may not be null.'
+        )
+        assert not BudgetingPeriod.objects.filter(user=user).exists()
+
+    def test_error_date_end_before_date_start(self, user):
+        """Test error on saving BudgetingPeriodSerializer with date_end earlier than date_start."""
+        payload = {
+            'name': '2023_01',
+            'user': user.id,
+            'date_start': date(2023, 5, 1),
+            'date_end': date(2023, 4, 30),
+        }
+
+        serializer = BudgetingPeriodSerializer(data=payload)
+
+        with pytest.raises(ValidationError) as exc:
+            serializer.is_valid(raise_exception=True)
+        assert 'date_start' in exc.value.detail
+        assert exc.value.detail['date_start'][0] == 'Start date should be earlier than end date.'
+        assert not BudgetingPeriod.objects.filter(user=user).exists()
+
+    @pytest.mark.parametrize(
+        'date_start, date_end',
+        (
+            # Date start before first existing period
+            (date(2023, 5, 1), date(2023, 6, 1)),
+            (date(2023, 5, 1), date(2023, 6, 15)),
+            (date(2023, 5, 1), date(2023, 6, 30)),
+            (date(2023, 5, 1), date(2023, 7, 1)),
+            (date(2023, 5, 1), date(2023, 7, 15)),
+            (date(2023, 5, 1), date(2023, 7, 31)),
+            (date(2023, 5, 1), date(2023, 8, 1)),
+            # Date start same as in first existing period
+            (date(2023, 6, 1), date(2023, 6, 15)),
+            (date(2023, 6, 1), date(2023, 6, 30)),
+            (date(2023, 6, 1), date(2023, 7, 1)),
+            (date(2023, 6, 1), date(2023, 7, 15)),
+            (date(2023, 6, 1), date(2023, 7, 31)),
+            (date(2023, 6, 1), date(2023, 8, 1)),
+            # Date start between first existing period daterange
+            (date(2023, 6, 15), date(2023, 6, 30)),
+            (date(2023, 6, 15), date(2023, 7, 1)),
+            (date(2023, 6, 15), date(2023, 7, 15)),
+            (date(2023, 6, 15), date(2023, 7, 31)),
+            (date(2023, 6, 15), date(2023, 8, 1)),
+            # Date start same as first existing period's end date
+            (date(2023, 6, 30), date(2023, 7, 1)),
+            (date(2023, 6, 30), date(2023, 7, 15)),
+            (date(2023, 6, 30), date(2023, 7, 31)),
+            (date(2023, 6, 30), date(2023, 8, 1)),
+            # Date start same as in second existing period
+            (date(2023, 7, 1), date(2023, 7, 15)),
+            (date(2023, 7, 1), date(2023, 7, 31)),
+            (date(2023, 7, 1), date(2023, 8, 1)),
+            # Date start between second existing period daterange
+            (date(2023, 7, 15), date(2023, 7, 31)),
+            # Date start same as second existing period's end date
+            (date(2023, 7, 31), date(2023, 8, 1)),
+        ),
+    )
+    def test_error_date_invalid(self, user, date_start: date, date_end: date):
+        """Test error on saving BudgetingPeriodSerializer with invalid dates."""
+        payload_1 = {
+            'name': '2023_06',
+            'user': user,
+            'date_start': date(2023, 6, 1),
+            'date_end': date(2023, 6, 30),
+        }
+        payload_2 = {
+            'name': '2023_07',
+            'user': user,
+            'date_start': date(2023, 7, 1),
+            'date_end': date(2023, 7, 31),
+        }
+        payload_invalid = {
+            'name': 'invalid',
+            'user': user.id,
+            'date_start': date_start,
+            'date_end': date_end,
+        }
+        BudgetingPeriod.objects.create(**payload_1)
+        BudgetingPeriod.objects.create(**payload_2)
+
+        serializer = BudgetingPeriodSerializer(data=payload_invalid)
+
+        with pytest.raises(ValidationError) as exc:
+            serializer.is_valid(raise_exception=True)
+        assert 'date_start' in exc.value.detail
+        assert (
+            exc.value.detail['date_start'][0]
+            == "Budgeting period date range collides with other user's budgeting periods."
+        )
+        assert BudgetingPeriod.objects.filter(user=user).count() == 2
