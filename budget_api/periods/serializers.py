cce0a076d569beb8b7261c5e15d8a7c223d45f61
MateDawid <mateusiakdawid@gmail.com>
2023-12-27 16:59:57 +0100

    Test for is_active in BudgetingPeriodSerializer

--- budget_api/periods/serializers.py
+++ budget_api/periods/serializers.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import get_user_model
 from periods.models import BudgetingPeriod
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class BudgetingPeriodSerializer(serializers.ModelSerializer):
@@ -9,3 +11,13 @@ class BudgetingPeriodSerializer(serializers.ModelSerializer):
         model = BudgetingPeriod
         fields = ['id', 'user', 'name', 'date_start', 'date_end', 'is_active']
         read_only_fields = ['id']
+
+    def validate_is_active(self, value: bool) -> bool:
+        if value is True:
+            user = get_user_model().objects.get(id=self.initial_data.get('user'))
+            active_periods = user.budgeting_periods.filter(is_active=True).exclude(
+                pk=getattr(self.instance, 'pk', None)
+            )
+            if active_periods.exists():
+                raise ValidationError('User already has active budgeting period.', code='active-invalid')
+        return value
