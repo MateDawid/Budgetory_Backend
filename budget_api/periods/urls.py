3d246347fdac3ff997412062207a1ee2fd05db35
MateDawid <mateusiakdawid@gmail.com>
2023-12-28 09:53:25 +0100

    Add app_name for periods urls

--- budget_api/periods/urls.py
+++ budget_api/periods/urls.py
@@ -1,6 +1,8 @@
 from periods.views import BudgetingPeriodViewSet
 from rest_framework import routers
 
+app_name = 'periods'
+
 router = routers.DefaultRouter()
 router.register(r'', BudgetingPeriodViewSet)
 
