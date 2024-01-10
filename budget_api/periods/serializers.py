9b14aaf1e3e6a956997d39b3cfe37bba4f796880
MateDawid <mateusiakdawid@gmail.com>
2024-01-10 10:00:57 +0100

    Save request user in BudgetingPeriod instead of passing its id

--- budget_api/periods/serializers.py
+++ budget_api/periods/serializers.py
@@ -1,7 +1,6 @@
 import datetime
 from typing import Union
 
-from django.contrib.auth import get_user_model
 from django.db.models import Q
 from periods.models import BudgetingPeriod
 from rest_framework import serializers
@@ -13,15 +12,16 @@ class BudgetingPeriodSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BudgetingPeriod
-        fields = ['id', 'user', 'name', 'date_start', 'date_end', 'is_active']
+        fields = ['id', 'name', 'date_start', 'date_end', 'is_active']
         read_only_fields = ['id']
 
     def validate_is_active(self, value: bool) -> bool:
         """Validates is_active param by checking if any other period is not marked as active already."""
         if value is True:
-            user = get_user_model().objects.get(id=self.initial_data.get('user'))
-            active_periods = user.budgeting_periods.filter(is_active=True).exclude(
-                pk=getattr(self.instance, 'pk', None)
+            active_periods = (
+                self.context['request']
+                .user.budgeting_periods.filter(is_active=True)
+                .exclude(pk=getattr(self.instance, 'pk', None))
             )
             if active_periods.exists():
                 raise ValidationError('User already has active budgeting period.')
@@ -33,11 +33,11 @@ class BudgetingPeriodSerializer(serializers.ModelSerializer):
         date_end = self._get_date(self.initial_data['date_end'])
         if date_start is None or date_end is None:
             return date_start
-        user = get_user_model().objects.get(id=self.initial_data['user'])
         if date_start >= date_end:
             raise ValidationError('Start date should be earlier than end date.')
         if (
-            user.budgeting_periods.filter(
+            self.context['request']
+            .user.budgeting_periods.filter(
                 Q(date_start__lte=date_start, date_end__gte=date_start)
                 | Q(date_start__lte=date_end, date_end__gte=date_end)
                 | Q(date_start__gte=date_start, date_end__lte=date_end)
