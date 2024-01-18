063be264621a2f3bc2e0a595d4e8b023427e99be
MateDawid <mateusiakdawid@gmail.com>
2024-01-18 09:55:10 +0100

    deposits: Deposit database model and migration

---
+++ budget_api/deposits/models.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.db import models
+
+
+class Deposit(models.Model):
+    """Deposit model where revenues are collected and from which payments are made."""
+
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='deposits')
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = (
+            'name',
+            'user',
+        )
