5ef80f673c53fd3d5e60b4eb4243d12ec488b0c5
MateDawid <mateusiakdawid@gmail.com>
2024-01-19 13:36:37 +0100

    deposits: Blank description in Deposit possible

--- budget_api/deposits/models.py
+++ budget_api/deposits/models.py
@@ -6,7 +6,7 @@ class Deposit(models.Model):
     """Deposit model where revenues are collected and from which payments are made."""
 
     name = models.CharField(max_length=128)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='deposits')
     is_active = models.BooleanField(default=False)
 
