856443d79a3be942de641c7f304f483698b589a4
MateDawid <mateusiakdawid@gmail.com>
2024-02-08 09:59:46 +0100

    deposits: Tests for Deposit create

--- budget_api/deposits/tests/test_api.py
+++ budget_api/deposits/tests/test_api.py
@@ -55,272 +55,105 @@ class TestDepositApi:
         assert response.status_code == status.HTTP_200_OK
         assert response.data['results'] == serializer.data
 
-    # def test_create_single_deposit(self, api_client: APIClient, base_user: Any):
-    #     """Test creating single Deposit."""
-    #     api_client.force_authenticate(base_user)
-    #     payload = {'name': 'My account', 'description': 'Account that I use.'}
-    #
-    #     response = api_client.post(DEPOSITS_URL, payload)
-    #
-    #     assert response.status_code == status.HTTP_201_CREATED
-    #     assert Deposit.objects.filter(user=base_user).count() == 1
-    #     period = BudgetingPeriod.objects.get(id=response.data['id'])
-    #     for key in payload:
-    #         assert getattr(period, key) == payload[key]
-    #     serializer = BudgetingPeriodSerializer(period)
-    #     assert response.data == serializer.data
-    #
-    # def test_create_two_periods_by_one_user(self, api_client: APIClient, base_user: Any):
-    #     """Test creating two valid BudgetingPeriods by single user."""
-    #     api_client.force_authenticate(base_user)
-    #     payload_1 = {
-    #         'name': '2023_01',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 31),
-    #     }
-    #     payload_2 = {
-    #         'name': '2023_02',
-    #         'date_start': date(2023, 2, 1),
-    #         'date_end': date(2023, 2, 28),
-    #     }
-    #
-    #     response_1 = api_client.post(PERIODS_URL, payload_1)
-    #     response_2 = api_client.post(PERIODS_URL, payload_2)
-    #
-    #     assert response_1.status_code == status.HTTP_201_CREATED
-    #     assert response_2.status_code == status.HTTP_201_CREATED
-    #     assert BudgetingPeriod.objects.filter(user=base_user).count() == 2
-    #     for response, payload in [(response_1, payload_1), (response_2, payload_2)]:
-    #         period = BudgetingPeriod.objects.get(id=response.data['id'])
-    #         for key in payload:
-    #             assert getattr(period, key) == payload[key]
-    #
-    # def test_create_same_period_by_two_users(self, api_client: APIClient, user_factory: Any):
-    #     """Test creating period with the same params by two users."""
-    #     payload = {
-    #         'name': '2023_01',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 31),
-    #     }
-    #     user_1 = user_factory()
-    #     api_client.force_authenticate(user_1)
-    #     api_client.post(PERIODS_URL, payload)
-    #
-    #     user_2 = user_factory()
-    #     api_client.force_authenticate(user_2)
-    #     api_client.post(PERIODS_URL, payload)
-    #
-    #     assert BudgetingPeriod.objects.all().count() == 2
-    #     assert BudgetingPeriod.objects.filter(user=user_1).count() == 1
-    #     assert BudgetingPeriod.objects.filter(user=user_2).count() == 1
-    #
-    # def test_error_name_too_long(self, api_client: APIClient, base_user: Any):
-    #     """Test error on creating BudgetingPeriod with name too long."""
-    #     api_client.force_authenticate(base_user)
-    #     max_length = BudgetingPeriod._meta.get_field('name').max_length
-    #     payload = {
-    #         'name': (max_length + 1) * 'a',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 31),
-    #     }
-    #
-    #     response = api_client.post(PERIODS_URL, payload)
-    #
-    #     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    #     assert 'name' in response.data
-    #     assert response.data['name'][0] == f'Ensure this field has no more than {max_length} characters.'
-    #     assert not BudgetingPeriod.objects.filter(user=base_user).exists()
-    #
-    # def test_error_name_already_used(self, api_client: APIClient, base_user: Any):
-    #     """Test error on creating BudgetingPeriod with already used name by the same user."""
-    #     api_client.force_authenticate(base_user)
-    #     payload = {
-    #         'name': '2023_01',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 2),
-    #     }
-    #     BudgetingPeriod.objects.create(user=base_user, **payload)
-    #     payload['date_start'] = date(2023, 1, 3)
-    #     payload['date_end'] = date(2023, 1, 4)
-    #
-    #     response = api_client.post(PERIODS_URL, payload)
-    #
-    #     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    #     assert 'name' in response.data
-    #     assert response.data['name'][0] == f"Users period with name {payload['name']} already exists."
-    #     assert BudgetingPeriod.objects.filter(user=base_user).count() == 1
-    #
-    # def test_create_active_period_successfully(self, api_client: APIClient, base_user: Any):
-    #     """Test creating BudgetingPeriod with is_active=True successfully."""
-    #     api_client.force_authenticate(base_user)
-    #     payload_inactive = {
-    #         'name': '2023_01',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 31),
-    #         'is_active': False,
-    #     }
-    #     payload_active = {
-    #         'name': '2023_02',
-    #         'date_start': date(2023, 2, 1),
-    #         'date_end': date(2023, 2, 28),
-    #         'is_active': True,
-    #     }
-    #
-    #     response_inactive = api_client.post(PERIODS_URL, payload_inactive)
-    #     response_active = api_client.post(PERIODS_URL, payload_active)
-    #
-    #     assert BudgetingPeriod.objects.all().count() == 2
-    #     assert BudgetingPeriod.objects.filter(user=base_user).count() == 2
-    #     for response, payload in [(response_inactive, payload_inactive), (response_active, payload_active)]:
-    #         assert response.status_code == status.HTTP_201_CREATED
-    #         period = BudgetingPeriod.objects.get(id=response.data['id'])
-    #         assert period.is_active == payload['is_active']
-    #
-    # def test_error_create_period_when_is_active_set_already(self, api_client: APIClient, base_user: Any):
-    #     """Test error on creating new BudgetingPeriod with is_active=True,
-    #     when another user's period active already."""
-    #     api_client.force_authenticate(base_user)
-    #     payload_1 = {
-    #         'name': '2023_01',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 31),
-    #         'is_active': True,
-    #     }
-    #     active_period = BudgetingPeriod.objects.create(user=base_user, **payload_1)
-    #     payload_2 = {
-    #         'name': '2023_02',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 31),
-    #         'is_active': True,
-    #     }
-    #
-    #     response = api_client.post(PERIODS_URL, payload_2)
-    #
-    #     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    #     assert 'is_active' in response.data
-    #     assert response.data['is_active'][0] == 'User already has active budgeting period.'
-    #     assert BudgetingPeriod.objects.filter(user=base_user).count() == 1
-    #     assert BudgetingPeriod.objects.filter(user=base_user).first() == active_period
-    #
-    # def test_is_active_default_value(self, api_client: APIClient, base_user: Any):
-    #     """Test creating BudgetingPeriod without passing is_active ends with default False value."""
-    #     api_client.force_authenticate(base_user)
-    #     payload = {
-    #         'name': '2023_02',
-    #         'date_start': date(2023, 1, 1),
-    #         'date_end': date(2023, 1, 31),
-    #         'is_active': '',
-    #     }
-    #
-    #     response = api_client.post(PERIODS_URL, payload)
-    #
-    #     assert response.status_code == status.HTTP_201_CREATED
-    #     assert BudgetingPeriod.objects.all().count() == 1
-    #     assert BudgetingPeriod.objects.filter(user=base_user).count() == 1
-    #     assert response.data['is_active'] is False
-    #
-    # @pytest.mark.parametrize('date_start, date_end', (('', date.today()), (date.today(), ''), ('', '')))
-    # def test_error_date_blank(
-    #     self, api_client: APIClient, base_user: Any, date_start: Optional[date], date_end: Optional[date]
-    # ):
-    #     """Test error on creating BudgetingPeriod with date_start or date_end blank."""
-    #     api_client.force_authenticate(base_user)
-    #     payload = {'name': '2023_01', 'date_start': date_start, 'date_end': date_end, 'is_active': False}
-    #     error_message = 'Date has wrong format. Use one of these formats instead: YYYY-MM-DD.'
-    #
-    #     response = api_client.post(PERIODS_URL, payload)
-    #
-    #     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    #     assert 'date_start' in response.data or 'date_end' in response.data
-    #     assert (
-    #         response.data.get('date_start', [''])[0] == error_message
-    #         or response.data.get('date_end', [''])[0] == error_message
-    #     )
-    #     assert not BudgetingPeriod.objects.filter(user=base_user).exists()
-    #
-    # def test_error_date_end_before_date_start(self, api_client: APIClient, base_user: Any):
-    #     """Test error on creating BudgetingPeriod with date_end earlier than date_start."""
-    #     api_client.force_authenticate(base_user)
-    #     payload = {'name': '2023_01', 'date_start': date(2023, 5, 1),
-    #     'date_end': date(2023, 4, 30), 'is_active': False}
-    #
-    #     response = api_client.post(PERIODS_URL, payload)
-    #
-    #     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    #     assert 'non_field_errors' in response.data
-    #     assert response.data['non_field_errors'][0] == 'Start date should be earlier than end date.'
-    #     assert not BudgetingPeriod.objects.filter(user=base_user).exists()
-    #
-    # @pytest.mark.parametrize(
-    #     'date_start, date_end',
-    #     (
-    #         # Date start before first existing period
-    #         (date(2023, 5, 1), date(2023, 6, 1)),
-    #         (date(2023, 5, 1), date(2023, 6, 15)),
-    #         (date(2023, 5, 1), date(2023, 6, 30)),
-    #         (date(2023, 5, 1), date(2023, 7, 1)),
-    #         (date(2023, 5, 1), date(2023, 7, 15)),
-    #         (date(2023, 5, 1), date(2023, 7, 31)),
-    #         (date(2023, 5, 1), date(2023, 8, 1)),
-    #         # Date start same as in first existing period
-    #         (date(2023, 6, 1), date(2023, 6, 15)),
-    #         (date(2023, 6, 1), date(2023, 6, 30)),
-    #         (date(2023, 6, 1), date(2023, 7, 1)),
-    #         (date(2023, 6, 1), date(2023, 7, 15)),
-    #         (date(2023, 6, 1), date(2023, 7, 31)),
-    #         (date(2023, 6, 1), date(2023, 8, 1)),
-    #         # Date start between first existing period daterange
-    #         (date(2023, 6, 15), date(2023, 6, 30)),
-    #         (date(2023, 6, 15), date(2023, 7, 1)),
-    #         (date(2023, 6, 15), date(2023, 7, 15)),
-    #         (date(2023, 6, 15), date(2023, 7, 31)),
-    #         (date(2023, 6, 15), date(2023, 8, 1)),
-    #         # Date start same as first existing period's end date
-    #         (date(2023, 6, 30), date(2023, 7, 1)),
-    #         (date(2023, 6, 30), date(2023, 7, 15)),
-    #         (date(2023, 6, 30), date(2023, 7, 31)),
-    #         (date(2023, 6, 30), date(2023, 8, 1)),
-    #         # Date start same as in second existing period
-    #         (date(2023, 7, 1), date(2023, 7, 15)),
-    #         (date(2023, 7, 1), date(2023, 7, 31)),
-    #         (date(2023, 7, 1), date(2023, 8, 1)),
-    #         # Date start between second existing period daterange
-    #         (date(2023, 7, 15), date(2023, 7, 31)),
-    #         # Date start same as second existing period's end date
-    #         (date(2023, 7, 31), date(2023, 8, 1)),
-    #     ),
-    # )
-    # def test_error_date_invalid(self, api_client: APIClient, base_user: Any, date_start: date, date_end: date):
-    #     """Test error on creating BudgetingPeriod with invalid dates."""
-    #     api_client.force_authenticate(base_user)
-    #     payload_1 = {
-    #         'name': '2023_06',
-    #         'date_start': date(2023, 6, 1),
-    #         'date_end': date(2023, 6, 30),
-    #     }
-    #     payload_2 = {
-    #         'name': '2023_07',
-    #         'date_start': date(2023, 7, 1),
-    #         'date_end': date(2023, 7, 31),
-    #     }
-    #     payload_invalid = {
-    #         'name': 'invalid',
-    #         'date_start': date_start,
-    #         'date_end': date_end,
-    #     }
-    #     BudgetingPeriod.objects.create(user=base_user, **payload_1)
-    #     BudgetingPeriod.objects.create(user=base_user, **payload_2)
-    #
-    #     response = api_client.post(PERIODS_URL, payload_invalid)
-    #
-    #     assert response.status_code == status.HTTP_400_BAD_REQUEST
-    #     assert 'non_field_errors' in response.data
-    #     assert (
-    #         response.data['non_field_errors'][0]
-    #         == "Budgeting period date range collides with other user's budgeting periods."
-    #     )
-    #     assert BudgetingPeriod.objects.filter(user=base_user).count() == 2
-    #
+    def test_create_single_deposit(self, api_client: APIClient, base_user: Any):
+        """Test creating single Deposit."""
+        api_client.force_authenticate(base_user)
+        payload = {'name': 'My account', 'description': 'Account that I use.', 'is_active': True}
+
+        response = api_client.post(DEPOSITS_URL, payload)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Deposit.objects.filter(user=base_user).count() == 1
+        deposit = Deposit.objects.get(id=response.data['id'])
+        for key in payload:
+            assert getattr(deposit, key) == payload[key]
+        serializer = DepositSerializer(deposit)
+        assert response.data == serializer.data
+
+    def test_create_two_deposits_by_one_user(self, api_client: APIClient, base_user: Any):
+        """Test creating two valid Deposits by single user."""
+        api_client.force_authenticate(base_user)
+        payload_1 = {'name': 'My account', 'description': 'Account that I use.', 'is_active': True}
+        payload_2 = {'name': 'Old account', 'description': 'Not used account.', 'is_active': False}
+
+        response_1 = api_client.post(DEPOSITS_URL, payload_1)
+        response_2 = api_client.post(DEPOSITS_URL, payload_2)
+
+        assert response_1.status_code == status.HTTP_201_CREATED
+        assert response_2.status_code == status.HTTP_201_CREATED
+        assert Deposit.objects.filter(user=base_user).count() == 2
+        for response, payload in [(response_1, payload_1), (response_2, payload_2)]:
+            period = Deposit.objects.get(id=response.data['id'])
+            for key in payload:
+                assert getattr(period, key) == payload[key]
+
+    def test_create_same_deposit_by_two_users(self, api_client: APIClient, user_factory: Any):
+        """Test creating deposit with the same params by two users."""
+        payload = {'name': 'My account', 'description': 'Account that I use.', 'is_active': True}
+        user_1 = user_factory()
+        api_client.force_authenticate(user_1)
+        api_client.post(DEPOSITS_URL, payload)
+
+        user_2 = user_factory()
+        api_client.force_authenticate(user_2)
+        api_client.post(DEPOSITS_URL, payload)
+
+        assert Deposit.objects.all().count() == 2
+        assert Deposit.objects.filter(user=user_1).count() == 1
+        assert Deposit.objects.filter(user=user_2).count() == 1
+
+    def test_error_name_too_long(self, api_client: APIClient, base_user: Any):
+        """Test error on creating Deposit with name too long."""
+        api_client.force_authenticate(base_user)
+        max_length = Deposit._meta.get_field('name').max_length
+        payload = {'name': (max_length + 1) * 'a', 'description': 'Account that I use.', 'is_active': True}
+
+        response = api_client.post(DEPOSITS_URL, payload)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'name' in response.data
+        assert response.data['name'][0] == f'Ensure this field has no more than {max_length} characters.'
+        assert not Deposit.objects.filter(user=base_user).exists()
+
+    def test_error_name_already_used(self, api_client: APIClient, base_user: Any):
+        """Test error on creating Deposit with already used name by the same user."""
+        api_client.force_authenticate(base_user)
+        payload = {'name': 'My account', 'description': 'Account that I use.', 'is_active': True}
+        Deposit.objects.create(user=base_user, **payload)
+
+        response = api_client.post(DEPOSITS_URL, payload)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'name' in response.data
+        assert response.data['name'][0] == f"Users deposit with name {payload['name']} already exists."
+        assert Deposit.objects.filter(user=base_user).count() == 1
+
+    def test_error_description_too_long(self, api_client: APIClient, base_user: Any):
+        """Test error on creating Deposit with description too long."""
+        api_client.force_authenticate(base_user)
+        max_length = Deposit._meta.get_field('description').max_length
+        payload = {'name': 'My account', 'description': (max_length + 1) * 'a', 'is_active': True}
+
+        response = api_client.post(DEPOSITS_URL, payload)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'name' in response.data
+        assert response.data['name'][0] == f'Ensure this field has no more than {max_length} characters.'
+        assert not Deposit.objects.filter(user=base_user).exists()
+
+    def test_is_active_default_value(self, api_client: APIClient, base_user: Any):
+        """Test creating Deposit without passing is_active ends with default value."""
+        api_client.force_authenticate(base_user)
+        default = Deposit._meta.get_field('is_active').default
+        payload = {'name': 'My account', 'description': 'Account that I use.'}
+
+        response = api_client.post(DEPOSITS_URL, payload)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Deposit.objects.all().count() == 1
+        assert Deposit.objects.filter(user=base_user).count() == 1
+        assert response.data['is_active'] == default
+
     # def test_get_period_details(
     #     self, api_client: APIClient, base_user: Any, budgeting_period_factory: FactoryMetaClass
     # ):
