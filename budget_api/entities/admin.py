320f21a2e03e4eee9a9a5911cd84518c79ad7cfe
MateDawid <mateusiakdawid@gmail.com>
2024-02-13 19:46:46 +0100

    entity: Admin site for Entities

---
+++ budget_api/entities/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from entities.models import Entity
+
+
+@admin.register(Entity)
+class EntityAdmin(admin.ModelAdmin):
+    """Custom admin view for Deposit model."""
+
+    list_display = ('name', 'description', 'is_personal', 'user')
+    list_filter = ('is_personal',)
