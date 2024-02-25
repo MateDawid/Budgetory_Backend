4b99892c38afd52f8b343f2bb3cbf047a7c2857d
MateDawid <mateusiakdawid@gmail.com>
2024-02-25 15:21:55 +0100

    transfer_category: TransferCategory name and user validation based on scope param

--- budget_api/transfers/models.py
+++ budget_api/transfers/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -51,3 +52,44 @@ class TransferCategory(models.Model):
             str: Custom string representation of instance.
         """
         return self.name
+
+    def save(self, *args, **kwargs):
+        """Override save method to clean data before saving model in database."""
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        """Clean TransferCategory input data before saving in database."""
+        self.clean_user()
+        self.clean_name()
+
+    def clean_name(self):
+        """
+        Check if TransferCategory name is unique in global scope for global TransferCategory
+        or in user scope for personal TransferCategory.
+        """
+        if (
+            self.scope == self.PERSONAL
+            and self.user.personal_transfer_categories.filter(name__iexact=self.name).exclude(id=self.id).exists()
+        ):
+            raise ValidationError(
+                'name: Personal transfer category with given name already exists.', code='personal-name-invalid'
+            )
+        elif (
+            self.scope == self.GLOBAL
+            and TransferCategory.global_transfer_categories.filter(name__iexact=self.name).exclude(id=self.id).exists()
+        ):
+            raise ValidationError(
+                'name: Global transfer category with given name already exists.', code='global-name-invalid'
+            )
+
+    def clean_user(self):
+        """Check if user field is filled only when type is "PERSONAL"."""
+        if self.scope == self.PERSONAL and self.user is None:
+            raise ValidationError(
+                'user: User was not provided for personal transfer category.', code='no-user-for-personal'
+            )
+        if self.scope == self.GLOBAL and self.user is not None:
+            raise ValidationError(
+                'user: User can be provided only for personal transfer category.', code='user-when-global'
+            )
