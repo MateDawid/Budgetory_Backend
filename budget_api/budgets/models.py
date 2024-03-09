944bf180e6a726eb93ba8699c57361632fdb5b80
MateDawid <mateusiakdawid@gmail.com>
2024-03-09 17:31:51 +0100

    budget: Budget model

--- budget_api/budgets/models.py
+++ budget_api/budgets/models.py
@@ -4,10 +4,31 @@ from django.db import models
 from django.db.models import Q
 
 
+class Budget(models.Model):
+    """Model for object gathering all data like incomes, expenses and predictions for particular budget."""
+
+    name = models.CharField(max_length=128)
+    description = models.TextField(null=True, blank=True, max_length=300)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='owned_budgets')
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='joined_budgets', blank=True)
+    currency = models.CharField(max_length=3)
+
+    class Meta:
+        unique_together = (
+            'name',
+            'owner',
+        )
+
+    def __str__(self):
+        """String representation of BudgetingPeriod model instance."""
+        return f'{self.name} ({self.owner.email})'
+
+
 class BudgetingPeriod(models.Model):
     """Model for period in which budget will be calculated and reported."""
 
     name = models.CharField(max_length=128)
+    # TODO - replace user with budget
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='budgeting_periods')
     date_start = models.DateField(null=False, blank=False)
     date_end = models.DateField(null=False, blank=False)
