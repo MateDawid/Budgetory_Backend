8b5acf9e63dbf3167805bc5a1d71719ae3977631
MateDawid <mateusiakdawid@gmail.com>
2024-03-19 20:45:01 +0100

    budget: Budget detail url tests

--- budget_api/budgets/tests/test_api.py
+++ budget_api/budgets/tests/test_api.py
@@ -197,237 +197,57 @@ class TestBudgetApi:
         assert response.data['name'][0] == f'Ensure this field has no more than {max_length} characters.'
         assert not BudgetingPeriod.objects.filter(user=base_user).exists()
 
+    def test_error_name_already_used(self, api_client: APIClient, base_user: AbstractUser):
+        """Test error on creating BudgetingPeriod with already used name by the same user."""
+        api_client.force_authenticate(base_user)
+        payload = {'name': 'Budget', 'description': 'Some budget', 'currency': 'eur'}
+        Budget.objects.create(owner=base_user, **payload)
 
-#     def test_error_name_already_used(self, api_client: APIClient, base_user: AbstractUser):
-#         """Test error on creating BudgetingPeriod with already used name by the same user."""
-#         api_client.force_authenticate(base_user)
-#         payload = {
-#             'name': '2023_01',
-#             'date_start': date(2023, 1, 1),
-#             'date_end': date(2023, 1, 2),
-#         }
-#         BudgetingPeriod.objects.create(user=base_user, **payload)
-#         payload['date_start'] = date(2023, 1, 3)
-#         payload['date_end'] = date(2023, 1, 4)
-#
-#         response = api_client.post(PERIODS_URL, payload)
-#
-#         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert 'name' in response.data
-#         assert response.data['name'][0] == f"Users period with name {payload['name']} already exists."
-#         assert BudgetingPeriod.objects.filter(user=base_user).count() == 1
-#
-#     def test_create_active_period_successfully(self, api_client: APIClient, base_user: AbstractUser):
-#         """Test creating BudgetingPeriod with is_active=True successfully."""
-#         api_client.force_authenticate(base_user)
-#         payload_inactive = {
-#             'name': '2023_01',
-#             'date_start': date(2023, 1, 1),
-#             'date_end': date(2023, 1, 31),
-#             'is_active': False,
-#         }
-#         payload_active = {
-#             'name': '2023_02',
-#             'date_start': date(2023, 2, 1),
-#             'date_end': date(2023, 2, 28),
-#             'is_active': True,
-#         }
-#
-#         response_inactive = api_client.post(PERIODS_URL, payload_inactive)
-#         response_active = api_client.post(PERIODS_URL, payload_active)
-#
-#         assert BudgetingPeriod.objects.all().count() == 2
-#         assert BudgetingPeriod.objects.filter(user=base_user).count() == 2
-#         for response, payload in [(response_inactive, payload_inactive), (response_active, payload_active)]:
-#             assert response.status_code == status.HTTP_201_CREATED
-#             period = BudgetingPeriod.objects.get(id=response.data['id'])
-#             assert period.is_active == payload['is_active']
-#
-#     def test_error_create_period_when_is_active_set_already(self, api_client: APIClient, base_user: AbstractUser):
-#         """Test error on creating new BudgetingPeriod with is_active=True,
-#         when another user's period active already."""
-#         api_client.force_authenticate(base_user)
-#         payload_1 = {
-#             'name': '2023_01',
-#             'date_start': date(2023, 1, 1),
-#             'date_end': date(2023, 1, 31),
-#             'is_active': True,
-#         }
-#         active_period = BudgetingPeriod.objects.create(user=base_user, **payload_1)
-#         payload_2 = {
-#             'name': '2023_02',
-#             'date_start': date(2023, 1, 1),
-#             'date_end': date(2023, 1, 31),
-#             'is_active': True,
-#         }
-#
-#         response = api_client.post(PERIODS_URL, payload_2)
-#
-#         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert 'is_active' in response.data
-#         assert response.data['is_active'][0] == 'User already has active budgeting period.'
-#         assert BudgetingPeriod.objects.filter(user=base_user).count() == 1
-#         assert BudgetingPeriod.objects.filter(user=base_user).first() == active_period
-#
-#     def test_is_active_default_value(self, api_client: APIClient, base_user: AbstractUser):
-#         """Test creating BudgetingPeriod without passing is_active ends with default False value."""
-#         api_client.force_authenticate(base_user)
-#         payload = {
-#             'name': '2023_02',
-#             'date_start': date(2023, 1, 1),
-#             'date_end': date(2023, 1, 31),
-#             'is_active': '',
-#         }
-#
-#         response = api_client.post(PERIODS_URL, payload)
-#
-#         assert response.status_code == status.HTTP_201_CREATED
-#         assert BudgetingPeriod.objects.all().count() == 1
-#         assert BudgetingPeriod.objects.filter(user=base_user).count() == 1
-#         assert response.data['is_active'] is False
-#
-#     @pytest.mark.parametrize('date_start, date_end', (('', date.today()), (date.today(), ''), ('', '')))
-#     def test_error_date_blank(
-#         self, api_client: APIClient, base_user: AbstractUser, date_start: Optional[date], date_end: Optional[date]
-#     ):
-#         """Test error on creating BudgetingPeriod with date_start or date_end blank."""
-#         api_client.force_authenticate(base_user)
-#         payload = {'name': '2023_01', 'date_start': date_start, 'date_end': date_end, 'is_active': False}
-#         error_message = 'Date has wrong format. Use one of these formats instead: YYYY-MM-DD.'
-#
-#         response = api_client.post(PERIODS_URL, payload)
-#
-#         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert 'date_start' in response.data or 'date_end' in response.data
-#         assert (
-#             response.data.get('date_start', [''])[0] == error_message
-#             or response.data.get('date_end', [''])[0] == error_message
-#         )
-#         assert not BudgetingPeriod.objects.filter(user=base_user).exists()
-#
-#     def test_error_date_end_before_date_start(self, api_client: APIClient, base_user: AbstractUser):
-#         """Test error on creating BudgetingPeriod with date_end earlier than date_start."""
-#         api_client.force_authenticate(base_user)
-#         payload = {'name': '2023_01', 'date_start': date(2023, 5, 1), 'date_end': date(2023, 4, 30),
-#         'is_active': False}
-#
-#         response = api_client.post(PERIODS_URL, payload)
-#
-#         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert 'non_field_errors' in response.data
-#         assert response.data['non_field_errors'][0] == 'Start date should be earlier than end date.'
-#         assert not BudgetingPeriod.objects.filter(user=base_user).exists()
-#
-#     @pytest.mark.parametrize(
-#         'date_start, date_end',
-#         (
-#             # Date start before first existing period
-#             (date(2023, 5, 1), date(2023, 6, 1)),
-#             (date(2023, 5, 1), date(2023, 6, 15)),
-#             (date(2023, 5, 1), date(2023, 6, 30)),
-#             (date(2023, 5, 1), date(2023, 7, 1)),
-#             (date(2023, 5, 1), date(2023, 7, 15)),
-#             (date(2023, 5, 1), date(2023, 7, 31)),
-#             (date(2023, 5, 1), date(2023, 8, 1)),
-#             # Date start same as in first existing period
-#             (date(2023, 6, 1), date(2023, 6, 15)),
-#             (date(2023, 6, 1), date(2023, 6, 30)),
-#             (date(2023, 6, 1), date(2023, 7, 1)),
-#             (date(2023, 6, 1), date(2023, 7, 15)),
-#             (date(2023, 6, 1), date(2023, 7, 31)),
-#             (date(2023, 6, 1), date(2023, 8, 1)),
-#             # Date start between first existing period daterange
-#             (date(2023, 6, 15), date(2023, 6, 30)),
-#             (date(2023, 6, 15), date(2023, 7, 1)),
-#             (date(2023, 6, 15), date(2023, 7, 15)),
-#             (date(2023, 6, 15), date(2023, 7, 31)),
-#             (date(2023, 6, 15), date(2023, 8, 1)),
-#             # Date start same as first existing period's end date
-#             (date(2023, 6, 30), date(2023, 7, 1)),
-#             (date(2023, 6, 30), date(2023, 7, 15)),
-#             (date(2023, 6, 30), date(2023, 7, 31)),
-#             (date(2023, 6, 30), date(2023, 8, 1)),
-#             # Date start same as in second existing period
-#             (date(2023, 7, 1), date(2023, 7, 15)),
-#             (date(2023, 7, 1), date(2023, 7, 31)),
-#             (date(2023, 7, 1), date(2023, 8, 1)),
-#             # Date start between second existing period daterange
-#             (date(2023, 7, 15), date(2023, 7, 31)),
-#             # Date start same as second existing period's end date
-#             (date(2023, 7, 31), date(2023, 8, 1)),
-#         ),
-#     )
-#     def test_error_date_invalid(self, api_client: APIClient, base_user: AbstractUser,
-#     date_start: date, date_end: date):
-#         """Test error on creating BudgetingPeriod with invalid dates."""
-#         api_client.force_authenticate(base_user)
-#         payload_1 = {
-#             'name': '2023_06',
-#             'date_start': date(2023, 6, 1),
-#             'date_end': date(2023, 6, 30),
-#         }
-#         payload_2 = {
-#             'name': '2023_07',
-#             'date_start': date(2023, 7, 1),
-#             'date_end': date(2023, 7, 31),
-#         }
-#         payload_invalid = {
-#             'name': 'invalid',
-#             'date_start': date_start,
-#             'date_end': date_end,
-#         }
-#         BudgetingPeriod.objects.create(user=base_user, **payload_1)
-#         BudgetingPeriod.objects.create(user=base_user, **payload_2)
-#
-#         response = api_client.post(PERIODS_URL, payload_invalid)
-#
-#         assert response.status_code == status.HTTP_400_BAD_REQUEST
-#         assert 'non_field_errors' in response.data
-#         assert (
-#             response.data['non_field_errors'][0]
-#             == "Budgeting period date range collides with other user's budgeting periods."
-#         )
-#         assert BudgetingPeriod.objects.filter(user=base_user).count() == 2
-#
-#     def test_get_period_details(
-#         self, api_client: APIClient, base_user: AbstractUser, budgeting_period_factory: FactoryMetaClass
-#     ):
-#         """Test get BudgetingPeriod details."""
-#         api_client.force_authenticate(base_user)
-#         period = budgeting_period_factory(user=base_user)
-#         url = period_detail_url(period.id)
-#
-#         response = api_client.get(url)
-#         serializer = BudgetingPeriodSerializer(period)
-#
-#         assert response.status_code == status.HTTP_200_OK
-#         assert response.data == serializer.data
-#
-#     def test_error_get_period_details_unauthenticated(
-#         self, api_client: APIClient, base_user: AbstractUser, budgeting_period_factory: FactoryMetaClass
-#     ):
-#         """Test error on getting BudgetingPeriod details being unauthenticated."""
-#         period = budgeting_period_factory(user=base_user)
-#         url = period_detail_url(period.id)
-#
-#         response = api_client.get(url)
-#
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-#
-#     def test_error_get_other_user_period_details(
-#         self, api_client: APIClient, user_factory: FactoryMetaClass, budgeting_period_factory: FactoryMetaClass
-#     ):
-#         """Test error on getting other user's BudgetingPeriod details."""
-#         user_1 = user_factory()
-#         user_2 = user_factory()
-#         period = budgeting_period_factory(user=user_1)
-#         api_client.force_authenticate(user_2)
-#
-#         url = period_detail_url(period.id)
-#         response = api_client.get(url)
-#
-#         assert response.status_code == status.HTTP_404_NOT_FOUND
-#
+        response = api_client.post(BUDGETS_URL, payload)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'name' in response.data
+        assert response.data['name'][0] == f'User already owns Budget with name "{payload["name"]}".'
+        assert Budget.objects.filter(owner=base_user).count() == 1
+
+    def test_get_budget_details(self, api_client: APIClient, base_user: AbstractUser, budget_factory: FactoryMetaClass):
+        """Test get Budget details."""
+        api_client.force_authenticate(base_user)
+        budget = budget_factory(owner=base_user)
+        url = budget_detail_url(budget.id)
+
+        response = api_client.get(url)
+        serializer = BudgetSerializer(budget)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == serializer.data
+
+    def test_error_get_budget_details_unauthenticated(
+        self, api_client: APIClient, base_user: AbstractUser, budget_factory: FactoryMetaClass
+    ):
+        """Test error on getting Budget details being unauthenticated."""
+        budget = budget_factory(owner=base_user)
+        url = budget_detail_url(budget.id)
+
+        response = api_client.get(url)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_error_get_other_user_budget_details(
+        self, api_client: APIClient, user_factory: FactoryMetaClass, budget_factory: FactoryMetaClass
+    ):
+        """Test error on getting other user's Budget details."""
+        user_1 = user_factory()
+        user_2 = user_factory()
+        budget = budget_factory(owner=user_1)
+        api_client.force_authenticate(user_2)
+
+        url = budget_detail_url(budget.id)
+        response = api_client.get(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
 #     @pytest.mark.parametrize(
 #         'param, value', [('date_start', date(2024, 1, 2)), ('date_end', date(2024, 1, 30)), ('is_active', True)]
 #     )
