c6045ad921f5a4e61b1fa59795f0c6e0a025badb
MateDawid <mateusiakdawid@gmail.com>
2024-03-24 09:46:16 +0100

    period_refactor: Tests for BudgetingPeriod model updated

--- budget_api/budgets/tests/test_models.py
+++ budget_api/budgets/tests/test_models.py
@@ -1,5 +1,4 @@
 from datetime import date
-from typing import Union
 
 import pytest
 from budgets.models import Budget, BudgetingPeriod
@@ -114,61 +113,69 @@ class TestBudgetModel:
 class TestBudgetingPeriodModel:
     """Tests for BudgetingPeriod model"""
 
-    def test_create_first_period_successful(self, user):
-        """Test creating first user BudgetingPeriod successfully."""
+    def test_create_first_period_successful(self, budget: Budget):
+        """
+        GIVEN: Budget model instance in database.
+        WHEN: BudgetingPeriod instance create attempt with valid data.
+        THEN: BudgetingPeriod model instance exists in database with given data.
+        """
         payload = {
             'name': '2023_01',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
         }
 
-        budgeting_period = BudgetingPeriod.objects.create(**payload)
+        period = BudgetingPeriod.objects.create(**payload)
 
-        assert budgeting_period.name == payload['name']
-        assert budgeting_period.user == payload['user']
-        assert budgeting_period.date_start == payload['date_start']
-        assert budgeting_period.date_end == payload['date_end']
-        assert budgeting_period.is_active is False
-        assert BudgetingPeriod.objects.filter(user=user).count() == 1
-        assert str(budgeting_period) == f'{budgeting_period.name} ({budgeting_period.user.email})'
+        for k, v in payload.items():
+            assert getattr(period, k) == v
+        assert period.is_active is False
+        assert BudgetingPeriod.objects.filter(budget=budget).count() == 1
+        assert str(period) == f'{period.name} ({period.budget.name})'
 
-    def test_create_two_periods_successful(self, user):
-        """Test creating two consecutive BudgetingPeriod successfully."""
+    def test_create_two_periods_successful(self, budget: Budget):
+        """
+        GIVEN: Budget model instance in database.
+        WHEN: Two BudgetingPeriod instances create attempt with valid, not colliding data.
+        THEN: Two BudgetingPeriod instances existing in database.
+        """
         payload_1 = {
             'name': '2023_01',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
         }
         payload_2 = {
             'name': '2023_02',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 2, 1),
             'date_end': date(2023, 2, 28),
         }
         budgeting_period_1 = BudgetingPeriod.objects.create(**payload_1)
         budgeting_period_2 = BudgetingPeriod.objects.create(**payload_2)
         for budgeting_period, payload in [(budgeting_period_1, payload_1), (budgeting_period_2, payload_2)]:
-            assert budgeting_period.name == payload['name']
-            assert budgeting_period.user == payload['user']
-            assert budgeting_period.date_start == payload['date_start']
-            assert budgeting_period.date_end == payload['date_end']
+            for k, v in payload.items():
+                assert getattr(budgeting_period, k) == v
             assert budgeting_period.is_active is False
-        assert BudgetingPeriod.objects.filter(user=user).count() == 2
+        assert BudgetingPeriod.objects.filter(budget=budget).count() == 2
 
-    def test_creating_same_period_by_two_users(self, user_factory):
-        """Test creating period with the same params by two different users."""
+    def test_creating_same_period_for_two_budgets(self, budget_factory: FactoryMetaClass):
+        """
+        GIVEN: Two Budget model instances in database.
+        WHEN: Two BudgetingPeriod instances - both for different Budgets - create attempt with valid data.
+        THEN: Two BudgetingPeriod instances for different Budgets existing in database.
+        """
         payload_1 = {
             'name': '2023_01',
-            'user': user_factory(),
+            'budget': budget_factory(),
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
             'is_active': True,
         }
         payload_2 = {
             'name': '2023_01',
-            'user': user_factory(),
+            'budget': budget_factory(),
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
             'is_active': True,
@@ -178,16 +185,20 @@ class TestBudgetingPeriodModel:
         BudgetingPeriod.objects.create(**payload_2)
 
         assert BudgetingPeriod.objects.all().count() == 2
-        assert BudgetingPeriod.objects.filter(user=payload_1['user']).count() == 1
-        assert BudgetingPeriod.objects.filter(user=payload_2['user']).count() == 1
+        assert BudgetingPeriod.objects.filter(budget=payload_1['budget']).count() == 1
+        assert BudgetingPeriod.objects.filter(budget=payload_2['budget']).count() == 1
 
     @pytest.mark.django_db(transaction=True)
-    def test_error_name_too_long(self, user):
-        """Test error on creating period with name too long."""
+    def test_error_name_too_long(self, budget: Budget):
+        """
+        GIVEN: Budget model instance in database.
+        WHEN: BudgetingPeriod instance create attempt with name too long in payload.
+        THEN: DataError raised, object not created in database.
+        """
         max_length = BudgetingPeriod._meta.get_field('name').max_length
         payload = {
             'name': (max_length + 1) * 'a',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
         }
@@ -195,14 +206,18 @@ class TestBudgetingPeriodModel:
         with pytest.raises(DataError) as exc:
             BudgetingPeriod.objects.create(**payload)
         assert str(exc.value) == f'value too long for type character varying({max_length})\n'
-        assert not BudgetingPeriod.objects.filter(user=user).exists()
+        assert not BudgetingPeriod.objects.filter(budget=budget).exists()
 
     @pytest.mark.django_db(transaction=True)
-    def test_error_name_already_used(self, user):
-        """Test error on creating period with already used name by the same user."""
+    def test_error_name_already_used(self, budget: Budget):
+        """
+        GIVEN: Single BudgetingPeriod for Budget in database.
+        WHEN: BudgetingPeriod instance create attempt with name already used for existing BudgetingPeriod.
+        THEN: DataError raised, object not created in database.
+        """
         payload = {
             'name': '2023_01',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
         }
@@ -212,21 +227,25 @@ class TestBudgetingPeriodModel:
         payload['date_end'] = date(2023, 2, 28)
         with pytest.raises(IntegrityError) as exc:
             BudgetingPeriod.objects.create(**payload)
-        assert f'DETAIL:  Key (name, user_id)=({payload["name"]}, {user.id}) already exists.' in str(exc.value)
-        assert BudgetingPeriod.objects.filter(user=user).count() == 1
+        assert f'DETAIL:  Key (name, budget_id)=({payload["name"]}, {budget.id}) already exists.' in str(exc.value)
+        assert BudgetingPeriod.objects.filter(budget=budget).count() == 1
 
-    def test_create_active_period_successfully(self, user):
-        """Test creating period with is_active=True successfully."""
+    def test_create_active_period_successfully(self, budget: Budget):
+        """
+        GIVEN: Single inactive BudgetingPeriod for Budget in database.
+        WHEN: BudgetingPeriod instance create attempt with is_active = True.
+        THEN: Two BudgetingPeriod model instances existing in database - one active, one inactive.
+        """
         payload_inactive = {
             'name': '2023_01',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
             'is_active': False,
         }
         payload_active = {
             'name': '2023_02',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 2, 1),
             'date_end': date(2023, 2, 28),
             'is_active': True,
@@ -236,13 +255,17 @@ class TestBudgetingPeriodModel:
         period_active = BudgetingPeriod.objects.create(**payload_active)
         assert period_inactive.is_active is False
         assert period_active.is_active is True
-        assert BudgetingPeriod.objects.filter(user=user).count() == 2
+        assert BudgetingPeriod.objects.filter(budget=budget).count() == 2
 
-    def test_error_is_active_set_already(self, user):
-        """Test error on making period active, when another user period active already."""
+    def test_error_is_active_set_already(self, budget: Budget):
+        """
+        GIVEN: Single active BudgetingPeriod for Budget in database.
+        WHEN: BudgetingPeriod instance create attempt with is_active = True.
+        THEN: ValidationError raised as active period already exists. New period not created in database.
+        """
         payload_1 = {
             'name': '2023_01',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
             'is_active': True,
@@ -251,7 +274,7 @@ class TestBudgetingPeriodModel:
 
         payload_2 = {
             'name': '2023_02',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 1, 1),
             'date_end': date(2023, 1, 31),
             'is_active': True,
@@ -259,46 +282,19 @@ class TestBudgetingPeriodModel:
         with pytest.raises(ValidationError) as exc:
             BudgetingPeriod.objects.create(**payload_2)
         assert exc.value.code == 'active-invalid'
-        assert exc.value.message == 'is_active: User already has active budgeting period.'
-        assert BudgetingPeriod.objects.filter(user=user).count() == 1
-        assert BudgetingPeriod.objects.filter(user=user).first() == active_period
+        assert exc.value.message == 'is_active: Active period already exists.'
+        assert BudgetingPeriod.objects.filter(budget=budget).count() == 1
+        assert BudgetingPeriod.objects.filter(budget=budget).first() == active_period
 
-    @pytest.mark.django_db(transaction=True)
-    def test_error_is_active_none(self, user):
-        """Test error on creating period with is_active set to None."""
+    def test_error_date_end_before_date_start(self, budget: Budget):
+        """
+        GIVEN: Single Budget in database.
+        WHEN: BudgetingPeriod instance create attempt with date_end before date_start.
+        THEN: ValidationError raised. New period not created in database.
+        """
         payload = {
             'name': '2023_01',
-            'user': user,
-            'date_start': date(2023, 1, 1),
-            'date_end': date(2023, 1, 31),
-            'is_active': None,
-        }
-        with pytest.raises(IntegrityError) as exc:
-            BudgetingPeriod.objects.create(**payload)
-        assert 'null value in column "is_active"' in str(exc.value)
-        assert not BudgetingPeriod.objects.filter(user=user).exists()
-
-    @pytest.mark.django_db(transaction=True)
-    @pytest.mark.parametrize('date_start, date_end', ((None, date.today()), (date.today(), None), (None, None)))
-    def test_error_date_not_set(self, user, date_start: Union[date, None], date_end: Union[date, None]):
-        """Test error on creating period with date_start or date_end set to None."""
-        payload = {
-            'name': '2023_01',
-            'user': user,
-            'date_start': date_start,
-            'date_end': date_end,
-        }
-
-        with pytest.raises(IntegrityError) as exc:
-            BudgetingPeriod.objects.create(**payload)
-        assert 'null value in column "date_' in str(exc.value)
-        assert not BudgetingPeriod.objects.filter(user=user).exists()
-
-    def test_error_date_end_before_date_start(self, user):
-        """Test error on creating period with date_end earlier than date_start."""
-        payload = {
-            'name': '2023_01',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 5, 1),
             'date_end': date(2023, 4, 30),
         }
@@ -307,7 +303,7 @@ class TestBudgetingPeriodModel:
             BudgetingPeriod.objects.create(**payload)
         assert exc.value.code == 'date-invalid'
         assert exc.value.message == 'start_date: Start date should be earlier than end date.'
-        assert not BudgetingPeriod.objects.filter(user=user).exists()
+        assert not BudgetingPeriod.objects.filter(budget=budget).exists()
 
     @pytest.mark.parametrize(
         'date_start, date_end',
@@ -348,23 +344,28 @@ class TestBudgetingPeriodModel:
             (date(2023, 7, 31), date(2023, 8, 1)),
         ),
     )
-    def test_error_date_invalid(self, user, date_start: date, date_end: date):
-        """Test error on creating period with invalid dates."""
+    def test_error_date_invalid(self, budget: Budget, date_start: date, date_end: date):
+        """
+        GIVEN: Two BudgetingPeriods for single Budget in database created.
+        WHEN: BudgetingPeriod instance create attempt with date_start and/or date_end colliding with
+        existing BudgetingPeriods.
+        THEN: ValidationError raised. New period not created in database.
+        """
         payload_1 = {
             'name': '2023_06',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 6, 1),
             'date_end': date(2023, 6, 30),
         }
         payload_2 = {
             'name': '2023_07',
-            'user': user,
+            'budget': budget,
             'date_start': date(2023, 7, 1),
             'date_end': date(2023, 7, 31),
         }
         payload_invalid = {
             'name': 'invalid',
-            'user': user,
+            'budget': budget,
             'date_start': date_start,
             'date_end': date_end,
         }
@@ -373,7 +374,5 @@ class TestBudgetingPeriodModel:
         with pytest.raises(ValidationError) as exc:
             BudgetingPeriod.objects.create(**payload_invalid)
         assert exc.value.code == 'period-range-invalid'
-        assert (
-            exc.value.message == "date_start: Budgeting period date range collides with other user's budgeting periods."
-        )
-        assert BudgetingPeriod.objects.filter(user=user).count() == 2
+        assert exc.value.message == "date_start: Period date range collides with other Budget's periods."
+        assert BudgetingPeriod.objects.filter(budget=budget).count() == 2
