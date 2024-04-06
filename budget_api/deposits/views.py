05d2c2fd8319ace7aa55fdfed4e51dc72a44606c
MateDawid <mateusiakdawid@gmail.com>
2024-04-06 21:51:07 +0200

    deposit_refactor: UserBelongToBudgetPermission class created and used in DepositViewSet and BudgetingPeriodViewSet

--- budget_api/deposits/views.py
+++ budget_api/deposits/views.py
@@ -1,5 +1,7 @@
+from app_config.permissions import UserBelongToBudgetPermission
 from deposits.models import Deposit
 from deposits.serializers import DepositSerializer
+from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
@@ -11,15 +13,11 @@ class DepositViewSet(viewsets.ModelViewSet):
     serializer_class = DepositSerializer
     queryset = Deposit.objects.all()
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, UserBelongToBudgetPermission]
 
     def get_queryset(self):
         """Retrieve Deposits for authenticated user."""
         user = getattr(self.request, 'user', None)
         if user and user.is_authenticated:
-            return self.queryset.filter(user=user).distinct()
+            return self.queryset.filter(Q(budget__owner=user) | Q(budget__members=user)).distinct()
         return self.queryset.none()
-
-    def perform_create(self, serializer):
-        """Additionally save user in Deposit model."""
-        serializer.save(user=self.request.user)
