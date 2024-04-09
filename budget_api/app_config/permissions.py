2a880dae0699b9acfc0566651ab328c7091e5756
MateDawid <mateusiakdawid@gmail.com>
2024-04-09 12:47:01 +0200

    deposit_refactor: Update UserBelongToBudgetPermission with budget field in request

--- budget_api/app_config/permissions.py
+++ budget_api/app_config/permissions.py
@@ -1,4 +1,3 @@
-from budgets.models import Budget
 from rest_framework import permissions
 from rest_framework.request import Request
 from rest_framework.views import APIView
@@ -20,5 +19,8 @@ class UserBelongToBudgetPermission(permissions.BasePermission):
         Returns:
             bool: True if User is owner or member of Budget, else False.
         """
-        budget = Budget.objects.filter(pk=request.parser_context.get('kwargs', {}).get('budget_pk')).first()
-        return bool(budget and request.user and (request.user == budget.owner or request.user in budget.members.all()))
+        return bool(
+            request.budget
+            and request.user
+            and (request.user == request.budget.owner or request.user in request.budget.members.all())
+        )
