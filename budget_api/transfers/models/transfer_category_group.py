ad937b48c0d4465a85d28e5791de7768ff995aec
MateDawid <mateusiakdawid@gmail.com>
2024-04-16 10:05:36 +0200

    category_refactor: TransferCategoryGroup model

---
+++ budget_api/transfers/models/transfer_category_group.py
@@ -0,0 +1,31 @@
+from django.db import models
+from transfers.models.base_transfer_model import BaseTransferModel
+
+
+class TransferCategoryGroup(BaseTransferModel):
+    """TransferCategoryGroup model for grouping TransferCategory model instances."""
+
+    budget = models.ForeignKey(
+        'budgets.Budget', on_delete=models.CASCADE, related_name='category_groups', null=False, blank=False
+    )
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    transfer_type = models.PositiveSmallIntegerField(
+        choices=BaseTransferModel.TransferTypes.choices, null=False, blank=False
+    )
+
+    class Meta:
+        verbose_name_plural = 'transfer category groups'
+        unique_together = (
+            'name',
+            'budget',
+        )
+
+    def __str__(self) -> str:
+        """
+        Returns string representation of TransferCategoryGroup model instance.
+
+        Returns:
+            str: Custom string representation of instance.
+        """
+        return self.name
