0551479536a8312973cdce08f94c772f82722d63
MateDawid <mateusiakdawid@gmail.com>
2024-04-22 09:44:46 +0200

    category_refactor: No base abstract class for TransferCategoryGroup

--- budget_api/transfers/tests/factories.py
+++ budget_api/transfers/tests/factories.py
@@ -1,36 +1,31 @@
-import random
-
 import factory.fuzzy
-from app_users.tests.factories import UserFactory
 from budgets.tests.factories import BudgetFactory
 from transfers.models.transfer_category_group_model import TransferCategoryGroup
-from transfers.models.transfer_category_model import TransferCategory
 
-
-class TransferCategoryFactory(factory.django.DjangoModelFactory):
-    """Factory for TransferCategory model."""
-
-    class Meta:
-        model = 'transfers.TransferCategory'
-
-    name = factory.Faker('text', max_nb_chars=128)
-    description = factory.Faker('text', max_nb_chars=255)
-    category_type = factory.Faker('random_element', elements=[TransferCategory.EXPENSE, TransferCategory.INCOME])
-    is_active = factory.Faker('boolean')
-
-    @factory.lazy_attribute
-    def user(self) -> str:
-        """Generates user field value - User model instance or None."""
-        options = [UserFactory(), None]
-        return random.choice(options)
-
-    @factory.lazy_attribute
-    def scope(self) -> str:
-        """Generates type field value basing on user field value."""
-        if self.user is None:
-            return TransferCategory.GLOBAL
-        else:
-            return TransferCategory.PERSONAL
+# class TransferCategoryFactory(factory.django.DjangoModelFactory):
+#     """Factory for TransferCategory model."""
+#
+#     class Meta:
+#         model = 'transfers.TransferCategory'
+#
+#     name = factory.Faker('text', max_nb_chars=128)
+#     description = factory.Faker('text', max_nb_chars=255)
+#     category_type = factory.Faker('random_element', elements=[TransferCategory.EXPENSE, TransferCategory.INCOME])
+#     is_active = factory.Faker('boolean')
+#
+#     @factory.lazy_attribute
+#     def user(self) -> str:
+#         """Generates user field value - User model instance or None."""
+#         options = [UserFactory(), None]
+#         return random.choice(options)
+#
+#     @factory.lazy_attribute
+#     def scope(self) -> str:
+#         """Generates type field value basing on user field value."""
+#         if self.user is None:
+#             return TransferCategory.GLOBAL
+#         else:
+#             return TransferCategory.PERSONAL
 
 
 class TransferCategoryGroupFactory(factory.django.DjangoModelFactory):
