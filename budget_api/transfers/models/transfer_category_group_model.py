3c484aefad91c47a45e6b1c3a1ba680fad8362f1
MateDawid <mateusiakdawid@gmail.com>
2024-04-22 09:44:46 +0200

    category_refactor: No base abstract class for TransferCategoryGroup

--- budget_api/transfers/models/transfer_category_group_model.py
+++ budget_api/transfers/models/transfer_category_group_model.py
@@ -1,18 +1,22 @@
 from django.db import models
-from transfers.models.base_transfer_model import BaseTransferModel
 
 
-class TransferCategoryGroup(BaseTransferModel):
+class TransferCategoryGroup(models.Model):
     """TransferCategoryGroup model for grouping TransferCategory model instances."""
+
+    class TransferTypes(models.IntegerChoices):
+        """Choices for deposit_type value."""
+
+        INCOME = 0, 'Income'
+        EXPENSE = 1, 'Expense'
+        RELOCATION = 2, 'Relocation'
 
     budget = models.ForeignKey(
         'budgets.Budget', on_delete=models.CASCADE, related_name='category_groups', null=False, blank=False
     )
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
-    transfer_type = models.PositiveSmallIntegerField(
-        choices=BaseTransferModel.TransferTypes.choices, null=False, blank=False
-    )
+    transfer_type = models.PositiveSmallIntegerField(choices=TransferTypes.choices, null=False, blank=False)
 
     class Meta:
         verbose_name_plural = 'transfer category groups'
