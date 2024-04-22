11edeaa42880dd6d2cc52e3145d3c221beeff955
MateDawid <mateusiakdawid@gmail.com>
2024-04-22 09:48:02 +0200

    category_refactor: Remove cleaning data from TransferCategory model

--- budget_api/transfers/models/transfer_category_model.py
+++ budget_api/transfers/models/transfer_category_model.py
@@ -29,42 +29,4 @@ class TransferCategory(models.Model):
         Returns:
             str: Custom string representation of instance.
         """
-        return self.name
-
-    def save(self, *args, **kwargs):
-        """Override save method to clean data before saving model in database."""
-        self.clean()
-        super().save(*args, **kwargs)
-
-    # def clean(self):
-    #     """Clean TransferCategory input data before saving in database."""
-    #     self.clean_owner()
-    #     self.clean_name()
-    #
-    # def clean_name(self):
-    #     if (
-    #         self.scope == self.PERSONAL
-    #         and self.user.personal_transfer_categories.filter(name__iexact=self.name).exclude(id=self.id).exists()
-    #     ):
-    #         raise ValidationError(
-    #             'name: Personal transfer category with given name already exists.', code='personal-name-invalid'
-    #         )
-    #     elif (
-    #         self.scope == self.GLOBAL
-    #         and TransferCategory.global_transfer_categories.filter(name__iexact=self.name).exclude(
-    #         id=self.id).exists()
-    #     ):
-    #         raise ValidationError(
-    #             'name: Global transfer category with given name already exists.', code='global-name-invalid'
-    #         )
-    #
-    # def clean_owner(self):
-    #     """Check if user field is filled only when type is "PERSONAL"."""
-    #     if self.scope == self.PERSONAL and self.user is None:
-    #         raise ValidationError(
-    #             'user: User was not provided for personal transfer category.', code='no-user-for-personal'
-    #         )
-    #     if self.scope == self.GLOBAL and self.user is not None:
-    #         raise ValidationError(
-    #             'user: User can be provided only for personal transfer category.', code='user-when-global'
-    #         )
+        return f'{self.name} ({self.group.name})'
