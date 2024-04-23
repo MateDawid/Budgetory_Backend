03f490f38480b71a3f6177a5a9db0fa6104801ad
MateDawid <mateusiakdawid@gmail.com>
2024-04-23 11:25:04 +0200

    category_refactor: TransferCategoryFactory

--- budget_api/transfers/tests/factories.py
+++ budget_api/transfers/tests/factories.py
@@ -1,31 +1,9 @@
+import random
+
 import factory.fuzzy
+from app_users.tests.factories import UserFactory
 from budgets.tests.factories import BudgetFactory
 from transfers.models.transfer_category_group_model import TransferCategoryGroup
-
-# class TransferCategoryFactory(factory.django.DjangoModelFactory):
-#     """Factory for TransferCategory model."""
-#
-#     class Meta:
-#         model = 'transfers.TransferCategory'
-#
-#     name = factory.Faker('text', max_nb_chars=128)
-#     description = factory.Faker('text', max_nb_chars=255)
-#     category_type = factory.Faker('random_element', elements=[TransferCategory.EXPENSE, TransferCategory.INCOME])
-#     is_active = factory.Faker('boolean')
-#
-#     @factory.lazy_attribute
-#     def user(self) -> str:
-#         """Generates user field value - User model instance or None."""
-#         options = [UserFactory(), None]
-#         return random.choice(options)
-#
-#     @factory.lazy_attribute
-#     def scope(self) -> str:
-#         """Generates type field value basing on user field value."""
-#         if self.user is None:
-#             return TransferCategory.GLOBAL
-#         else:
-#             return TransferCategory.PERSONAL
 
 
 class TransferCategoryGroupFactory(factory.django.DjangoModelFactory):
@@ -38,3 +16,21 @@ class TransferCategoryGroupFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('text', max_nb_chars=128)
     description = factory.Faker('text', max_nb_chars=255)
     transfer_type = factory.fuzzy.FuzzyChoice([x[0] for x in TransferCategoryGroup.TransferTypes.choices])
+
+
+class TransferCategoryFactory(factory.django.DjangoModelFactory):
+    """Factory for TransferCategory model."""
+
+    class Meta:
+        model = 'transfers.TransferCategory'
+
+    group = factory.SubFactory(TransferCategoryGroupFactory)
+    name = factory.Faker('text', max_nb_chars=128)
+    description = factory.Faker('text', max_nb_chars=255)
+    is_active = factory.Faker('boolean')
+
+    @factory.lazy_attribute
+    def owner(self) -> str:
+        """Generates user field value - User model instance or None."""
+        options = [UserFactory(), None]
+        return random.choice(options)
