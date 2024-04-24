40ac5384827a5f72084728c3d84b0d135a32e346
MateDawid <mateusiakdawid@gmail.com>
2024-04-24 09:57:37 +0200

    category_refactor: Initial TransferCategoryViewSet

--- budget_api/transfers/views/transfer_category_view.py
+++ budget_api/transfers/views/transfer_category_view.py
@@ -1,49 +1,28 @@
-from django.db.models import Q
-from rest_framework import status, viewsets
+from app_config.permissions import UserBelongsToBudgetPermission
+from budgets.mixins import BudgetMixin
+from django.db.models import QuerySet
+from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from transfers.models.transfer_category_model import TransferCategory
-from transfers.permissions import IsPersonalTransferCategoryOwnerOrAdmin
 from transfers.serializers.transfer_category_serializer import (
     TransferCategorySerializer,
 )
 
 
-class TransferCategoryViewSet(viewsets.ModelViewSet):
+class TransferCategoryViewSet(BudgetMixin, viewsets.ModelViewSet):
     """View for managing TransferCategories."""
 
     serializer_class = TransferCategorySerializer
     queryset = TransferCategory.objects.all()
     authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, UserBelongsToBudgetPermission]
 
-    def get_permissions(self):
-        """Checks permissions depending on view to execute."""
-        if self.request.method == 'POST' and self.request.data.get('scope') == 'GLOBAL':
-            return (IsAdminUser(),)
-        if self.request.method in ['DELETE', 'PUT', 'PATCH']:
-            return (IsPersonalTransferCategoryOwnerOrAdmin(),)
-        return (IsAuthenticated(),)
+    def get_queryset(self) -> QuerySet:
+        """
+        Retrieve TransferCategories for Budget passed in URL.
 
-    def get_queryset(self):
-        """Retrieve global TransferCategories and personal TransferCategories for authenticated user."""
-        user = getattr(self.request, 'user', None)
-        if user and user.is_authenticated:
-            return self.queryset.filter(
-                Q(scope=TransferCategory.GLOBAL) | Q(scope=TransferCategory.PERSONAL, user=user)
-            ).distinct()
-        return self.queryset.none()  # pragma: no cover
-
-    def create(self, request, *args, **kwargs):
-        """Extend create method with passing user in serializer depending on TransferCategory type."""
-        data = request.data.copy()
-        match request.data.get('scope'):
-            case TransferCategory.PERSONAL:
-                data['user'] = request.user.id
-            case _:
-                data['user'] = None
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        Returns:
+            QuerySet: Filtered TransferCategory QuerySet.
+        """
+        return self.queryset.prefetch_related('owner', 'group').filter(group__budget=self.request.budget).distinct()
