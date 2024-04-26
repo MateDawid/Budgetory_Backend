0ca0c2dd418e0ed267bbb2d6601c9b19052466fb
MateDawid <mateusiakdawid@gmail.com>
2024-04-26 11:01:00 +0200

    category_refactor: Validating input data in TransferCategorySerializer

--- budget_api/transfers/serializers/transfer_category_serializer.py
+++ budget_api/transfers/serializers/transfer_category_serializer.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 
+from django.contrib.auth.models import AbstractUser
 from rest_framework import serializers
+from transfers.models.transfer_category_group_model import TransferCategoryGroup
 from transfers.models.transfer_category_model import TransferCategory
 
 
@@ -23,24 +25,39 @@ class TransferCategorySerializer(serializers.ModelSerializer):
             OrderedDict: Dictionary with validated attrs values.
         """
         name = attrs.get('name') or getattr(self.instance, 'name', None)
-        user = attrs.get('user') or getattr(self.instance, 'user', None)
+        owner = attrs.get('owner') or getattr(self.instance, 'owner', None)
+        group = attrs.get('group') or getattr(self.instance, 'group', None)
 
+        self._validate_group(group)
+        self._validate_owner(owner)
+        self._validate_name(name, owner)
+
+        return attrs
+
+    def _validate_group(self, group: TransferCategoryGroup):
+        if group not in self.context['request'].budget.category_groups.all():
+            raise serializers.ValidationError('TransferCategoryGroup does not belong to Budget.')
+
+    def _validate_owner(self, owner: AbstractUser):
+        if not (owner == self.context['request'].budget.owner or owner in self.context['request'].budget.members.all()):
+            raise serializers.ValidationError('Provided owner does not belong to Budget.')
+
+    def _validate_name(self, name: str | None, owner: AbstractUser):
         if (
-            user
-            and user.personal_categories.filter(name__iexact=name)
+            owner
+            and owner.personal_categories.filter(group__budget=self.context['request'].budget, name__iexact=name)
             .exclude(id=getattr(self.instance, 'id', None))
             .exists()
         ):
-            raise serializers.ValidationError('Personal TransferCategory with given name already exists in Budget.')
+            raise serializers.ValidationError(
+                'Personal TransferCategory with given name already exists in Budget for provided owner.'
+            )
         elif (
-            user is None
-            and TransferCategory.objects.filter(owner__isnull=True, name__iexact=name)
+            owner is None
+            and TransferCategory.objects.filter(
+                group__budget=self.context['request'].budget, owner__isnull=True, name__iexact=name
+            )
             .exclude(id=getattr(self.instance, 'id', None))
             .exists()
         ):
             raise serializers.ValidationError('Common TransferCategory with given name already exists in Budget.')
-
-        return attrs
-
-    # TODO - validate if group from accessible budget
-    # TODO - validate if user from accessible budget
