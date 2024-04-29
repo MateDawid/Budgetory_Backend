8a357badf1ea21d35d8c3fe52ea33dbda1fd782a
MateDawid <mateusiakdawid@gmail.com>
2024-04-29 10:53:54 +0200

    category_refactor: TestTransferCategoryApiDetail

--- budget_api/transfers/tests/test_api_transfer_category.py
+++ budget_api/transfers/tests/test_api_transfer_category.py
@@ -440,75 +440,75 @@ class TestTransferCategoryApiCreate:
         assert TransferCategory.objects.filter(group__budget=budget, owner__isnull=True).count() == 1
 
 
-# @pytest.mark.django_db
-# class TestTransferCategoryApiDetail:
-#     """Tests for detail view on TransferCategoryViewSet."""
-#
-#     @pytest.mark.parametrize('user_type', ['owner', 'member'])
-#     def test_get_category_details(
-#         self,
-#         api_client: APIClient,
-#         base_user: AbstractUser,
-#         budget_factory: FactoryMetaClass,
-#         transfer_category_factory: FactoryMetaClass,
-#         user_type: str,
-#     ):
-#         """
-#         GIVEN: TransferCategory instance for Budget created in database.
-#         WHEN: TransferCategoryViewSet detail view called by User belonging to Budget.
-#         THEN: HTTP 200, TransferCategory details returned.
-#         """
-#         if user_type == 'owner':
-#             budget = budget_factory(owner=base_user)
-#         else:
-#             budget = budget_factory(members=[base_user])
-#         category = transfer_category_factory(budget=budget)
-#         api_client.force_authenticate(base_user)
-#         url = category_detail_url(budget.id, category.id)
-#
-#         response = api_client.get(url)
-#         serializer = TransferCategorySerializer(category)
-#
-#         assert response.status_code == status.HTTP_200_OK
-#         assert response.data == serializer.data
-#
-#     def test_error_get_category_details_unauthenticated(
-#         self, api_client: APIClient, base_user: AbstractUser, transfer_category_factory: FactoryMetaClass
-#     ):
-#         """
-#         GIVEN: TransferCategory instance for Budget created in database.
-#         WHEN: TransferCategoryViewSet detail view called without authentication.
-#         THEN: Unauthorized HTTP 401.
-#         """
-#         category = transfer_category_factory()
-#         url = category_detail_url(category.budget.id, category.id)
-#
-#         response = api_client.get(url)
-#
-#         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-#
-#     def test_error_get_details_from_not_accessible_budget(
-#         self,
-#         api_client: APIClient,
-#         base_user: AbstractUser,
-#         budget_factory: FactoryMetaClass,
-#         transfer_category_factory: FactoryMetaClass,
-#     ):
-#         """
-#         GIVEN: TransferCategory instance for Budget created in database.
-#         WHEN: TransferCategoryViewSet detail view called by User not belonging to Budget.
-#         THEN: Forbidden HTTP 403 returned.
-#         """
-#         category = transfer_category_factory(budget=budget_factory())
-#         api_client.force_authenticate(base_user)
-#
-#         url = category_detail_url(category.budget.id, category.id)
-#         response = api_client.get(url)
-#
-#         assert response.status_code == status.HTTP_403_FORBIDDEN
-#         assert response.data['detail'] == 'User does not have access to Budget.'
-#
-#
+@pytest.mark.django_db
+class TestTransferCategoryApiDetail:
+    """Tests for detail view on TransferCategoryViewSet."""
+
+    @pytest.mark.parametrize('user_type', ['owner', 'member'])
+    def test_get_category_details(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        transfer_category_factory: FactoryMetaClass,
+        user_type: str,
+    ):
+        """
+        GIVEN: TransferCategory instance for Budget created in database.
+        WHEN: TransferCategoryViewSet detail view called by User belonging to Budget.
+        THEN: HTTP 200, TransferCategory details returned.
+        """
+        if user_type == 'owner':
+            budget = budget_factory(owner=base_user)
+        else:
+            budget = budget_factory(members=[base_user])
+        category = transfer_category_factory(group__budget=budget)
+        api_client.force_authenticate(base_user)
+        url = category_detail_url(budget.id, category.id)
+
+        response = api_client.get(url)
+        serializer = TransferCategorySerializer(category)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == serializer.data
+
+    def test_error_get_category_details_unauthenticated(
+        self, api_client: APIClient, base_user: AbstractUser, transfer_category_factory: FactoryMetaClass
+    ):
+        """
+        GIVEN: TransferCategory instance for Budget created in database.
+        WHEN: TransferCategoryViewSet detail view called without authentication.
+        THEN: Unauthorized HTTP 401.
+        """
+        category = transfer_category_factory()
+        url = category_detail_url(category.group.budget.id, category.id)
+
+        response = api_client.get(url)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_error_get_details_from_not_accessible_budget(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        transfer_category_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: TransferCategory instance for Budget created in database.
+        WHEN: TransferCategoryViewSet detail view called by User not belonging to Budget.
+        THEN: Forbidden HTTP 403 returned.
+        """
+        category = transfer_category_factory(group__budget=budget_factory())
+        api_client.force_authenticate(base_user)
+
+        url = category_detail_url(category.group.budget.id, category.id)
+        response = api_client.get(url)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert response.data['detail'] == 'User does not have access to Budget.'
+
+
 # @pytest.mark.django_db
 # class TestTransferCategoryApiPartialUpdate:
 #     """Tests for partial update view on TransferCategoryViewSet."""
