05c0eef4a30e8641d7792b89c9e0c5a2fe860d71
MateDawid <mateusiakdawid@gmail.com>
2024-05-01 08:03:32 +0200

    category_refactor: Remove data_import app

--- budget_api/data_import/urls.py
+++
@@ -1,8 +0,0 @@
-from data_import.views import ImportFileViewSet
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register(r'', ImportFileViewSet, basename='importfile')
-
-
-urlpatterns = router.urls
