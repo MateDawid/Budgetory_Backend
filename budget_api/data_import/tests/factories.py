068d4ccb2bd9d3e652b462966923e936eac8ce0b
MateDawid <mateusiakdawid@gmail.com>
2024-05-01 08:03:32 +0200

    category_refactor: Remove data_import app

--- budget_api/data_import/tests/factories.py
+++
@@ -1,9 +0,0 @@
-import factory
-from data_import.models import ImportFile
-
-
-class ImportFileFactory(factory.django.DjangoModelFactory):
-    date_added = factory.Faker('date_time_between', start_date='-1y', end_date='now')
-
-    class Meta:
-        model = ImportFile
