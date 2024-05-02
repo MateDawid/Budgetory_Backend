f26e2d5b473d24fc4d801529d81bc84c90f92170
MateDawid <mateusiakdawid@gmail.com>
2024-05-02 08:23:12 +0200

    entity_refactor: EntityFactory refactored

--- budget_api/entities/tests/factories.py
+++ budget_api/entities/tests/factories.py
@@ -1,8 +1,4 @@
-import random
-
 import factory
-from app_users.tests.factories import UserFactory
-from entities.models import Entity
 
 
 class EntityFactory(factory.django.DjangoModelFactory):
@@ -13,17 +9,3 @@ class EntityFactory(factory.django.DjangoModelFactory):
 
     name = factory.Faker('text', max_nb_chars=128)
     description = factory.Faker('text', max_nb_chars=255)
-
-    @factory.lazy_attribute
-    def user(self) -> str:
-        """Generates user field value - User model instance or None."""
-        options = [UserFactory(), None]
-        return random.choice(options)
-
-    @factory.lazy_attribute
-    def type(self) -> str:
-        """Generates type field value basing on user field value."""
-        if self.user is None:
-            return Entity.GLOBAL
-        else:
-            return Entity.PERSONAL
