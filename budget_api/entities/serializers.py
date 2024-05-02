bb36451685d79149a230e2f693d4a1b68375e000
MateDawid <mateusiakdawid@gmail.com>
2024-05-02 08:35:03 +0200

    entity_refactor: EntitySerialzier

--- budget_api/entities/serializers.py
+++ budget_api/entities/serializers.py
@@ -1,7 +1,6 @@
-from typing import Any
-
 from entities.models import Entity
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class EntitySerializer(serializers.ModelSerializer):
@@ -9,36 +8,22 @@ class EntitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Entity
-        fields = ['id', 'name', 'description', 'type', 'user']
+        fields = ['id', 'name', 'description']
         read_only_fields = ['id']
 
-    def validate(self, attrs):
-        """Validates user and name before saving serializer."""
-        name = attrs.get('name') or getattr(self.instance, 'name', None)
-        user = attrs.get('user') or getattr(self.instance, 'user', None)
-        type_ = attrs.get('type') or getattr(self.instance, 'type', None)
+    def validate_name(self, name: str):
+        """
+        Checks if Entity with given name exists in Budget already.
 
-        self._validate_user(user=user, type_=type_)
-        self._validate_name(name=name, user=user, type_=type_)
-        return attrs
+        Args:
+            name: Name of TransferCategoryGroup.
 
-    @staticmethod
-    def _validate_user(user: Any, type_: str) -> None:
-        """Check if user field is filled only when type is "PERSONAL"."""
-        if type_ == 'PERSONAL' and user is None:
-            raise serializers.ValidationError('User was not provided for personal Entity.')
-        if type_ == 'GLOBAL' and user is not None:
-            raise serializers.ValidationError('User can be provided only for personal Entities.')
+        Returns:
+            str: Validated name of Entity.
 
-    def _validate_name(self, name: str, user: Any, type_: str) -> None:
-        """Checks if user has not used entity name already."""
-        if (
-            type_ == 'PERSONAL'
-            and user.personal_entities.filter(name__iexact=name).exclude(id=getattr(self.instance, 'id', None)).exists()
-        ):
-            raise serializers.ValidationError('Personal entity with given name already exists.')
-        elif (
-            type_ == 'GLOBAL'
-            and Entity.global_entities.filter(name__iexact=name).exclude(id=getattr(self.instance, 'id', None)).exists()
-        ):
-            raise serializers.ValidationError('Global entity with given name already exists.')
+        Raises:
+            ValidationError: Raised if Entity with given name exists in Budget already.
+        """
+        if self.Meta.model.objects.filter(budget=self.context['request'].budget, name__iexact=name).exists():
+            raise ValidationError('Entity with given name already exists in Budget.')
+        return name
