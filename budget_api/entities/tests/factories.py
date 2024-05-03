2fe3c360438f2fe1b46dd880ae5564ca0f37f770
MateDawid <mateusiakdawid@gmail.com>
2024-05-03 07:55:04 +0200

    entity_refactor: TestEntityApiList

--- budget_api/entities/tests/factories.py
+++ budget_api/entities/tests/factories.py
@@ -1,4 +1,5 @@
 import factory
+from budgets.tests.factories import BudgetFactory
 
 
 class EntityFactory(factory.django.DjangoModelFactory):
@@ -7,5 +8,6 @@ class EntityFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'entities.Entity'
 
+    budget = factory.SubFactory(BudgetFactory)
     name = factory.Faker('text', max_nb_chars=128)
     description = factory.Faker('text', max_nb_chars=255)
