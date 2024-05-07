334cea0ea3da08dbc50bc17bbaf8db704ac49bde
MateDawid <mateusiakdawid@gmail.com>
2024-05-07 09:31:17 +0200

    category_group_remove: TransferCategoryFactory

--- budget_api/transfers/tests/factories.py
+++ budget_api/transfers/tests/factories.py
@@ -3,6 +3,7 @@ import random
 import factory.fuzzy
 from app_users.tests.factories import UserFactory
 from budgets.tests.factories import BudgetFactory
+from transfers.models import TransferCategory
 
 
 class TransferCategoryFactory(factory.django.DjangoModelFactory):
@@ -15,6 +16,8 @@ class TransferCategoryFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('text', max_nb_chars=128)
     description = factory.Faker('text', max_nb_chars=255)
     is_active = factory.Faker('boolean')
+    expense_group = factory.fuzzy.FuzzyChoice([x[0] for x in TransferCategory.ExpenseGroups.choices])
+    income_group = factory.fuzzy.FuzzyChoice([x[0] for x in TransferCategory.IncomeGroups.choices])
 
     @factory.lazy_attribute
     def owner(self) -> str:
