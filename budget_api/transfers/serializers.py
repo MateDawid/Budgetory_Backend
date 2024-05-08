fac90e05205562ba633d2cacf348fc9855444621
MateDawid <mateusiakdawid@gmail.com>
2024-05-08 14:26:02 +0200

    category_group_remove: TransferCategorySerializer

--- budget_api/transfers/serializers.py
+++ budget_api/transfers/serializers.py
@@ -10,12 +10,12 @@ class TransferCategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TransferCategory
-        fields = ['id', 'group', 'name', 'description', 'owner', 'is_active']
+        fields = ['id', 'name', 'expense_group', 'income_group', 'description', 'is_active', 'owner']
         read_only_fields = ['id']
 
     def validate(self, attrs: OrderedDict) -> OrderedDict:
         """
-        Checks if common or personal TransferCategory already exists in Budget.
+        Validates TransferCategory.
 
         Args:
             attrs [OrderedDict]: Dictionary containing given TransferCategory params
@@ -25,22 +25,46 @@ class TransferCategorySerializer(serializers.ModelSerializer):
         """
         name = attrs.get('name') or getattr(self.instance, 'name', None)
         owner = attrs.get('owner') or getattr(self.instance, 'owner', None)
+        expense_group = attrs.get('expense_group') or getattr(self.instance, 'expense_group', None)
+        income_group = attrs.get('income_group') or getattr(self.instance, 'income_group', None)
 
         self._validate_owner(owner)
         self._validate_name(name, owner)
+        self._validate_groups(expense_group, income_group)
 
         return attrs
 
-    def _validate_owner(self, owner: AbstractUser | None):
+    def _validate_owner(self, owner: AbstractUser | None) -> None:
+        """
+        Checks if provided owner belongs to Budget.
+
+        Args:
+            owner [AbstractUser | None]:  TransferCategory owner or None.
+
+        Raises:
+            ValidationError: Raised when given User does not belong to Budget.
+        """
         if owner and not (
             owner == self.context['request'].budget.owner or owner in self.context['request'].budget.members.all()
         ):
             raise serializers.ValidationError('Provided owner does not belong to Budget.')
 
-    def _validate_name(self, name: str | None, owner: AbstractUser):
+    def _validate_name(self, name: str | None, owner: AbstractUser | None) -> None:
+        """
+        Checks if personal (when owner provided) or common TransferCategory with provided name
+        already exists in Budget.
+
+        Args:
+            name [str]: Name for TransferCategory
+            owner [AbstractUser | None]: Owner of TransferCategory or None
+
+        Raises:
+            ValidationError: Raised when TransferCategory for particular owner (including None value) already
+            exists in Budget.
+        """
         if (
             owner
-            and owner.personal_categories.filter(group__budget=self.context['request'].budget, name__iexact=name)
+            and owner.personal_categories.filter(budget=self.context['request'].budget, name__iexact=name)
             .exclude(id=getattr(self.instance, 'id', None))
             .exists()
         ):
@@ -50,9 +74,26 @@ class TransferCategorySerializer(serializers.ModelSerializer):
         elif (
             owner is None
             and TransferCategory.objects.filter(
-                group__budget=self.context['request'].budget, owner__isnull=True, name__iexact=name
+                budget=self.context['request'].budget, owner__isnull=True, name__iexact=name
             )
             .exclude(id=getattr(self.instance, 'id', None))
             .exists()
         ):
             raise serializers.ValidationError('Common TransferCategory with given name already exists in Budget.')
+
+    @staticmethod
+    def _validate_groups(expense_group, income_group) -> None:
+        """
+        Checks if exactly one of groups was indicated for TransferCategory.
+
+        Args:
+            expense_group [int | None]: Expense group number or None
+            income_group [int | None]: Income group number or None
+
+        Raises:
+            ValidationError: Raised when no group was selected or when both groups were selected.
+        """
+        if expense_group and income_group:
+            raise serializers.ValidationError('Only one type of group can be selected for TransferCategory.')
+        elif not (expense_group or income_group):
+            raise serializers.ValidationError('Expense group or income group has to be selected for TransferCategory.')
