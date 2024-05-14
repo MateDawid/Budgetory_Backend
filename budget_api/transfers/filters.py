f503e9a5fd7d34e7e77e32ee2eed16141f3ed1b2
MateDawid <mateusiakdawid@gmail.com>
2024-05-14 14:14:53 +0200

    category_group_remove: Separate TransferCategoryFilterSets for Expense and Income categories

--- budget_api/transfers/filters.py
+++ budget_api/transfers/filters.py
@@ -2,11 +2,43 @@ from django_filters import rest_framework as filters
 from transfers.models import TransferCategory
 
 
-class TransferCategoriesFilterSet(filters.FilterSet):
-    """FilterSet for /quests endpoint."""
+class TransferCategoryFilterSet(filters.FilterSet):
+    """Base FilterSet for TransferCategory endpoints."""
 
     name = filters.CharFilter(lookup_expr='icontains', field_name='name')
+    common_only = filters.BooleanFilter(method='get_common_categories')
+
+    class Meta:
+        abstract = True
+
+    def get_common_categories(self, queryset, name, value):
+        """
+        Filtering QuerySet with objects containing .
+
+        Args:
+            queryset [QuerySet]: Input QuerySet
+            name [str]: Name of filtered param
+            value [Decimal]: Value of filtered param
+
+        Returns:
+            QuerySet: Input QuerySet filtered by filter param value.
+        """
+        if value:
+            return queryset.filter(owner__isnull=True)
+        return queryset
+
+
+class ExpenseCategoryFilterSet(TransferCategoryFilterSet):
+    """FilterSet for /expense_categories endpoint."""
 
     class Meta:
         model = TransferCategory
-        fields = ['expense_group', 'income_group', 'owner', 'is_active']
+        fields = ['expense_group', 'owner', 'is_active']
+
+
+class IncomeCategoryFilterSet(TransferCategoryFilterSet):
+    """FilterSet for /income_categories endpoint."""
+
+    class Meta:
+        model = TransferCategory
+        fields = ['income_group', 'owner', 'is_active']
