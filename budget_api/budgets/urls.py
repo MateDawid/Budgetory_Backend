03fafc2656897b6bd9e7fdb651c92ab2a516f176
MateDawid <mateusiakdawid@gmail.com>
2024-05-14 14:17:46 +0200

    category_group_remove: URLs for TransferCategory viewsets

--- budget_api/budgets/urls.py
+++ budget_api/budgets/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from entities.views import EntityViewSet
 from rest_framework import routers
 from rest_framework_nested.routers import NestedSimpleRouter
-from transfers.views import TransferCategoryViewSet
+from transfers.views import ExpenseCategoryViewSet, IncomeCategoryViewSet
 
 app_name = 'budgets'
 
@@ -15,7 +15,8 @@ router.register(r'', BudgetViewSet)
 budget_router = NestedSimpleRouter(router, r'', lookup='budget')
 budget_router.register(r'periods', BudgetingPeriodViewSet, basename='period')
 budget_router.register(r'deposits', DepositViewSet, basename='deposit')
-budget_router.register(r'categories', TransferCategoryViewSet, basename='category')
+budget_router.register(r'expense_categories', ExpenseCategoryViewSet, basename='expense_category')
+budget_router.register(r'income_categories', IncomeCategoryViewSet, basename='income_category')
 budget_router.register(r'entities', EntityViewSet, basename='entity')
 
 urlpatterns = [
