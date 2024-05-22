d4b0bb5dbc52ff4e39df71ba38b14b25c5d49f38
MateDawid <mateusiakdawid@gmail.com>
2024-05-22 20:18:00 +0200

    category_group_remove: ExpenseCategory and IncomeCategory database models

--- budget_api/app_config/settings.py
+++ budget_api/app_config/settings.py
@@ -27,7 +27,7 @@ OUTER_APPS = [
     'drf_yasg',
 ]
 
-CREATED_APPS = ['app_users', 'budgets', 'deposits', 'entities', 'transfers']
+CREATED_APPS = ['app_users', 'budgets', 'deposits', 'entities', 'categories']
 
 INSTALLED_APPS = DJANGO_APPS + OUTER_APPS + CREATED_APPS
 
