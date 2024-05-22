9f5bb9a1ea088dc7f864daf95c4b94fc48d74ff7
MateDawid <mateusiakdawid@gmail.com>
2024-05-22 20:18:00 +0200

    category_group_remove: ExpenseCategory and IncomeCategory database models

--- budget_api/budgets/urls.py
+++ budget_api/budgets/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 from entities.views import EntityViewSet
 from rest_framework import routers
 from rest_framework_nested.routers import NestedSimpleRouter
-from transfers.views import ExpenseCategoryViewSet, IncomeCategoryViewSet
 
 app_name = 'budgets'
 
@@ -15,8 +14,6 @@ router.register(r'', BudgetViewSet)
 budget_router = NestedSimpleRouter(router, r'', lookup='budget')
 budget_router.register(r'periods', BudgetingPeriodViewSet, basename='period')
 budget_router.register(r'deposits', DepositViewSet, basename='deposit')
-budget_router.register(r'expense_categories', ExpenseCategoryViewSet, basename='expense_category')
-budget_router.register(r'income_categories', IncomeCategoryViewSet, basename='income_category')
 budget_router.register(r'entities', EntityViewSet, basename='entity')
 
 urlpatterns = [
