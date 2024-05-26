ac869c06d5682f26a692215951f698c41c7b3d7f
MateDawid <mateusiakdawid@gmail.com>
2024-05-26 12:26:55 +0200

    categories: IncomeCategory and ExpenseCategory models

--- backend/categories/models.py
+++ backend/categories/models.py
@@ -1,3 +1,74 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class IncomeCategory(models.Model):
+    """IncomeCategory model for grouping Income model instances."""
+
+    class IncomeGroups(models.IntegerChoices):
+        """Choices for group value."""
+
+        REGULAR = 1, 'Regular'
+        IRREGULAR = 2, 'Irregular'
+
+    budget = models.ForeignKey('budgets.Budget', on_delete=models.CASCADE, related_name='income_categories')
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='personal_income_categories',
+    )
+    is_active = models.BooleanField(default=True)
+    group = models.PositiveSmallIntegerField(choices=IncomeGroups.choices, null=False, blank=False)
+
+    class Meta:
+        verbose_name_plural = 'income categories'
+
+    def __str__(self) -> str:
+        """
+        Returns string representation of IncomeCategory model instance.
+
+        Returns:
+            str: Custom string representation of instance.
+        """
+        return f'{self.name} ({self.budget.name})'
+
+
+class ExpenseCategory(models.Model):
+    """ExpenseCategory model for grouping Income model instances."""
+
+    class ExpenseGroups(models.IntegerChoices):
+        """Choices for group value."""
+
+        MOST_IMPORTANT = 1, 'Most important'
+        DEBTS = 2, 'Debts'
+        SAVINGS = 3, 'Savings'
+        OTHERS = 4, 'Others'
+
+    budget = models.ForeignKey('budgets.Budget', on_delete=models.CASCADE, related_name='expense_categories')
+    name = models.CharField(max_length=128)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name='personal_expense_categories',
+    )
+    is_active = models.BooleanField(default=True)
+    group = models.PositiveSmallIntegerField(choices=ExpenseGroups.choices, null=False, blank=False)
+
+    class Meta:
+        verbose_name_plural = 'expense categories'
+
+    def __str__(self) -> str:
+        """
+        Returns string representation of ExpenseCategory model instance.
+
+        Returns:
+            str: Custom string representation of instance.
+        """
+        return f'{self.name} ({self.budget.name})'
