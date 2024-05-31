e8b5ab80562c7888aaf8d427510f3a5891b55e8b
MateDawid <mateusiakdawid@gmail.com>
2024-05-31 07:49:52 +0200

    categories: Filtersets for ExpenseCategoryViewSet and IncomeCategoryViewSet

--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,5 +1,6 @@
 from app_config.permissions import UserBelongsToBudgetPermission
 from budgets.mixins import BudgetMixin
+from categories.filters import ExpenseCategoryFilterSet, IncomeCategoryFilterSet
 from categories.models import ExpenseCategory, IncomeCategory
 from categories.serializers import (
     ExpenseCategorySerializer,
@@ -7,11 +8,9 @@ from categories.serializers import (
     TransferCategorySerializer,
 )
 from django.db.models import QuerySet
-
-# from django_filters import rest_framework as filters
+from django_filters import rest_framework as filters
 from rest_framework.authentication import TokenAuthentication
-
-# from rest_framework.filters import OrderingFilter
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -21,7 +20,7 @@ class TransferCategoryViewSet(BudgetMixin, ModelViewSet):
 
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticated, UserBelongsToBudgetPermission)
-    # filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
 
     def get_queryset(self) -> QuerySet:
         """
@@ -44,12 +43,12 @@ class TransferCategoryViewSet(BudgetMixin, ModelViewSet):
 class ExpenseCategoryViewSet(TransferCategoryViewSet):
     serializer_class = ExpenseCategorySerializer
     queryset = ExpenseCategory.objects.all()
-    # filterset_class = ExpenseCategoryFilterSet
-    # ordering = ('id', 'group', 'name')
+    filterset_class = ExpenseCategoryFilterSet
+    ordering = ('id', 'group', 'name')
 
 
 class IncomeCategoryViewSet(TransferCategoryViewSet):
     serializer_class = IncomeCategorySerializer
     queryset = IncomeCategory.objects.all()
-    # filterset_class = IncomeCategoryFilterSet
-    # ordering = ('id', 'group', 'name')
+    filterset_class = IncomeCategoryFilterSet
+    ordering = ('id', 'group', 'name')
