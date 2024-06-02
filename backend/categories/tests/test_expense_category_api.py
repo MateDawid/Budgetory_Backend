80f3dc0b752b2a53116b4ed2d92cf49cd85846a1
MateDawid <mateusiakdawid@gmail.com>
2024-06-02 08:43:56 +0200

    categories: TestExpenseCategoryApiList init

--- backend/categories/tests/test_expense_category_api.py
+++ backend/categories/tests/test_expense_category_api.py
@@ -1,5 +1,7 @@
 import pytest
 from categories.models import ExpenseCategory
+from categories.serializers import ExpenseCategorySerializer
+from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 from factory.base import FactoryMetaClass
 from rest_framework import status
@@ -78,115 +80,204 @@ class TestExpenseCategoryApiAccess:
         assert response.data['detail'] == 'User does not have access to Budget.'
 
 
-# @pytest.mark.django_db
-# class TestExpenseCategoryApiList:
-#     """Tests for list view on ExpenseCategoryViewSet."""
-#
-#     @pytest.mark.parametrize(
-#         'category_type, url, serializer',
-#         [
-#             pytest.param(CategoryType.INCOME, income_category_url, IncomeCategorySerializer),
-#             pytest.param(CategoryType.EXPENSE, expense_category_url, ExpenseCategorySerializer),
-#         ],
-#     )
-#     def test_retrieve_category_list_by_owner(
-#         self,
-#         api_client: APIClient,
-#         base_user: AbstractUser,
-#         budget_factory: FactoryMetaClass,
-#         expense_category_factory: FactoryMetaClass,
-#         category_type: CategoryType,
-#         url: Callable[[int], str],
-#         serializer: ExpenseCategorySerializer,
-#     ):
-#         """
-#         GIVEN: Two ExpenseCategory model instances for single Budget created in database.
-#         WHEN: ExpenseCategoryViewSet called by Budget owner.
-#         THEN: Response with serialized Budget ExpenseCategory list returned.
-#         """
-#         budget = budget_factory(owner=base_user)
-#         api_client.force_authenticate(base_user)
-#         for _ in range(2):
-#             expense_category_factory(budget=budget, category_type=category_type)
-#
-#         response = api_client.get(url(budget.id))
-#
-#         categories = ExpenseCategory.objects.filter(budget=budget, category_type=category_type.value)
-#         serializer = serializer(categories, many=True)  # noqa
-#         assert response.status_code == status.HTTP_200_OK
-#         assert response.data['results'] == serializer.data
-#
-#     @pytest.mark.parametrize(
-#         'category_type, url, serializer',
-#         [
-#             pytest.param(CategoryType.INCOME, income_category_url, IncomeCategorySerializer),
-#             pytest.param(CategoryType.EXPENSE, expense_category_url, ExpenseCategorySerializer),
-#         ],
-#     )
-#     def test_retrieve_category_list_by_member(
-#         self,
-#         api_client: APIClient,
-#         base_user: AbstractUser,
-#         budget_factory: FactoryMetaClass,
-#         expense_category_factory: FactoryMetaClass,
-#         category_type: CategoryType,
-#         url: Callable[[int], str],
-#         serializer: ExpenseCategorySerializer,
-#     ):
-#         """
-#         GIVEN: Two ExpenseCategory model instances for single Budget created in database.
-#         WHEN: ExpenseCategoryViewSet called by Budget member.
-#         THEN: Response with serialized Budget ExpenseCategory list returned.
-#         """
-#         budget = budget_factory(members=[base_user])
-#         api_client.force_authenticate(base_user)
-#         for _ in range(2):
-#             expense_category_factory(budget=budget, category_type=category_type)
-#
-#         response = api_client.get(url(budget.id))
-#
-#         categories = ExpenseCategory.objects.filter(budget=budget, category_type=category_type.value)
-#         serializer = serializer(categories, many=True)  # noqa
-#         assert response.status_code == status.HTTP_200_OK
-#         assert response.data['results'] == serializer.data
-#
-#     @pytest.mark.parametrize(
-#         'category_type, url, serializer',
-#         [
-#             pytest.param(CategoryType.INCOME, income_category_url, IncomeCategorySerializer),
-#             pytest.param(CategoryType.EXPENSE, expense_category_url, ExpenseCategorySerializer),
-#         ],
-#     )
-#     def test_category_list_limited_to_budget(
-#         self,
-#         api_client: APIClient,
-#         base_user: AbstractUser,
-#         budget_factory: FactoryMetaClass,
-#         expense_category_factory: FactoryMetaClass,
-#         category_type: CategoryType,
-#         url: Callable[[int], str],
-#         serializer: ExpenseCategorySerializer,
-#     ):
-#         """
-#         GIVEN: Two ExpenseCategory model instances for different Budgets created in database.
-#         WHEN: ExpenseCategoryViewSet called by one of Budgets owner.
-#         THEN: Response with serialized ExpenseCategory list (only from given Budget) returned.
-#         """
-#         budget = budget_factory(owner=base_user)
-#         category = expense_category_factory(budget=budget, category_type=category_type)
-#         expense_category_factory(category_type=category_type)
-#         api_client.force_authenticate(base_user)
-#
-#         response = api_client.get(url(budget.id))
-#
-#         categories = ExpenseCategory.objects.filter(budget=budget, category_type=category_type.value)
-#         serializer = serializer(categories, many=True)  # noqa
-#         assert response.status_code == status.HTTP_200_OK
-#         assert len(response.data['results']) == len(serializer.data) == categories.count() == 1
-#         assert response.data['results'] == serializer.data
-#         assert response.data['results'][0]['id'] == category.id
-#
-#
+@pytest.mark.django_db
+class TestExpenseCategoryApiList:
+    """Tests for list view on ExpenseCategoryViewSet."""
+
+    def test_retrieve_category_list_by_owner(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_category_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: Two ExpenseCategory model instances for single Budget created in database.
+        WHEN: ExpenseCategoryViewSet called by Budget owner.
+        THEN: Response with serialized Budget ExpenseCategory list returned.
+        """
+        budget = budget_factory(owner=base_user)
+        api_client.force_authenticate(base_user)
+        for _ in range(2):
+            expense_category_factory(budget=budget)
+
+        response = api_client.get(expense_category_url(budget.id))
+
+        categories = ExpenseCategory.objects.filter(budget=budget)
+        serializer = ExpenseCategorySerializer(categories, many=True)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['results'] == serializer.data
+
+    def test_retrieve_category_list_by_member(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_category_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: Two ExpenseCategory model instances for single Budget created in database.
+        WHEN: ExpenseCategoryViewSet called by Budget member.
+        THEN: Response with serialized Budget ExpenseCategory list returned.
+        """
+        budget = budget_factory(members=[base_user])
+        api_client.force_authenticate(base_user)
+        for _ in range(2):
+            expense_category_factory(budget=budget)
+
+        response = api_client.get(expense_category_url(budget.id))
+
+        categories = ExpenseCategory.objects.filter(budget=budget)
+        serializer = ExpenseCategorySerializer(categories, many=True)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['results'] == serializer.data
+
+    def test_category_list_limited_to_budget(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_category_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: Two ExpenseCategory model instances for different Budgets created in database.
+        WHEN: ExpenseCategoryViewSet called by one of Budgets owner.
+        THEN: Response with serialized ExpenseCategory list (only from given Budget) returned.
+        """
+        budget = budget_factory(owner=base_user)
+        category = expense_category_factory(budget=budget)
+        expense_category_factory()
+        api_client.force_authenticate(base_user)
+
+        response = api_client.get(expense_category_url(budget.id))
+
+        categories = ExpenseCategory.objects.filter(budget=budget)
+        serializer = ExpenseCategorySerializer(categories, many=True)
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.data['results']) == len(serializer.data) == categories.count() == 1
+        assert response.data['results'] == serializer.data
+        assert response.data['results'][0]['id'] == category.id
+
+    @pytest.mark.parametrize('sort_param', ('id', '-id', 'group', '-group', 'name', '-name'))
+    def test_get_categories_list_sorted_by_param(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_category_factory: FactoryMetaClass,
+        sort_param: str,
+    ):
+        """
+        GIVEN: Three ExpenseCategory objects created in database.
+        WHEN: The ExpenseCategoryViewSet list view is called with sorting by given param and without any filters.
+        THEN: Response must contain all ExpenseCategory existing in database sorted by given param.
+        """
+        budget = budget_factory(owner=base_user)
+        for _ in range(3):
+            expense_category_factory(budget=budget)
+        api_client.force_authenticate(base_user)
+
+        response = api_client.get(expense_category_url(budget.id), data={'ordering': sort_param})
+
+        assert response.status_code == status.HTTP_200_OK
+        categories = ExpenseCategory.objects.all().order_by(sort_param)
+        serializer = ExpenseCategorySerializer(categories, many=True)
+        assert response.data['results'] and serializer.data
+        assert len(response.data['results']) == len(serializer.data) == len(categories) == 3
+        assert response.data['results'] == serializer.data
+
+    @pytest.mark.parametrize(
+        'filter_value', ('Test', 'TEST', 'test', 'name', 'NAME', 'Name', 'Test name', 'TEST NAME', 'test name')
+    )
+    def test_get_categories_list_filtered_by_name(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_category_factory: FactoryMetaClass,
+        filter_value: str,
+    ):
+        """
+        GIVEN: Two ExpenseCategory objects for single Budget.
+        WHEN: The ExpenseCategoryViewSet list view is called with name filter.
+        THEN: Response must contain all ExpenseCategory existing in database assigned to Budget matching given
+        name value.
+        """
+        budget = budget_factory(owner=base_user)
+        category = expense_category_factory(name='Test name', budget=budget)
+        expense_category_factory(name='Other category', budget=budget)
+        api_client.force_authenticate(base_user)
+
+        response = api_client.get(expense_category_url(budget.id), data={'name': filter_value})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert ExpenseCategory.objects.all().count() == 2
+        categories = ExpenseCategory.objects.filter(budget=category.budget, name__icontains=filter_value)
+        serializer = ExpenseCategorySerializer(
+            categories,
+            many=True,
+        )
+        assert response.data['results'] and serializer.data
+        assert len(response.data['results']) == len(serializer.data) == categories.count() == 1
+        assert response.data['results'] == serializer.data
+        assert response.data['results'][0]['id'] == category.id
+
+    def test_get_categories_list_filtered_by_common_only_true(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_category_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: Two ExpenseCategory objects for single Budget.
+        WHEN: The ExpenseCategoryViewSet list view is called with True common_only filter.
+        THEN: Response must contain only common ExpenseCategory objects existing in database assigned to Budget.
+        """
+        budget = budget_factory(owner=base_user)
+        category = expense_category_factory(budget=budget, owner=None)
+        expense_category_factory(budget=budget, owner=base_user)
+        api_client.force_authenticate(base_user)
+
+        response = api_client.get(expense_category_url(budget.id), data={'common_only': True})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert ExpenseCategory.objects.all().count() == 2
+        categories = ExpenseCategory.objects.filter(budget=category.budget, owner__isnull=True)
+        serializer = ExpenseCategorySerializer(categories, many=True)
+        assert response.data['results'] and serializer.data
+        assert len(response.data['results']) == len(serializer.data) == categories.count() == 1
+        assert response.data['results'] == serializer.data
+        assert response.data['results'][0]['id'] == category.id
+
+    def test_get_categories_list_filtered_by_common_only_false(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_category_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: Two ExpenseCategory objects for single Budget.
+        WHEN: The ExpenseCategoryViewSet list view is called with False common_only filter.
+        THEN: Response must contain all ExpenseCategory objects existing in database assigned to Budget.
+        """
+        budget = budget_factory(owner=base_user)
+        expense_category_factory(budget=budget, owner=base_user)
+        expense_category_factory(budget=budget, owner=None)
+        api_client.force_authenticate(base_user)
+
+        response = api_client.get(expense_category_url(budget.id), data={'common_only': False})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert ExpenseCategory.objects.all().count() == 2
+        categories = ExpenseCategory.objects.filter(budget=budget)
+        serializer = ExpenseCategorySerializer(categories, many=True)
+        assert response.data['results'] and serializer.data
+        assert len(response.data['results']) == len(serializer.data) == categories.count() == 2
+        assert response.data['results'] == serializer.data
+
+
 # @pytest.mark.django_db
 # class TestExpenseCategoryApiCreate:
 #     """Tests for create ExpenseCategory on ExpenseCategoryViewSet."""
