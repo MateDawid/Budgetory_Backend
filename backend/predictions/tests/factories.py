77eca17b1bb19f90028a76d3d39646a5fa6a3025
MateDawid <mateusiakdawid@gmail.com>
2024-06-10 13:45:02 +0200

    predictions: Factory for ExpensePrediction

---
+++ backend/predictions/tests/factories.py
@@ -0,0 +1,15 @@
+import factory
+from budgets.tests.factories import BudgetingPeriodFactory
+from categories.tests.factories import ExpenseCategoryFactory
+
+
+class ExpensePredictionFactory(factory.django.DjangoModelFactory):
+    """Factory for ExpensePrediction model."""
+
+    class Meta:
+        model = 'predictions.ExpensePrediction'
+
+    period = factory.SubFactory(BudgetingPeriodFactory)
+    category = factory.SubFactory(ExpenseCategoryFactory)
+    value = factory.Faker('pyint', min_value=0, max_value=9999999999)
+    description = factory.Faker('text', max_nb_chars=255)
