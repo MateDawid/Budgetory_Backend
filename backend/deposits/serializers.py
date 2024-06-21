197f4b31a9d3cfe05485f81c57a468e929810c29
MateDawid <mateusiakdawid@gmail.com>
2024-06-21 09:48:31 +0200

    predictions: Deposits refactored with budget_pk usage

--- backend/deposits/serializers.py
+++ backend/deposits/serializers.py
@@ -1,5 +1,5 @@
+from app_users.models import User
 from deposits.models import Deposit
-from django.contrib.auth.models import AbstractUser
 from entities.models import Entity
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
@@ -28,25 +28,25 @@ class DepositSerializer(serializers.ModelSerializer):
         Raises:
             ValidationError: Raised if Deposit with given name exists in Budget already.
         """
-        if self.Meta.model.objects.filter(budget=self.context['view'].budget, name__iexact=name).exists():
+        if self.Meta.model.objects.filter(
+            budget__pk=self.context['view'].kwargs['budget_pk'], name__iexact=name
+        ).exists():
             raise ValidationError('Deposit with given name already exists in Budget.')
-        elif Entity.objects.filter(budget=self.context['view'].budget, name__iexact=name).exists():
+        elif Entity.objects.filter(budget__pk=self.context['view'].kwargs['budget_pk'], name__iexact=name).exists():
             raise ValidationError('Entity with given name already exists in Budget.')
         return name
 
-    def validate_owner(self, owner: AbstractUser | None):
+    def validate_owner(self, owner: User | None):
         """
         Checks if provided Deposit owner is part of Budget.
 
         Args:
-            owner [AbstractUser | None]:
+            owner [User | None]:
 
         Returns:
-            AbstractUser | None: User model instance or None.
+            User | None: User model instance or None.
         """
-        if owner and not (
-            owner == self.context['view'].budget.owner or owner in self.context['view'].budget.members.all()
-        ):
+        if owner and not owner.is_budget_member(self.context['view'].kwargs['budget_pk']):
             raise ValidationError('Provided owner does not belong to Budget.')
         return owner
 
