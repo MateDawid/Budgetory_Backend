b6a378a02aac5c57b580998a4fc9f8827f93caf4
MateDawid <mateusiakdawid@gmail.com>
2024-07-04 14:32:03 +0200

    predictions: Validating prediction value

--- backend/predictions/serializers.py
+++ backend/predictions/serializers.py
@@ -1,8 +1,10 @@
 from collections import OrderedDict
+from decimal import Decimal
 
 from django.db.models import Model
 from predictions.models import ExpensePrediction
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class ExpensePredictionSerializer(serializers.ModelSerializer):
@@ -12,6 +14,21 @@ class ExpensePredictionSerializer(serializers.ModelSerializer):
         model: Model = ExpensePrediction
         fields = ('id', 'period', 'category', 'value', 'description')
         read_only_fields = ['id']
+
+    @staticmethod
+    def validate_value(value: Decimal) -> Decimal:
+        """
+        Checks if provided value is higher than zero.
+
+        Args:
+            value [Decimal]: Value of given ExpensePrediction.
+
+        Returns:
+            Decimal: Validated value of ExpensePrediction.
+        """
+        if value <= Decimal('0.00'):
+            raise ValidationError('Value should be higher than 0.00.')
+        return value
 
     def to_representation(self, instance: ExpensePrediction) -> OrderedDict:
         """
