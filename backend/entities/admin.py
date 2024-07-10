393744208597561f249b5ee9de49654e0892e8ec
MateDawid <mateusiakdawid@gmail.com>
2024-07-10 08:06:20 +0200

    entities_refactor: Adjusted admin site for Entity and Deposit

--- backend/entities/admin.py
+++ backend/entities/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from entities.models import Entity
+from entities.models import Deposit, Entity
 
 
 @admin.register(Entity)
 class EntityAdmin(admin.ModelAdmin):
     """Custom admin view for Deposit model."""
 
-    list_display = ('name', 'budget', 'description', 'deposit')
-    list_filter = ('budget__name', 'budget__owner__email')
+    list_display = ('name', 'budget', 'description', 'is_active', 'is_deposit')
+    list_filter = ('budget__name', 'budget__owner__email', 'is_active')
+
+
+@admin.register(Deposit)
+class DepositAdmin(admin.ModelAdmin):
+    """Custom admin view for Deposit model."""
+
+    list_display = ('name', 'budget', 'description', 'is_active')
+    list_filter = ('budget__name', 'budget__owner__email', 'is_active')
