aa265393c3f86a50714f1794eba6b75aa804512f
MateDawid <mateusiakdawid@gmail.com>
2024-07-10 08:01:12 +0200

    entities_refactor: Entity model extension. Deposit as Entity proxy

--- backend/entities/models.py
+++ backend/entities/models.py
@@ -2,16 +2,15 @@ from django.db import models
 
 
 class Entity(models.Model):
-    """Entity model for seller (or source of income) representation."""
+    """Entity model for Transfer actor (payer or receiver) representation."""
 
     budget = models.ForeignKey(
         'budgets.Budget', on_delete=models.CASCADE, related_name='entities', null=False, blank=False
     )
     name = models.CharField(max_length=128)
     description = models.CharField(max_length=255, blank=True)
-    deposit = models.OneToOneField(
-        'deposits.Deposit', on_delete=models.SET_NULL, related_name='entity', null=True, blank=True
-    )
+    is_active = models.BooleanField(default=True)
+    is_deposit = models.BooleanField(default=False)
 
     class Meta:
         verbose_name_plural = 'entities'
@@ -22,3 +21,19 @@ class Entity(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.budget.name})'
+
+
+class Deposit(Entity):
+    """Deposit proxy model for Entity owned by Budget member representation."""
+
+    class DepositManager(models.Manager):
+        """Manager for Deposit Entities."""
+
+        def get_queryset(self):
+            return super().get_queryset().filter(is_deposit=True)
+
+    class Meta:
+        proxy = True
+        verbose_name_plural = 'deposits'
+
+    objects = DepositManager()
