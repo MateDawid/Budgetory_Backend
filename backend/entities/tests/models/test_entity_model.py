2d1b8621baa12baa94a1a72055735612810f389f
MateDawid <mateusiakdawid@gmail.com>
2024-07-15 12:21:31 +0200

    entities_refactor: Tests of Entity model

--- backend/entities/tests/models/test_entity_model.py
+++ backend/entities/tests/models/test_entity_model.py
@@ -1,8 +1,8 @@
 import pytest
 from budgets.models import Budget
+from django.core.exceptions import ValidationError
 from django.db import DataError, IntegrityError
 from entities.models import Entity
-from factory.base import FactoryMetaClass
 
 
 @pytest.mark.django_db
@@ -16,7 +16,27 @@ class TestEntityModel:
         'is_deposit': False,
     }
 
-    def test_create_entity_successfully(self, budget: Budget):
+    def test_save_entity(self, budget: Budget):
+        """
+        GIVEN: Budget model instance in database.
+        WHEN: Deposit instance save attempt with valid data.
+        THEN: Deposit model instance exists in database with given data.
+        """
+        payload = self.PAYLOAD.copy()
+        payload['budget'] = budget
+
+        deposit = Entity(**payload)
+        deposit.full_clean()
+        deposit.save()
+
+        for key in payload:
+            assert getattr(deposit, key) == payload[key]
+        assert Entity.objects.filter(budget=budget).count() == 1
+        assert Entity.deposits.filter(budget=budget).count() == 0
+        assert deposit.is_deposit is False
+        assert str(deposit) == f'{deposit.name} ({deposit.budget.name})'
+
+    def test_create_entity(self, budget: Budget):
         """
         GIVEN: Budget model instance in database.
         WHEN: Entity instance create attempt with valid data.
@@ -30,23 +50,49 @@ class TestEntityModel:
         for key in payload:
             assert getattr(entity, key) == payload[key]
         assert Entity.objects.filter(budget=budget).count() == 1
+        assert Entity.deposits.filter(budget=budget).count() == 0
+        assert entity.is_deposit is False
         assert str(entity) == f'{entity.name} ({entity.budget.name})'
 
-    def test_create_deposit(self, budget: Budget, entity_factory: FactoryMetaClass, deposit_factory: FactoryMetaClass):
+    def test_save_deposit(self, budget: Budget):
         """
-        GIVEN: Budget and two Entities (with is_deposit=False and is_deposit=True) models instances in database.
-        WHEN: Calling .deposits Entity manager for objects.
-        THEN: Manager returns only object with is_deposit=True.
+        GIVEN: Budget model instance in database.
+        WHEN: Entity instance save attempt with valid data and is_deposit=True.
+        THEN: Entity model instance exists in database with given data.
         """
-        entity_factory(budget=budget)
-        deposit = deposit_factory(budget=budget)
+        payload = self.PAYLOAD.copy()
+        payload['budget'] = budget
+        payload['is_deposit'] = True
 
-        entity_qs = Entity.objects.all()
-        deposit_qs = Entity.deposits.all()
+        entity = Entity(**payload)
+        entity.full_clean()
+        entity.save()
 
-        assert entity_qs.count() == 2
-        assert deposit_qs.count() == 1
-        assert deposit in deposit_qs
+        for key in payload:
+            assert getattr(entity, key) == payload[key]
+        assert Entity.objects.filter(budget=budget).count() == 1
+        assert Entity.deposits.filter(budget=budget).count() == 1
+        assert entity.is_deposit is True
+        assert str(entity) == f'{entity.name} ({entity.budget.name})'
+
+    def test_create_deposit(self, budget: Budget):
+        """
+        GIVEN: Budget model instance in database.
+        WHEN: Entity instance create attempt with valid data and is_deposit=True.
+        THEN: Entity model instance exists in database with given data.
+        """
+        payload = self.PAYLOAD.copy()
+        payload['budget'] = budget
+        payload['is_deposit'] = True
+
+        entity = Entity.objects.create(**payload)
+
+        for key in payload:
+            assert getattr(entity, key) == payload[key]
+        assert Entity.objects.filter(budget=budget).count() == 1
+        assert Entity.deposits.filter(budget=budget).count() == 1
+        assert entity.is_deposit is True
+        assert str(entity) == f'{entity.name} ({entity.budget.name})'
 
     @pytest.mark.django_db(transaction=True)
     @pytest.mark.parametrize('field_name', ['name', 'description'])
@@ -60,8 +106,20 @@ class TestEntityModel:
         payload = self.PAYLOAD.copy()
         payload[field_name] = (max_length + 1) * 'a'
 
+        # .full_clean() & .save() scenario
+        with pytest.raises(ValidationError) as exc:
+            deposit = Entity(**payload)
+            deposit.full_clean()
+
+        assert (
+            f'Ensure this value has at most {max_length} characters' in exc.value.error_dict[field_name][0].messages[0]
+        )
+        assert not Entity.objects.filter(budget=budget).exists()
+
+        # .create() scenario
         with pytest.raises(DataError) as exc:
             Entity.objects.create(**payload)
+
         assert str(exc.value) == f'value too long for type character varying({max_length})\n'
         assert not Entity.objects.filter(budget=budget).exists()
 
@@ -74,9 +132,17 @@ class TestEntityModel:
         """
         payload = self.PAYLOAD.copy()
         payload['budget'] = budget
-
         Entity.objects.create(**payload)
 
+        # .full_clean() & .save() scenario
+        with pytest.raises(ValidationError) as exc:
+            deposit = Entity(**payload)
+            deposit.full_clean()
+
+        assert 'Entity with this Name and Budget already exists.' in exc.value.error_dict['__all__'][0].messages[0]
+        assert Entity.objects.filter(budget=budget).count() == 1
+
+        # .create() scenario
         with pytest.raises(IntegrityError) as exc:
             Entity.objects.create(**payload)
         assert f'DETAIL:  Key (name, budget_id)=({payload["name"]}, {budget.id}) already exists.' in str(exc.value)
