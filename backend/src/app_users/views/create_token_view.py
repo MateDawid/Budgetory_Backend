2deff18465b0b83e8758d517022ef21e0ab92ec6
MateDawid <mateusiakdawid@gmail.com>
2024-07-29 11:45:52 +0200

    entities_refactor: app_users app reorganized

---
+++ backend/src/app_users/views/create_token_view.py
@@ -0,0 +1,10 @@
+from app_users.serializers.auth_token_serializer import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+class CreateTokenView(ObtainAuthToken):
+    """View to create new AuthToken for User."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
