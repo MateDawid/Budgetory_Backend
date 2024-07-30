ed133cd56b10a37aa2765be9c50c76298077a9f7
MateDawid <mateusiakdawid@gmail.com>
2024-07-30 12:10:11 +0200

    entities_refactor: docstrings and typing in test_user_model

--- backend/tests/app_users_tests/models/test_user_model.py
+++ backend/tests/app_users_tests/models/test_user_model.py
@@ -7,7 +7,11 @@ class TestUserModel:
     """Tests for User model"""
 
     def test_create_user_successful(self):
-        """Test creating a user with an email is successful."""
+        """
+        GIVEN: User email and password.
+        WHEN: UserManager.create_user() called with given data.
+        THEN: User created successfully.
+        """
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(email=email, password=password)
@@ -25,17 +29,29 @@ class TestUserModel:
         ],
     )
     def test_new_user_email_normalized(self, email: str, normalized_email: str):
-        """Test email is normalized for new users."""
+        """
+        GIVEN: Not normalized User emails.
+        WHEN: UserManager.create_user() called with given data.
+        THEN: User created with normalized email.
+        """
         user = get_user_model().objects.create_user(email, 'sample123')
         assert user.email == normalized_email
 
     def test_new_user_without_email_raises_error(self):
-        """Test that creating a user without an email raises a ValueError."""
+        """
+        GIVEN: Empty User email and password.
+        WHEN: UserManager.create_user() called with given data.
+        THEN: ValueError raised.
+        """
         with pytest.raises(ValueError):
             get_user_model().objects.create_user('', 'test123')
 
     def test_create_superuser(self):
-        """Test creating a superuser"""
+        """
+        GIVEN: User email and password.
+        WHEN: UserManager.create_superuser() called with given data.
+        THEN: User with superuser status created.
+        """
         user = get_user_model().objects.create_superuser('test@example.com', 'test123')
         assert user.is_superuser is True
         assert user.is_staff is True
