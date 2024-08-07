724489afc92bff75f96e2b670816509b17938f62
MateDawid <mateusiakdawid@gmail.com>
2024-08-07 08:18:37 +0200

    entities_refactor: Custom nested router

--- backend/src/budgets/urls.py
+++ backend/src/budgets/urls.py
@@ -1,4 +1,4 @@
-from app_infrastructure.routers import AppRouter
+from app_infrastructure.routers import AppNestedRouter, AppRouter
 from budgets.views.budget_viewset import BudgetViewSet
 from budgets.views.budgeting_period_viewset import BudgetingPeriodViewSet
 from categories.views import ExpenseCategoryViewSet, IncomeCategoryViewSet
@@ -6,14 +6,13 @@ from django.urls import include, path
 from entities.views.deposit_viewset import DepositViewSet
 from entities.views.entity_viewset import EntityViewSet
 from predictions.views import ExpensePredictionViewSet
-from rest_framework_nested.routers import NestedSimpleRouter
 
 app_name = 'budgets'
 
 router = AppRouter()
 router.register(r'', BudgetViewSet)
 
-budget_router = NestedSimpleRouter(router, r'', lookup='budget')
+budget_router = AppNestedRouter(router, r'', lookup='budget')
 budget_router.register(r'periods', BudgetingPeriodViewSet, basename='period')
 budget_router.register(r'deposits', DepositViewSet, basename='deposit')
 budget_router.register(r'entities', EntityViewSet, basename='entity')
