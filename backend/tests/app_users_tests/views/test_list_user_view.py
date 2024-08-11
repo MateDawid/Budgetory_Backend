533eccdeda3248a44811631870254ef35f487dab
MateDawid <mateusiakdawid@gmail.com>
2024-08-11 08:35:43 +0200

    entities_refactor: Fix test_getting_user_list_as_admin

--- backend/tests/app_users_tests/views/test_list_user_view.py
+++ backend/tests/app_users_tests/views/test_list_user_view.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-LIST_USER_URL = reverse('app_users:list')
+LIST_USER_URL = reverse("app_users:list")
 
 
 class TestListUserView:
@@ -20,13 +20,13 @@ class TestListUserView:
         WHEN: GET request on ListUserView.
         THEN: HTTP 200 returned.
         """
-        response = api_client.get(LIST_USER_URL)
         api_client.force_authenticate(superuser)
+        response = api_client.get(LIST_USER_URL)
 
-        users = get_user_model().objects.all().order_by('id')
+        users = get_user_model().objects.all().order_by("id")
         serializer = UserSerializer(users, many=True)
         assert response.status_code == status.HTTP_200_OK
-        assert response.data['results'] == serializer.data
+        assert response.data["results"] == serializer.data
 
     @pytest.mark.django_db
     def test_getting_user_list_as_base_user(self, api_client: APIClient, base_user: Any):
@@ -38,7 +38,7 @@ class TestListUserView:
         api_client.force_authenticate(base_user)
         response = api_client.get(LIST_USER_URL)
         assert response.status_code == status.HTTP_403_FORBIDDEN
-        assert response.data['detail'] == 'You do not have permission to perform this action.'
+        assert response.data["detail"] == "You do not have permission to perform this action."
 
     def test_getting_user_list_not_authenticated(self, api_client: APIClient):
         """
@@ -49,4 +49,4 @@ class TestListUserView:
         response = api_client.get(LIST_USER_URL)
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
-        assert str(response.data['detail']) == 'Authentication credentials were not provided.'
+        assert str(response.data["detail"]) == "Authentication credentials were not provided."
