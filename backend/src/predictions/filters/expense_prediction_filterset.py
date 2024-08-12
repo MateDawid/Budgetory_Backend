d242b7d9a305c2ff5401e798cb51b0d5502564b5
MateDawid <mateusiakdawid@gmail.com>
2024-08-12 11:48:22 +0200

    entities_refactor: predictions app refactored

--- backend/src/predictions/filters/expense_prediction_filterset.py
+++ backend/src/predictions/filters/expense_prediction_filterset.py
@@ -1,21 +1,22 @@
 from django.db.models import QuerySet
 from django_filters import rest_framework as filters
-from predictions.models import ExpensePrediction
+from predictions.models.expense_prediction_model import ExpensePrediction
 
 
 class ExpensePredictionFilterSet(filters.FilterSet):
     """FilterSet for ExpensePrediction endpoints."""
 
-    period_name = filters.CharFilter(method='get_period_name')
-    period_id = filters.NumberFilter(method='get_period_id')
-    category_name = filters.CharFilter(method='get_category_name')
-    category_id = filters.NumberFilter(method='get_category_id')
+    period_name = filters.CharFilter(method="get_period_name")
+    period_id = filters.NumberFilter(method="get_period_id")
+    category_name = filters.CharFilter(method="get_category_name")
+    category_id = filters.NumberFilter(method="get_category_id")
 
     class Meta:
         model = ExpensePrediction
-        fields = ['period_id', 'period_name', 'category_id', 'category_name']
+        fields = ["period_id", "period_name", "category_id", "category_name"]
 
-    def get_period_name(self, queryset: QuerySet, name: str, value: str):
+    @staticmethod
+    def get_period_name(queryset: QuerySet, name: str, value: str):
         """
         Filtering QuerySet with period name or its part.
 
@@ -28,7 +29,8 @@ class ExpensePredictionFilterSet(filters.FilterSet):
         """
         return queryset.filter(period__name__icontains=value)
 
-    def get_period_id(self, queryset: QuerySet, name: str, value: int):
+    @staticmethod
+    def get_period_id(queryset: QuerySet, name: str, value: int):
         """
         Filtering QuerySet with period id.
 
@@ -41,7 +43,8 @@ class ExpensePredictionFilterSet(filters.FilterSet):
         """
         return queryset.filter(period__id=value)
 
-    def get_category_name(self, queryset: QuerySet, name: str, value: str):
+    @staticmethod
+    def get_category_name(queryset: QuerySet, name: str, value: str):
         """
         Filtering QuerySet with category name or its part.
 
@@ -54,7 +57,8 @@ class ExpensePredictionFilterSet(filters.FilterSet):
         """
         return queryset.filter(category__name__icontains=value)
 
-    def get_category_id(self, queryset: QuerySet, name: str, value: int):
+    @staticmethod
+    def get_category_id(queryset: QuerySet, name: str, value: int):
         """
         Filtering QuerySet with category id.
 
