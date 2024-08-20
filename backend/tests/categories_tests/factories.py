23fade0fa2217be488a3c619f1c01d639cd95e54
MateDawid <mateusiakdawid@gmail.com>
2024-08-20 11:15:56 +0200

    transfer_category_proxy: TransferCategoryFactory with category_type and priority lazy attrs

--- backend/tests/categories_tests/factories.py
+++ backend/tests/categories_tests/factories.py
@@ -4,6 +4,62 @@ import factory.fuzzy
 from app_users_tests.factories import UserFactory
 from budgets_tests.factories import BudgetFactory
 from categories.models import ExpenseCategory, IncomeCategory
+from categories.models.category_priority_choices import CategoryPriority
+from categories.models.category_type_choices import CategoryType
+
+
+class TransferCategoryFactory(factory.django.DjangoModelFactory):
+    """Factory for TransferCategory model."""
+
+    class Meta:
+        model = "categories.TransferCategory"
+
+    budget = factory.SubFactory(BudgetFactory)
+    name = factory.Faker("text", max_nb_chars=128)
+    description = factory.Faker("text", max_nb_chars=255)
+    is_active = factory.Faker("boolean")
+
+    @factory.lazy_attribute
+    def owner(self) -> str:
+        """Generates user field value - User model instance or None."""
+        options = [UserFactory(), None]
+        return random.choice(options)
+
+    @factory.lazy_attribute
+    def category_type(self, *args) -> CategoryType:
+        """
+        Returns CategoryType matching priority value.
+
+        Returns:
+            CategoryType: Category type choice matching selected priority value.
+        """
+        priority = self._Resolver__step.builder.extras.get("priority")
+        if priority is None:
+            category_type = random.choice([choice for choice in CategoryType])
+        elif priority == CategoryPriority.INCOMES:
+            category_type = CategoryType.INCOME
+        else:
+            category_type = CategoryType.EXPENSE
+        self._Resolver__step.builder.extras["category_type"] = category_type
+        return category_type
+
+    @factory.lazy_attribute
+    def priority(self, *args) -> CategoryPriority:
+        """
+        Returns CategoryPriority matching category_type.
+
+        Returns:
+            CategoryPriority: Category priority choice matching selected category_type.
+        """
+        category_type = self._Resolver__step.builder.extras.get("category_type")
+        if category_type is None:
+            priority = random.choice([choice for choice in CategoryType])
+        elif category_type == CategoryType.INCOME:
+            priority = CategoryPriority.INCOMES
+        else:
+            priority = random.choice([choice for choice in CategoryPriority if choice != CategoryPriority.INCOMES])
+        self._Resolver__step.builder.extras["priority"] = priority
+        return priority
 
 
 class IncomeCategoryFactory(factory.django.DjangoModelFactory):
