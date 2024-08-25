69de52110d767330850a5b425ec13d9b075845c5
MateDawid <mateusiakdawid@gmail.com>
2024-08-25 09:01:04 +0200

    transfer_category_proxy: Code quality updated

--- backend/code_quality.ps1
+++ backend/code_quality.ps1
@@ -1,7 +1,7 @@
 Write-Output "[Quality check]";
 .\venv\Scripts\activate;
 Write-Output "* [black]";
-black .;
+black . --line-length=120;
 Write-Output "* [isort]";
 isort . --profile black;
 Write-Output "* [flake8]";
