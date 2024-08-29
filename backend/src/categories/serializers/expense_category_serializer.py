b43e46d0f39cad5c2177d3c3945006fa33923132
MateDawid <mateusiakdawid@gmail.com>
2024-08-29 08:20:57 +0200

    transfer_category_proxy: ExpenseCategory serializer and viewset

---
+++ backend/src/categories/serializers/expense_category_serializer.py
@@ -0,0 +1,11 @@
+from categories.models.expense_category_model import ExpenseCategory
+from categories.serializers.transfer_category_serializer import (
+    TransferCategorySerializer,
+)
+
+
+class ExpenseCategorySerializer(TransferCategorySerializer):
+    """Class for serializing ExpenseCategory model instances."""
+
+    class Meta(TransferCategorySerializer.Meta):
+        model = ExpenseCategory
--- backend/src/categories/views/income_category_viewset.py
+++ backend/src/categories/views/income_category_viewset.py
@@ -1,42 +1,8 @@
-from django.db.models import QuerySet
-from django_filters import rest_framework as filters
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
-
-from app_infrastructure.permissions import UserBelongsToBudgetPermission
-from categories.models.income_category_model import IncomeCategory
 from categories.serializers.income_category_serializer import IncomeCategorySerializer
+from categories.views.transfer_category_viewset import TransferCategoryViewSet
 
 
-class IncomeCategoryViewSet(ModelViewSet):
-    """Base view for managing TransferCategories."""
+class IncomeCategoryViewSet(TransferCategoryViewSet):
+    """ViewSet for managing IncomeCategories."""
 
     serializer_class = IncomeCategorySerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, UserBelongsToBudgetPermission)
-    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
-    ordering = ("id", "category_type", "priority")
-
-    def get_queryset(self) -> QuerySet:
-        """
-        Retrieve IncomeCategories for Budget passed in URL.
-
-        Returns:
-            QuerySet: Filtered TransferCategory QuerySet.
-        """
-        return (
-            IncomeCategory.objects.prefetch_related("budget", "owner")
-            .filter(budget__pk=self.kwargs.get("budget_pk"))
-            .distinct()
-        )
-
-    def perform_create(self, serializer: IncomeCategorySerializer) -> None:
-        """
-        Additionally save Budget from URL on TransferCategory instance during saving serializer.
-
-        Args:
-            serializer [TransferCategorySerializer]: Serializer for TransferCategory model.
-        """
-        serializer.save(budget_id=self.kwargs.get("budget_pk"))
