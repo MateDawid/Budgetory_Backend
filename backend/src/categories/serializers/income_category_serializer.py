5e7cc7272573b8e95d4d1936e76bbd89228e8e78
MateDawid <mateusiakdawid@gmail.com>
2024-08-29 08:08:23 +0200

    transfer_category_proxy: TransferCategory and IncomeCategory serializers

--- backend/src/categories/serializers/income_category_serializer.py
+++ backend/src/categories/serializers/income_category_serializer.py
@@ -1,36 +1,11 @@
-from collections import OrderedDict
-
-from django.db.models import Model
-from rest_framework import serializers
-
-from app_users.models import User
-from categories.models import TransferCategory
-from categories.models.expense_category_model import ExpenseCategory
 from categories.models.income_category_model import IncomeCategory
-
-CATEGORY_NAME_ERRORS = {
-    "PERSONAL": "Personal {class_name} with given name already exists in Budget for provided owner.",
-    "COMMON": "Common {class_name} with given name already exists in Budget.",
-}
+from categories.serializers.transfer_category_serializer import (
+    TransferCategorySerializer,
+)
 
 
-class TransferCategorySerializer(serializers.ModelSerializer):
-    """Base class for TransferCategorySerializers"""
+class IncomeCategorySerializer(TransferCategorySerializer):
+    """Class for serializing IncomeCategory model instances."""
 
-    class Meta:
-        model = TransferCategory
-        fields = ["id", "name", "description", "is_active", "owner", "priority"]
-        read_only_fields = ["id"]
-
-    def validate(self, attrs: OrderedDict) -> OrderedDict:
-        """
-        Validates TransferCategory.
-        Args:
-            attrs [OrderedDict]: Dictionary containing given TransferCategory params
-        Returns:
-            OrderedDict: Dictionary with validated attrs values.
-        """
-
-        x = TransferCategory(**attrs).full_clean()
-
-        return attrs
+    class Meta(TransferCategorySerializer.Meta):
+        model = IncomeCategory
