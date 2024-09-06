91ee27622f7e053217fdfcccc057e89061b12ebc
MateDawid <mateusiakdawid@gmail.com>
2024-09-06 09:42:46 +0200

    transfer_category_proxy: Reordered transfer categories choices

--- backend/src/categories/models/transfer_category_choices.py
+++ backend/src/categories/models/transfer_category_choices.py
@@ -4,8 +4,8 @@ from django.db import models
 class CategoryType(models.IntegerChoices):
     """Choices for TransferCategory type value."""
 
-    INCOME = 1, "Income"
-    EXPENSE = 2, "Expense"
+    EXPENSE = 1, "Expense"
+    INCOME = 2, "Income"
 
 
 class ExpenseCategoryPriority(models.IntegerChoices):
