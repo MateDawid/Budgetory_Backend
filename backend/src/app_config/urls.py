3f90f5c9367a9b4336fde4a2777d3ab0e8545165
MateDawid <mateusiakdawid@gmail.com>
2024-09-19 08:40:56 +0200

    predictions_refactor: HealthcheckView and DatabaseConnectionService

--- backend/src/app_config/urls.py
+++ backend/src/app_config/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions, routers
+
+from app_infrastructure.views.healthcheck_view import HealthcheckView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -25,7 +26,7 @@ urlpatterns = [
     path("api/swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("api/swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("api/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("api/healthcheck", lambda r: HttpResponse()),
+    path("api/healthcheck", HealthcheckView.as_view(), name="healthcheck"),
     path("api/admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api/users/", include("app_users.urls")),
