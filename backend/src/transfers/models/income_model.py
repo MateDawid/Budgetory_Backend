1060021b60573ae5450f8ff11e03a37964b0d3c7
MateDawid <mateusiakdawid@gmail.com>
2024-09-21 08:24:58 +0200

    transfers: Placeholders for Income and Expense proxy models

---
+++ backend/src/transfers/models/income_model.py
@@ -0,0 +1,11 @@
+from transfers.models.transfer_model import Transfer
+
+
+class Income(Transfer):
+    """Income proxy model for Transfer with IncomeCategory as category."""
+
+    # objects = IncomeManager()
+
+    class Meta:
+        proxy = True
+        verbose_name_plural = "incomes"
