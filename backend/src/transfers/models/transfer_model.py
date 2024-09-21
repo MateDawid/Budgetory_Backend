482b59a047a0c57466cfdda6f4e100a822daf306
MateDawid <mateusiakdawid@gmail.com>
2024-09-21 08:21:30 +0200

    transfers: Transfer model

---
+++ backend/src/transfers/models/transfer_model.py
@@ -0,0 +1,42 @@
+from decimal import Decimal
+
+from django.db import models
+
+
+class TransferCategory(models.Model):
+    """Transfer model for representing cash flow between Entities"""
+
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField(null=False, blank=False)
+    period = models.ForeignKey("budgets.BudgetingPeriod", on_delete=models.PROTECT, related_name="transfers")
+    entity = models.ForeignKey("entities.Entity", on_delete=models.PROTECT, related_name="entity_transfers")
+    deposit = models.ForeignKey("entities.Deposit", on_delete=models.PROTECT, related_name="deposit_transfers")
+    category = models.ForeignKey("categories.TransferCategory", on_delete=models.PROTECT, related_name="transfers")
+
+    objects = models.Manager()
+    # incomes = IncomeManager()
+    # expenses = ExpenseManager()
+
+    class Meta:
+        verbose_name_plural = "transfers"
+        constraints = (
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_value_gt_0",
+                check=models.Q(value__gt=Decimal("0.00")),
+            ),
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_deposit_and_entity_not_the_same",
+                check=models.Q(_negated=True, entity__pk=models.F("deposit__pk")),
+            ),
+        )
+
+    def __str__(self) -> str:
+        """
+        Returns string representation of Transfer model instance.
+
+        Returns:
+            str: Custom string representation of instance.
+        """
+        return f"{self.date} | {self.category} | {self.value}"
