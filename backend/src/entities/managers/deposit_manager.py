ef7117e6f8cd5bd2341ed469db046c81a03e1ff9
MateDawid <mateusiakdawid@gmail.com>
2024-09-30 10:45:49 +0200

    transfers: Fix DepositManager and its tests

--- backend/src/entities/managers/deposit_manager.py
+++ backend/src/entities/managers/deposit_manager.py
@@ -2,6 +2,30 @@ from django.db import models
 from django.db.models import Model, QuerySet
 
 
+class DepositQuerySet(QuerySet):
+    """Custom DepositQuerySet for handling Deposit QuerySets."""
+
+    def create(self, **kwargs) -> Model:
+        """
+        Sets is_deposit param to True on object creation.
+
+        Returns:
+            Model: Deposit model instance.
+        """
+        kwargs["is_deposit"] = True
+        return super().create(**kwargs)
+
+    def update(self, **kwargs) -> int:
+        """
+        Sets is_deposit param to True on objects update.
+
+        Returns:
+            int: Number of affected database rows.
+        """
+        kwargs["is_deposit"] = True
+        return super().update(**kwargs)
+
+
 class DepositManager(models.Manager):
     """Manager for Deposit Entities."""
 
@@ -12,24 +36,4 @@ class DepositManager(models.Manager):
         Returns:
             QuerySet: QuerySet containing only Entities with is_deposit=True.
         """
-        return super().get_queryset().filter(is_deposit=True)
-
-    def create(self, *args, **kwargs) -> Model:
-        """
-        Sets is_deposit param to True on object creation.
-
-        Returns:
-            Model: Deposit model instance.
-        """
-        kwargs["is_deposit"] = True
-        return super().create(*args, **kwargs)
-
-    def update(self, *args, **kwargs) -> int:
-        """
-        Sets is_deposit param to True on objects update.
-
-        Returns:
-            int: Number of affected database rows.
-        """
-        kwargs["is_deposit"] = True
-        return super().update(**kwargs)
+        return DepositQuerySet(self.model, using=self._db).filter(is_deposit=True)
