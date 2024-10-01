b95e2775d0fcf6f86f44137eb394d027c9dff94c
MateDawid <mateusiakdawid@gmail.com>
2024-10-01 12:11:51 +0200

    transfers: Extended save() for Income model

--- backend/src/transfers/models/income_model.py
+++ backend/src/transfers/models/income_model.py
@@ -1,3 +1,6 @@
+from django.core.exceptions import ValidationError
+
+from categories.models.transfer_category_choices import CategoryType
 from transfers.managers.income_manager import IncomeManager
 from transfers.models.transfer_model import Transfer
 
@@ -10,3 +13,8 @@ class Income(Transfer):
     class Meta:
         proxy = True
         verbose_name_plural = "incomes"
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.category.category_type == CategoryType.INCOME:
+            raise ValidationError("Income model instance can not be created with ExpenseCategory.")
+        super().save(*args, **kwargs)
