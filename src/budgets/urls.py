8b623d87c0a3e0c1ce60c7992cb98d83f89746c8
MateDawid <mateusiakdawid@gmail.com>
2024-10-26 15:36:52 +0200

    wallets: WalletDeposit serializer and viewset

--- src/budgets/urls.py
+++ src/budgets/urls.py
@@ -10,6 +10,7 @@ from entities.views.entity_viewset import EntityViewSet
 from predictions.views.expense_prediction_viewset import ExpensePredictionViewSet
 from transfers.views.expense_viewset import ExpenseViewSet
 from transfers.views.income_viewset import IncomeViewSet
+from wallets.views.wallet_deposit_viewset import WalletDepositViewSet
 from wallets.views.wallet_viewset import WalletViewSet
 
 app_name = "budgets"
@@ -28,7 +29,11 @@ budget_router.register(r"incomes", IncomeViewSet, basename="income")
 budget_router.register(r"expenses", ExpenseViewSet, basename="expense")
 budget_router.register(r"wallets", WalletViewSet, basename="wallet")
 
+wallet_router = AppNestedRouter(budget_router, r"wallets", lookup="wallet")
+wallet_router.register(r"deposits", WalletDepositViewSet, basename="wallet_deposit")
+
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(budget_router.urls)),
+    path("", include(wallet_router.urls)),
 ]
