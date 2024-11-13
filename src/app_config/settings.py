65aff4a9ef818a0d70b35b9572bc7afd711c0a13
MateDawid <mateusiakdawid@gmail.com>
2024-11-13 14:21:38 +0100

    jwt: JWT settings

--- src/app_config/settings.py
+++ src/app_config/settings.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from pathlib import Path
 
@@ -153,7 +154,7 @@ AUTH_USER_MODEL = "app_users.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "app_infrastructure.paginations.DefaultPagination",
@@ -161,6 +162,12 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=1),
+    "USER_ID_CLAIM": "id",
 }
 
 SWAGGER_SETTINGS = {
