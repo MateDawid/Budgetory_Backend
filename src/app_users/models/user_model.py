bc7603afff7814edd29c7bfa7f54ca5585ed9ffe
MateDawid <mateusiakdawid@gmail.com>
2024-11-13 14:23:23 +0100

    jwt: User model updated

--- src/app_users/models/user_model.py
+++ src/app_users/models/user_model.py
@@ -8,9 +8,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     """App User database model."""
 
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = UserManager()
 
