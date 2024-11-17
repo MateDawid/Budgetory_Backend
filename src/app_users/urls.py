3a663249e25893be32507d7dd74bfac75b5db150
MateDawid <mateusiakdawid@gmail.com>
2024-11-17 09:10:34 +0100

    jwt: Login serializer and view

--- src/app_users/urls.py
+++ src/app_users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
+from app_users.views.user_login_view import UserLoginView
 from app_users.views.user_register_view import UserRegisterView
 
 app_name = "app_users"
 
 urlpatterns = [
-    # path("", ListUserView.as_view(), name="list"),
     path("register/", UserRegisterView.as_view(), name="register"),
-    # path("token/", CreateTokenView.as_view(), name="token"),
-    # path("me/", AuthenticatedUserView.as_view(), name="me"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
