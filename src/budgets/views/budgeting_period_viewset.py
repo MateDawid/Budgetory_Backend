3a8758dd0171d923e35ef07532f06f06f97a82f9
JE01TD <dawid.mateusiak@ing.com>
2025-02-16 06:37:27 +0100

    periods_filter: Ordering and filtering BudgetingPeriods

--- src/budgets/views/budgeting_period_viewset.py
+++ src/budgets/views/budgeting_period_viewset.py
@@ -1,8 +1,11 @@
 from django.db.models import Q, QuerySet
+from django_filters import rest_framework as filters
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from app_infrastructure.permissions import UserBelongsToBudgetPermission
+from budgets.filtersets.budgeting_period_filterset import BudgetingPeriodFilterSet
 from budgets.models import BudgetingPeriod
 from budgets.serializers.budgeting_period_serializer import BudgetingPeriodSerializer
 
@@ -13,6 +16,14 @@ class BudgetingPeriodViewSet(ModelViewSet):
     serializer_class = BudgetingPeriodSerializer
     queryset = BudgetingPeriod.objects.all()
     permission_classes = [IsAuthenticated, UserBelongsToBudgetPermission]
+    filterset_class = BudgetingPeriodFilterSet
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    ordering_fields = (
+        "id",
+        "name",
+        "date_start",
+        "date_end",
+    )
 
     def get_queryset(self) -> QuerySet:
         """
