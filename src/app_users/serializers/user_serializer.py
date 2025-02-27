1d26e9d040bb43b72ab7a19770ee7382c0e27f32
JE01TD <dawid.mateusiak@ing.com>
2025-02-27 14:33:14 +0100

    budget_members: Budget view adjusted to not having owner field and username usage

---
+++ src/app_users/serializers/user_serializer.py
@@ -0,0 +1,11 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Class for serializing User model."""
+
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "email", "username")
+        read_only_fields = ("id",)
