265a9e7476aac2d702be1eaf5ab507de70f6544f
MateDawid <mateusiakdawid@gmail.com>
2025-03-03 14:19:27 +0100

    categories: UserSerializer extended with value and label fields

--- src/app_users/serializers/user_serializer.py
+++ src/app_users/serializers/user_serializer.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from app_users.models import User
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Class for serializing User model."""
@@ -9,3 +11,18 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ("id", "email", "username")
         read_only_fields = ("id",)
+
+    def to_representation(self, instance: User):
+        """
+        Extends model representation with "value" and "label" fields for React MUI DataGrid filtering purposes.
+
+        Attributes:
+            instance [AbstractUser]: AbstractUser model instance
+
+        Returns:
+            OrderedDict: Dictionary containing additional fields.
+        """
+        representation = super().to_representation(instance)
+        representation["value"] = instance.id
+        representation["label"] = instance.username
+        return representation
