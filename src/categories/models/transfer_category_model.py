e5ceb7d2f97cc0f238331271986318401c92be0b
JE01TD <dawid.mateusiak@ing.com>
2025-03-05 13:40:17 +0100

    budget_members: TransferCategory model for both Income and Expense categories

--- src/categories/models/transfer_category_model.py
+++ src/categories/models/transfer_category_model.py
@@ -2,9 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.db.models import CheckConstraint, Q, UniqueConstraint
 
-from categories.managers.expense_category_manager import ExpenseCategoryManager
-from categories.managers.income_category_manager import IncomeCategoryManager
-from categories.models.transfer_category_choices import CategoryType, ExpenseCategoryPriority, IncomeCategoryPriority
+from categories.models.choices.category_priority import CategoryPriority
+from categories.models.choices.category_type import CategoryType
 
 
 class TransferCategory(models.Model):
@@ -22,13 +21,7 @@ class TransferCategory(models.Model):
     )
     is_active = models.BooleanField(default=True)
     category_type = models.PositiveSmallIntegerField(choices=CategoryType.choices, null=False, blank=False)
-    priority = models.PositiveSmallIntegerField(
-        choices=ExpenseCategoryPriority.choices + IncomeCategoryPriority.choices, null=False, blank=False
-    )
-
-    objects = models.Manager()
-    income_categories = IncomeCategoryManager()
-    expense_categories = ExpenseCategoryManager()
+    priority = models.PositiveSmallIntegerField(choices=CategoryPriority.choices, null=False, blank=False)
 
     class Meta:
         verbose_name_plural = "transfer categories"
@@ -36,8 +29,8 @@ class TransferCategory(models.Model):
             CheckConstraint(
                 name="%(app_label)s_%(class)s_correct_priority_for_type",
                 check=(
-                    Q(category_type=CategoryType.INCOME, priority__in=IncomeCategoryPriority.values)
-                    | Q(category_type=CategoryType.EXPENSE, priority__in=ExpenseCategoryPriority.values)
+                    Q(category_type=CategoryType.INCOME, priority__in=CategoryPriority.income_priorities())
+                    | Q(category_type=CategoryType.EXPENSE, priority__in=CategoryPriority.expense_priorities())
                 ),
             ),
             UniqueConstraint(
