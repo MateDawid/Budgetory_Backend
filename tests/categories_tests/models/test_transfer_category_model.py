81ab833fbca8f78adabf351475bebe346585d797
JE01TD <dawid.mateusiak@ing.com>
2025-03-06 20:24:28 +0100

    budget_members: TYPE_AND_PRIORITY_COMBINATIONS in utils

--- tests/categories_tests/models/test_transfer_category_model.py
+++ tests/categories_tests/models/test_transfer_category_model.py
@@ -1,4 +1,5 @@
 import pytest
+from categories_tests.utils import INVALID_TYPE_AND_PRIORITY_COMBINATIONS, VALID_TYPE_AND_PRIORITY_COMBINATIONS
 from django.db import DataError, IntegrityError
 from factory.base import FactoryMetaClass
 
@@ -6,66 +7,6 @@ from budgets.models.budget_model import Budget
 from categories.models.choices.category_priority import CategoryPriority
 from categories.models.choices.category_type import CategoryType
 from categories.models.transfer_category_model import TransferCategory
-
-VALID_TYPE_AND_PRIORITY_COMBINATIONS = (
-    pytest.param(
-        CategoryType.INCOME,
-        CategoryPriority.REGULAR,
-        id=f"{CategoryType.INCOME.label}-{CategoryPriority.REGULAR.label}",
-    ),
-    pytest.param(
-        CategoryType.INCOME,
-        CategoryPriority.IRREGULAR,
-        id=f"{CategoryType.INCOME.label}-{CategoryPriority.IRREGULAR.label}",
-    ),
-    pytest.param(
-        CategoryType.EXPENSE,
-        CategoryPriority.MOST_IMPORTANT,
-        id=f"{CategoryType.EXPENSE.label}-{CategoryPriority.MOST_IMPORTANT.label}",
-    ),
-    pytest.param(
-        CategoryType.EXPENSE, CategoryPriority.DEBTS, id=f"{CategoryType.EXPENSE.label}-{CategoryPriority.DEBTS.label}"
-    ),
-    pytest.param(
-        CategoryType.EXPENSE,
-        CategoryPriority.SAVINGS,
-        id=f"{CategoryType.EXPENSE.label}-{CategoryPriority.SAVINGS.label}",
-    ),
-    pytest.param(
-        CategoryType.EXPENSE,
-        CategoryPriority.OTHERS,
-        id=f"{CategoryType.EXPENSE.label}-{CategoryPriority.OTHERS.label}",
-    ),
-)
-
-INVALID_TYPE_AND_PRIORITY_COMBINATIONS = (
-    pytest.param(
-        CategoryType.EXPENSE,
-        CategoryPriority.REGULAR,
-        id=f"{CategoryType.EXPENSE.label}-{CategoryPriority.REGULAR.label}",
-    ),
-    pytest.param(
-        CategoryType.EXPENSE,
-        CategoryPriority.IRREGULAR,
-        id=f"{CategoryType.EXPENSE.label}-{CategoryPriority.IRREGULAR.label}",
-    ),
-    pytest.param(
-        CategoryType.INCOME,
-        CategoryPriority.MOST_IMPORTANT,
-        id=f"{CategoryType.INCOME.label}-{CategoryPriority.MOST_IMPORTANT.label}",
-    ),
-    pytest.param(
-        CategoryType.INCOME, CategoryPriority.DEBTS, id=f"{CategoryType.INCOME.label}-{CategoryPriority.DEBTS.label}"
-    ),
-    pytest.param(
-        CategoryType.INCOME,
-        CategoryPriority.SAVINGS,
-        id=f"{CategoryType.INCOME.label}-{CategoryPriority.SAVINGS.label}",
-    ),
-    pytest.param(
-        CategoryType.INCOME, CategoryPriority.OTHERS, id=f"{CategoryType.INCOME.label}-{CategoryPriority.OTHERS.label}"
-    ),
-)
 
 
 @pytest.mark.django_db
