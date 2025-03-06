5db6db2f7687cc945032bc32a970c58f690d851a
JE01TD <dawid.mateusiak@ing.com>
2025-03-06 20:28:22 +0100

    budget_members: CategoryPriority viewset

--- src/categories/urls.py
+++ src/categories/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from categories.views.expense_category_priority_view import ExpenseCategoryPriorityView
-from categories.views.income_category_priority_view import IncomeCategoryPriorityView
+from categories.views.category_priority_view import CategoryPriorityView
 
 app_name = "categories"
 
 urlpatterns = [
-    path("priorities/expense", ExpenseCategoryPriorityView.as_view(), name="expense-category-priorities"),
-    path("priorities/income", IncomeCategoryPriorityView.as_view(), name="income-category-priorities"),
+    path("priorities", CategoryPriorityView.as_view(), name="category-priorities"),
 ]
--- tests/categories_tests/views/test_category_priority_view.py
+++ tests/categories_tests/views/test_category_priority_view.py
@@ -3,34 +3,34 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from categories.models.choices.transfer_category_choices import ExpenseCategoryPriority
+from categories.models.choices.category_priority import CategoryPriority
 
-EXPENSE_CATEGORY_PRIORITY_URL = reverse("categories:expense-category-priorities")
+CATEGORY_PRIORITY_URL = reverse("categories:category-priorities")
 
 
 @pytest.mark.django_db
-class TestExpenseCategoryPriorityList:
+class TestCategoryPriorityList:
     """Tests for list view on ExpenseCategoryViewSet."""
 
     def test_auth_not_required(self, api_client: APIClient):
         """
-        GIVEN: ExpenseCategoryPriority choices defined in application.
+        GIVEN: CategoryPriority choices defined in application.
         WHEN: Performing HTTP GET request on EXPENSE_CATEGORY_PRIORITY_URL without authentication.
         THEN: HTTP_200_OK returned, endpoint accessible without authentication.
         """
-        response = api_client.get(EXPENSE_CATEGORY_PRIORITY_URL)
+        response = api_client.get(CATEGORY_PRIORITY_URL)
 
         assert response.status_code == status.HTTP_200_OK
 
     def test_get_priorities_list(self, api_client: APIClient):
         """
-        GIVEN: ExpenseCategoryPriority choices defined in application.
+        GIVEN: CategoryPriority choices defined in application.
         WHEN: Performing HTTP GET request on EXPENSE_CATEGORY_PRIORITY_URL.
-        THEN: HTTP_200_OK, choices for ExpenseCategoryPriority returned.
+        THEN: HTTP_200_OK, choices for CategoryPriority returned.
         """
-        response = api_client.get(EXPENSE_CATEGORY_PRIORITY_URL)
+        response = api_client.get(CATEGORY_PRIORITY_URL)
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data["results"] == [
-            {"value": choice[0], "label": choice[1]} for choice in ExpenseCategoryPriority.choices
+            {"value": choice[0], "label": choice[1]} for choice in CategoryPriority.choices
         ]
