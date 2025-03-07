ee4ae97aeae5a5532955cfb62d89ff9e7814969d
MateDawid <mateusiakdawid@gmail.com>
2025-03-07 07:31:03 +0100

    budget_members: Reorder CategoryType

--- src/categories/models/choices/category_type.py
+++ src/categories/models/choices/category_type.py
@@ -4,5 +4,5 @@ from django.db import models
 class CategoryType(models.IntegerChoices):
     """Choices for TransferCategory type value."""
 
-    EXPENSE = 1, "Expense"
-    INCOME = 2, "Income"
+    INCOME = 1, "📈 Income"
+    EXPENSE = 2, "📉 Expense"
