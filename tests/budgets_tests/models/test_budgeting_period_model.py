ecc76735b40a4759171016eea1fea1bd71cf71cb
MateDawid <mateusiakdawid@gmail.com>
2025-03-22 06:03:20 +0100

    expense_predictions_adjust: BudgetingPeriod model tests adjustments

--- tests/budgets_tests/models/test_budgeting_period_model.py
+++ tests/budgets_tests/models/test_budgeting_period_model.py
@@ -3,10 +3,10 @@ from datetime import date
 import pytest
 from django.core.exceptions import ValidationError
 from django.db import DataError, IntegrityError
-from factory.base import FactoryMetaClass
 
 from budgets.models.budget_model import Budget
 from budgets.models.budgeting_period_model import BudgetingPeriod
+from budgets.models.choices.period_status import PeriodStatus
 
 
 @pytest.mark.django_db
@@ -22,6 +22,7 @@ class TestBudgetingPeriodModel:
         payload = {
             "name": "2023_01",
             "budget": budget,
+            "status": PeriodStatus.ACTIVE,
             "date_start": date(2023, 1, 1),
             "date_end": date(2023, 1, 31),
         }
@@ -30,7 +31,6 @@ class TestBudgetingPeriodModel:
 
         for k, v in payload.items():
             assert getattr(period, k) == v
-        assert period.is_active is False
         assert BudgetingPeriod.objects.filter(budget=budget).count() == 1
         assert str(period) == f"{period.name} ({period.budget.name})"
 
@@ -43,12 +43,14 @@ class TestBudgetingPeriodModel:
         payload_1 = {
             "name": "2023_01",
             "budget": budget,
+            "status": PeriodStatus.ACTIVE,
             "date_start": date(2023, 1, 1),
             "date_end": date(2023, 1, 31),
         }
         payload_2 = {
             "name": "2023_02",
             "budget": budget,
+            "status": PeriodStatus.DRAFT,
             "date_start": date(2023, 2, 1),
             "date_end": date(2023, 2, 28),
         }
@@ -57,36 +59,7 @@ class TestBudgetingPeriodModel:
         for budgeting_period, payload in [(budgeting_period_1, payload_1), (budgeting_period_2, payload_2)]:
             for k, v in payload.items():
                 assert getattr(budgeting_period, k) == v
-            assert budgeting_period.is_active is False
         assert BudgetingPeriod.objects.filter(budget=budget).count() == 2
-
-    def test_creating_same_period_for_two_budgets(self, budget_factory: FactoryMetaClass):
-        """
-        GIVEN: Two Budget model instances in database.
-        WHEN: Two BudgetingPeriod instances - both for different Budgets - create attempt with valid data.
-        THEN: Two BudgetingPeriod instances for different Budgets existing in database.
-        """
-        payload_1 = {
-            "name": "2023_01",
-            "budget": budget_factory(),
-            "date_start": date(2023, 1, 1),
-            "date_end": date(2023, 1, 31),
-            "is_active": True,
-        }
-        payload_2 = {
-            "name": "2023_01",
-            "budget": budget_factory(),
-            "date_start": date(2023, 1, 1),
-            "date_end": date(2023, 1, 31),
-            "is_active": True,
-        }
-
-        BudgetingPeriod.objects.create(**payload_1)
-        BudgetingPeriod.objects.create(**payload_2)
-
-        assert BudgetingPeriod.objects.all().count() == 2
-        assert BudgetingPeriod.objects.filter(budget=payload_1["budget"]).count() == 1
-        assert BudgetingPeriod.objects.filter(budget=payload_2["budget"]).count() == 1
 
     @pytest.mark.django_db(transaction=True)
     def test_error_name_too_long(self, budget: Budget):
@@ -99,6 +72,7 @@ class TestBudgetingPeriodModel:
         payload = {
             "name": (max_length + 1) * "a",
             "budget": budget,
+            "status": PeriodStatus.ACTIVE,
             "date_start": date(2023, 1, 1),
             "date_end": date(2023, 1, 31),
         }
@@ -118,6 +92,7 @@ class TestBudgetingPeriodModel:
         payload = {
             "name": "2023_01",
             "budget": budget,
+            "status": PeriodStatus.DRAFT,
             "date_start": date(2023, 1, 1),
             "date_end": date(2023, 1, 31),
         }
@@ -132,59 +107,101 @@ class TestBudgetingPeriodModel:
 
     def test_create_active_period_successfully(self, budget: Budget):
         """
-        GIVEN: Single inactive BudgetingPeriod for Budget in database.
-        WHEN: BudgetingPeriod instance create attempt with is_active = True.
-        THEN: Two BudgetingPeriod model instances existing in database - one active, one inactive.
+        GIVEN: Single closed BudgetingPeriod for Budget in database.
+        WHEN: BudgetingPeriod instance create attempt with stats = PeriodStatus.ACTIVE.
+        THEN: Two BudgetingPeriod model instances existing in database - one active, one closed.
         """
         payload_inactive = {
             "name": "2023_01",
             "budget": budget,
+            "status": PeriodStatus.CLOSED,
             "date_start": date(2023, 1, 1),
             "date_end": date(2023, 1, 31),
-            "is_active": False,
         }
         payload_active = {
             "name": "2023_02",
             "budget": budget,
+            "status": PeriodStatus.ACTIVE,
             "date_start": date(2023, 2, 1),
             "date_end": date(2023, 2, 28),
-            "is_active": True,
         }
 
         period_inactive = BudgetingPeriod.objects.create(**payload_inactive)
         period_active = BudgetingPeriod.objects.create(**payload_active)
-        assert period_inactive.is_active is False
-        assert period_active.is_active is True
+        assert period_inactive.status is PeriodStatus.CLOSED
+        assert period_active.status is PeriodStatus.ACTIVE
         assert BudgetingPeriod.objects.filter(budget=budget).count() == 2
 
-    def test_error_is_active_set_already(self, budget: Budget):
+    def test_error_active_period_already_exists(self, budget: Budget):
         """
         GIVEN: Single active BudgetingPeriod for Budget in database.
-        WHEN: BudgetingPeriod instance create attempt with is_active = True.
+        WHEN: BudgetingPeriod instance create attempt with status = PeriodStatus.ACTIVE.
         THEN: ValidationError raised as active period already exists. New period not created in database.
         """
         payload_1 = {
             "name": "2023_01",
             "budget": budget,
+            "status": PeriodStatus.ACTIVE,
             "date_start": date(2023, 1, 1),
             "date_end": date(2023, 1, 31),
-            "is_active": True,
         }
         active_period = BudgetingPeriod.objects.create(**payload_1)
 
         payload_2 = {
             "name": "2023_02",
             "budget": budget,
+            "status": PeriodStatus.ACTIVE,
             "date_start": date(2023, 1, 1),
             "date_end": date(2023, 1, 31),
-            "is_active": True,
         }
         with pytest.raises(ValidationError) as exc:
             BudgetingPeriod.objects.create(**payload_2)
-        assert exc.value.code == "active-invalid"
-        assert exc.value.message == "is_active: Active period already exists."
+        assert exc.value.message == "status: Active period already exists in Budget."
         assert BudgetingPeriod.objects.filter(budget=budget).count() == 1
         assert BudgetingPeriod.objects.filter(budget=budget).first() == active_period
+
+    def test_error_closed_period_cannot_be_changed(self, budget: Budget):
+        """
+        GIVEN: Single closed BudgetingPeriod for Budget in database.
+        WHEN: BudgetingPeriod instance update attempt.
+        THEN: ValidationError raised as closed period cannot be changed. Period not updated in database.
+        """
+        payload_1 = {
+            "name": "2023_01",
+            "budget": budget,
+            "status": PeriodStatus.CLOSED,
+            "date_start": date(2023, 1, 1),
+            "date_end": date(2023, 1, 31),
+        }
+        period = BudgetingPeriod.objects.create(**payload_1)
+        with pytest.raises(ValidationError) as exc:
+            period.name = "2023_01_1"
+            period.save()
+        period.refresh_from_db()
+        assert exc.value.message == "status: Closed period cannot be changed."
+        assert period.name == payload_1["name"]
+
+    def test_error_active_period_cannot_be_draft_again(self, budget: Budget):
+        """
+        GIVEN: Single active BudgetingPeriod for Budget in database.
+        WHEN: BudgetingPeriod instance update attempt with draft status.
+        THEN: ValidationError raised as active period cannot be moved back to draft status.
+        Period not updated in database.
+        """
+        payload_1 = {
+            "name": "2023_01",
+            "budget": budget,
+            "status": PeriodStatus.ACTIVE,
+            "date_start": date(2023, 1, 1),
+            "date_end": date(2023, 1, 31),
+        }
+        period = BudgetingPeriod.objects.create(**payload_1)
+        with pytest.raises(ValidationError) as exc:
+            period.status = PeriodStatus.DRAFT
+            period.save()
+        period.refresh_from_db()
+        assert exc.value.message == "status: Active period cannot be moved back to Draft status."
+        assert period.status == payload_1["status"]
 
     def test_error_date_end_before_date_start(self, budget: Budget):
         """
@@ -195,6 +212,7 @@ class TestBudgetingPeriodModel:
         payload = {
             "name": "2023_01",
             "budget": budget,
+            "status": PeriodStatus.ACTIVE,
             "date_start": date(2023, 5, 1),
             "date_end": date(2023, 4, 30),
         }
@@ -254,18 +272,21 @@ class TestBudgetingPeriodModel:
         payload_1 = {
             "name": "2023_06",
             "budget": budget,
+            "status": PeriodStatus.DRAFT,
             "date_start": date(2023, 6, 1),
             "date_end": date(2023, 6, 30),
         }
         payload_2 = {
             "name": "2023_07",
             "budget": budget,
+            "status": PeriodStatus.DRAFT,
             "date_start": date(2023, 7, 1),
             "date_end": date(2023, 7, 31),
         }
         payload_invalid = {
             "name": "invalid",
             "budget": budget,
+            "status": PeriodStatus.DRAFT,
             "date_start": date_start,
             "date_end": date_end,
         }
