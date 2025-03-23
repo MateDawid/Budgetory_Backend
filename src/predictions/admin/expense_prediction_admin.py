f8e0bb91d16ca7305f6c97a52ba3e7210e09ff92
MateDawid <mateusiakdawid@gmail.com>
2025-03-23 07:57:31 +0100

    expense_predictions_adjust: Update admin site of ExpensePrediction model

--- src/predictions/admin/expense_prediction_admin.py
+++ src/predictions/admin/expense_prediction_admin.py
@@ -7,5 +7,6 @@ from predictions.models.expense_prediction_model import ExpensePrediction
 class ExpensePredictionAdmin(admin.ModelAdmin):
     """Custom admin view for ExpensePrediction model."""
 
-    list_display = ("period", "category", "value")
+    list_display = ("period", "category", "initial_value", "current_value")
     list_filter = ("period__budget__name",)
+    readonly_fields = ("initial_value",)
