5ca3813f8631d8eeb9bd505122ba112cdd172d1e
MateDawid <mateusiakdawid@gmail.com>
2025-04-14 10:54:57 +0200

    budgets_rework: Enable to return all items from endpoint when no pagination details passed

--- src/app_infrastructure/paginations.py
+++ src/app_infrastructure/paginations.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class DefaultPagination(PageNumberPagination):
-    page_size = 10
+    page_size = None  # None value enables to return all objects when no pagination params passed.
     page_size_query_param = "page_size"
     max_page_size = 1000
