96d1e441b86460f0807afb73c711873d1bdea86d
MateDawid <mateusiakdawid@gmail.com>
2025-04-15 07:44:54 +0200

    budgets_rework: balance in DepositSerializer

--- src/entities/serializers/deposit_serializer.py
+++ src/entities/serializers/deposit_serializer.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from entities.models.deposit_model import Deposit
 from entities.serializers.entity_serializer import EntitySerializer
 
@@ -5,7 +7,9 @@ from entities.serializers.entity_serializer import EntitySerializer
 class DepositSerializer(EntitySerializer):
     """Serializer for Deposit."""
 
+    balance = serializers.IntegerField(default=0)
+
     class Meta:
         model = Deposit
-        fields = ["id", "name", "description", "is_active"]
+        fields = ["id", "name", "description", "is_active", "balance"]
         read_only_fields = ["id"]
