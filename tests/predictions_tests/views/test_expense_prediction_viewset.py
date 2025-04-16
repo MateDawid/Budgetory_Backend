2d6b1b344787dfdb6237d2502c20956cd3fc1b13
MateDawid <mateusiakdawid@gmail.com>
2025-04-16 12:06:33 +0200

    budgets_rework: predictions app tests updated

--- tests/predictions_tests/views/test_expense_prediction_viewset.py
+++ tests/predictions_tests/views/test_expense_prediction_viewset.py
@@ -56,6 +56,53 @@ class TestExpensePredictionViewSetList:
         response = api_client.get(url, HTTP_AUTHORIZATION=f"Bearer {jwt_access_token}")
         assert response.status_code == status.HTTP_200_OK
 
+    def test_get_response_without_pagination(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_prediction_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: Ten ExpensePrediction model instances for single Budget created in database.
+        WHEN: ExpensePredictionViewSet called by Budget member without pagination parameters.
+        THEN: HTTP 200 - Response with all objects returned.
+        """
+        budget = budget_factory(members=[base_user])
+        for _ in range(10):
+            expense_prediction_factory(budget=budget)
+        api_client.force_authenticate(base_user)
+
+        response = api_client.get(expense_prediction_url(budget.id))
+
+        assert response.status_code == status.HTTP_200_OK
+        assert "results" not in response.data
+        assert "count" not in response.data
+        assert len(response.data) == 10
+
+    def test_get_response_with_pagination(
+        self,
+        api_client: APIClient,
+        base_user: AbstractUser,
+        budget_factory: FactoryMetaClass,
+        expense_prediction_factory: FactoryMetaClass,
+    ):
+        """
+        GIVEN: Ten ExpensePrediction model instances for single Budget created in database.
+        WHEN: ExpensePredictionViewSet called by Budget member with pagination parameters - page_size and page.
+        THEN: HTTP 200 - Paginated response returned.
+        """
+        budget = budget_factory(members=[base_user])
+        for _ in range(10):
+            expense_prediction_factory(budget=budget)
+        api_client.force_authenticate(base_user)
+
+        response = api_client.get(expense_prediction_url(budget.id), data={"page_size": 2, "page": 1})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert "results" in response.data
+        assert response.data["count"] == 10
+
     def test_user_not_budget_member(
         self,
         api_client: APIClient,
@@ -99,7 +146,7 @@ class TestExpensePredictionViewSetList:
         predictions = ExpensePrediction.objects.filter(period__budget=budget)
         serializer = ExpensePredictionSerializer(predictions, many=True)
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"] == serializer.data
+        assert response.data == serializer.data
 
     def test_prediction_list_limited_to_budget(
         self,
@@ -123,9 +170,9 @@ class TestExpensePredictionViewSetList:
         predictions = ExpensePrediction.objects.filter(period__budget=budget)
         serializer = ExpensePredictionSerializer(predictions, many=True)
         assert response.status_code == status.HTTP_200_OK
-        assert len(response.data["results"]) == len(serializer.data) == predictions.count() == 1
-        assert response.data["results"] == serializer.data
-        assert response.data["results"][0]["id"] == prediction.id
+        assert len(response.data) == len(serializer.data) == predictions.count() == 1
+        assert response.data == serializer.data
+        assert response.data[0]["id"] == prediction.id
 
 
 @pytest.mark.django_db
