53c64b591f3381c6d6d9b9e0c83fc990f235cd0c
MateDawid <mateusiakdawid@gmail.com>
2025-07-27 08:16:33 +0200

    predictions_in_periods: owner field on Entity model

--- src/entities/admin/deposit_admin.py
+++ src/entities/admin/deposit_admin.py
@@ -7,5 +7,5 @@ from entities.models.deposit_model import Deposit
 class DepositAdmin(admin.ModelAdmin):
     """Custom admin view for Deposit model."""
 
-    list_display = ("name", "budget", "description", "is_active")
-    list_filter = ("budget__name", "is_active")
+    list_display = ("name", "budget", "description", "is_active", "owner")
+    list_filter = ("budget__name", "is_active", "owner")
--- src/entities/models/entity_model.py
+++ src/entities/models/entity_model.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 from entities.managers.deposit_manager import DepositManager
@@ -13,6 +14,13 @@ class Entity(models.Model):
     description = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
     is_deposit = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="deposits",  # Entity.owner field dedicated to be used only in terms of Deposit ownership.
+    )
 
     objects = models.Manager()
     deposits = DepositManager()
@@ -22,6 +30,12 @@ class Entity(models.Model):
         unique_together = (
             "name",
             "budget",
+        )
+        constraints = (
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_owner_value_only_on_deposit",
+                check=models.Q(is_deposit=True) | models.Q(is_deposit=False, owner__isnull=True),
+            ),
         )
 
     def __str__(self) -> str:
