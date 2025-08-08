e2a64f06079e3a8a5013ed8deb155554d4fc5cc8
MateDawid <mateusiakdawid@gmail.com>
2025-08-08 13:16:59 +0200

    predictions_update: Update Transfer and Entity factories

--- tests/entities_tests/factories.py
+++ tests/entities_tests/factories.py
@@ -1,6 +1,8 @@
 import factory
 from budgets_tests.factories import BudgetFactory
 
+from entities.models.choices.deposit_type import DepositType
+
 
 class EntityFactory(factory.django.DjangoModelFactory):
     """Factory for Entity model."""
@@ -13,12 +15,15 @@ class EntityFactory(factory.django.DjangoModelFactory):
     description = factory.Faker("text", max_nb_chars=255)
     is_active = factory.Faker("boolean")
     is_deposit = factory.LazyAttribute(lambda _: False)
+    deposit_type = factory.LazyAttribute(lambda _: None)
+    owner = factory.LazyAttribute(lambda _: None)
 
 
 class DepositFactory(EntityFactory):
     """Factory for Deposit model."""
 
     is_deposit = factory.LazyAttribute(lambda _: True)
+    deposit_type = factory.Iterator(DepositType.values)
 
     class Meta:
         model = "entities.Deposit"
