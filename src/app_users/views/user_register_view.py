7156c9060545ceab697d80945695687283ce8684
MateDawid <mateusiakdawid@gmail.com>
2025-09-11 09:21:21 +0200

    user_register_update: Fix UserRegisterView.post

--- src/app_users/views/user_register_view.py
+++ src/app_users/views/user_register_view.py
@@ -1,5 +1,5 @@
 from django.db import transaction
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.response import Response
 
 from app_users.serializers.user_register_serializer import UserRegisterSerializer
@@ -13,11 +13,8 @@ class UserRegisterView(generics.CreateAPIView):
     permission_classes = ()
 
     def post(self, request, *args, **kwargs) -> Response:
-        try:
-            with transaction.atomic():
-                response = self.create(request, *args, **kwargs)
-                budget = Budget.objects.create(name="Your Budget", currency="$")
-                budget.members.add(response.data.serializer.instance)
-        except Exception:
-            response = Response("User creation failed.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        return response
+        with transaction.atomic():
+            response = self.create(request, *args, **kwargs)
+            budget = Budget.objects.create(name="Your Budget", currency="$")
+            budget.members.add(response.data.serializer.instance)
+            return response
