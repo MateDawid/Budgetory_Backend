44bfffe47b51c418de8888b2bf4c22dfc00525af
MateDawid <mateusiakdawid@gmail.com>
2025-09-11 08:45:34 +0200

    user_register_update: Create initial Budget on User registration

--- src/app_users/views/user_register_view.py
+++ src/app_users/views/user_register_view.py
@@ -1,6 +1,9 @@
-from rest_framework import generics
+from django.db import transaction
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 from app_users.serializers.user_register_serializer import UserRegisterSerializer
+from budgets.models import Budget
 
 
 class UserRegisterView(generics.CreateAPIView):
@@ -8,3 +11,13 @@ class UserRegisterView(generics.CreateAPIView):
 
     serializer_class = UserRegisterSerializer
     permission_classes = ()
+
+    def post(self, request, *args, **kwargs) -> Response:
+        try:
+            with transaction.atomic():
+                response = self.create(request, *args, **kwargs)
+                budget = Budget.objects.create(name="Your Budget", currency="$")
+                budget.members.add(response.data.serializer.instance)
+        except Exception:
+            response = Response("User creation failed.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response
