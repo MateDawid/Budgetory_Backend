82bdaa0703ee2bd7a2e334fac25478a07574c2ec
MateDawid <mateusiakdawid@gmail.com>
2025-10-09 08:25:08 +0200

    predictions_separate_page: Possibility to filter CategoryPriority by CategoryType in CategoryPriorityView

--- src/categories/views/category_priority_view.py
+++ src/categories/views/category_priority_view.py
@@ -1,8 +1,26 @@
+from typing import Callable
+
+from django.utils.functional import _StrPromise
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from categories.models.choices.category_priority import CategoryPriority
+from categories.models.choices.category_type import CategoryType
+
+
+def format_typed_priorities(
+    priorities: tuple[CategoryPriority, ...]
+) -> list[tuple[Callable[[], int], Callable[[], str | _StrPromise]]]:
+    """
+    Function to prepare select choices of CategoryPriority filtered values on frontend.
+    Args:
+        priorities (tuple[CategoryPriority, ...]): List of filtered CategoryPriority choices.
+
+    Returns:
+        list[tuple[Callable[[], int], Callable[[], str | _StrPromise]]]: Formatted priorities.
+    """
+    return [(priority.value, priority.label) for priority in priorities]
 
 
 class CategoryPriorityView(APIView):
@@ -23,4 +41,13 @@ class CategoryPriorityView(APIView):
         Returns:
             Response: Priority field choices for TransferCategory.
         """
-        return Response({"results": [{"value": choice[0], "label": choice[1]} for choice in self.choices]})
+        choices = []
+        match int(request.query_params.get("type", 0)):
+            case CategoryType.INCOME.value:
+                choices = format_typed_priorities(CategoryPriority.income_priorities())
+            case CategoryType.EXPENSE.value:
+                choices = format_typed_priorities(CategoryPriority.expense_priorities())
+            case _:
+                choices = CategoryPriority.choices
+
+        return Response({"results": [{"value": choice[0], "label": choice[1]} for choice in choices]})
