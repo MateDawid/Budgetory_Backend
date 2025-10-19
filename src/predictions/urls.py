659248e250780590b9e5a617ae0c1f9588ac73a9
MateDawid <mateusiakdawid@gmail.com>
2025-10-19 07:31:16 +0200

    categories_owned_by_deposit: url for DepositsResultsAPIView updated

--- src/predictions/urls.py
+++ src/predictions/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from predictions.views.copy_predictions_from_previous_period_view import CopyPredictionsFromPreviousPeriodAPIView
-from predictions.views.users_results_view import UsersResultsAPIView
+from predictions.views.deposits_results_view import DepositsResultsAPIView
 
 app_name = "predictions"
 
 urlpatterns = [
-    path("user_results/<int:period_pk>/", UsersResultsAPIView.as_view(), name="users-results"),
+    path("deposits_results/<int:period_pk>/", DepositsResultsAPIView.as_view(), name="deposits-results"),
     path(
         "copy_predictions_from_previous_period/<int:period_pk>/",
         CopyPredictionsFromPreviousPeriodAPIView.as_view(),
