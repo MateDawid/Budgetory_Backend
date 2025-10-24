8a8370f64b0f1b608662decfe1a107da8ff9044d
MateDawid <mateusiakdawid@gmail.com>
2025-10-24 08:04:00 +0200

    categories_owned_by_deposit: Tests for CategoryPriorityView filtering

--- tests/categories_tests/views/test_category_priority_view.py
+++ tests/categories_tests/views/test_category_priority_view.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from categories.models.choices.category_priority import CategoryPriority
+from categories.models.choices.category_type import CategoryType
+from categories.views.category_priority_view import format_typed_priorities
 
 CATEGORY_PRIORITY_URL = reverse("categories:category-priority")
 
@@ -33,4 +35,32 @@ class TestCategoryPriorityView:
         assert response.status_code == status.HTTP_200_OK
         assert response.data["results"] == [
             {"value": choice[0], "label": choice[1]} for choice in CategoryPriority.choices
+        ]
+
+    def test_get_priorities_list_filtered_with_income(self, api_client: APIClient):
+        """
+        GIVEN: CategoryPriority choices defined in application.
+        WHEN: Performing HTTP GET request on CATEGORY_PRIORITY_URL with INCOME type filter.
+        THEN: HTTP_200_OK, choices for CategoryPriority returned.
+        """
+        response = api_client.get(CATEGORY_PRIORITY_URL, data={"type": CategoryType.INCOME.value})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"] == [
+            {"value": choice[0], "label": choice[1]}
+            for choice in format_typed_priorities(CategoryPriority.income_priorities())
+        ]
+
+    def test_get_priorities_list_filtered_with_expense(self, api_client: APIClient):
+        """
+        GIVEN: CategoryPriority choices defined in application.
+        WHEN: Performing HTTP GET request on CATEGORY_PRIORITY_URL with EXPENSE type filter.
+        THEN: HTTP_200_OK, choices for CategoryPriority returned.
+        """
+        response = api_client.get(CATEGORY_PRIORITY_URL, data={"type": CategoryType.EXPENSE.value})
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"] == [
+            {"value": choice[0], "label": choice[1]}
+            for choice in format_typed_priorities(CategoryPriority.expense_priorities())
         ]
