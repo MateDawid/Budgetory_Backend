00d469b39db6b0c0c2546e1cb4b144fe0cc62b28
MateDawid <mateusiakdawid@gmail.com>
2025-11-24 08:34:11 +0100

    uncategorized_expense_prediction: Remove separate UncategorizedPredictionsView

--- src/predictions/urls.py
+++ src/predictions/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from predictions.views.copy_predictions_from_previous_period_view import CopyPredictionsFromPreviousPeriodAPIView
 from predictions.views.deposits_predictions_results_view import DepositsPredictionsResultsAPIView
-from predictions.views.uncategorized_prediction_view import UncategorizedPredictionView
 
 app_name = "predictions"
 
@@ -21,10 +20,5 @@ urlpatterns = [
         "copy_predictions_from_previous_period/<int:period_pk>/",
         CopyPredictionsFromPreviousPeriodAPIView.as_view(),
         name="copy-predictions-from-previous-period",
-    ),
-    path(
-        "uncategorized_prediction/<int:period_pk>/",
-        UncategorizedPredictionView.as_view(),
-        name="uncategorized-prediction",
     ),
 ]
