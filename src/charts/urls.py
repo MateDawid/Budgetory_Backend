4e5301bc6fce69645f73b411435fe5602b6c21b5
MateDawid <mateusiakdawid@gmail.com>
2025-12-22 07:20:33 +0100

    landing_page: CategoriesInPeriodsChartAPIView and tests

--- src/charts/urls.py
+++ src/charts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from charts.views.categories_in_periods_chart_view import CategoriesInPeriodsChartAPIView
 from charts.views.deposits_in_periods_chart_view import DepositsInPeriodsChartAPIView
 from charts.views.transfers_in_periods_chart_view import TransfersInPeriodsChartApiView
 
@@ -15,5 +16,10 @@ urlpatterns = [
         "budgets/<int:budget_pk>/charts/transfers_in_periods/",
         TransfersInPeriodsChartApiView.as_view(),
         name="transfers-in-periods-chart",
+    ),
+    path(
+        "budgets/<int:budget_pk>/charts/categories_in_periods/",
+        CategoriesInPeriodsChartAPIView.as_view(),
+        name="categories-in-periods-chart",
     ),
 ]
