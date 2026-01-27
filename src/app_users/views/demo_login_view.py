c9cba696588af8295d049c37e9686e72da3256bd
MateDawid <mateusiakdawid@gmail.com>
2026-01-27 07:45:21 +0100

    demo_login: Utilize demo_login_service in demo_login_view

--- src/app_users/views/demo_login_view.py
+++ src/app_users/views/demo_login_view.py
@@ -16,4 +16,4 @@ class DemoLoginView(APIView):
     def post(self, request: Request) -> Response:
         if token := get_demo_user_token():
             return Response(token, status=status.HTTP_200_OK)
-        return Response("Demo login failed.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": "Demo login failed."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
